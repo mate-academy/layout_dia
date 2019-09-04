82aa11ea3a2f3d76cccd9736e0166fc60c32ec58
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-04 20:13:25 +0300

    nearly final

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,3 +1,10 @@
 'use strict';
-
-console.log('hello mate academy!');
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  const x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
