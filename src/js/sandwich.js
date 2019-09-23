0a9570090e8fc9b5027439068b0e9192a9a4ac60
Leon <FeelineKing@gmail.com>
2019-09-23 04:47:49 +0300

    v.0.8(a)

--- src/js/sandwich.js
+++ src/js/sandwich.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const z = document.getElementById('nav__sandwich');
-z.addEventListener('click', open);
+const z = document.getElementsByClassName('nav__clicker');
+z[0].addEventListener('change', open);
 
 function open() {
   document.getElementById('nav__list').classList.toggle('nav__show');
