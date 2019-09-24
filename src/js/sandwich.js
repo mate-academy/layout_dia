e8ea26603bfddf66ad0f7dc30a5e99ae91c3e57c
Leon <FeelineKing@gmail.com>
2019-09-24 17:39:39 +0300

    v.0.9(a)

--- src/js/sandwich.js
+++ src/js/sandwich.js
@@ -1,8 +1,23 @@
 'use strict';
 
-const z = document.getElementsByClassName('nav__clicker');
-z[0].addEventListener('change', open);
+const checker = document.querySelector('.nav__clicker');
+const heightChanger = document.querySelector('.nav');
 
 function open() {
   document.getElementById('nav__list').classList.toggle('nav__show');
+  checker.checked = !!checker.checked;
+
+  if (checker.checked === false) {
+    heightChanger.style.height = '0';
+  }
+
+  if (checker.checked === true && window.matchMedia('(min-width: 851px)').matches) {
+    heightChanger.style.height = '80px';
+  }
+
+  if (checker.checked === true && window.matchMedia('(max-width: 850px)').matches) {
+    heightChanger.style.height = '100vh';
+  }
 }
+
+checker.addEventListener('change', open);
