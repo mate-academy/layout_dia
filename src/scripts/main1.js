6d937632e6fd615ff3d878f1171379996f9d936e
bandura <banduraweb@ukr.net>
2019-11-06 00:30:28 +0200

    process

--- src/scripts/main1.js
+++ src/scripts/main1.js
@@ -1,5 +1,12 @@
 'use strict';
+
 (() => {
+  const id = setInterval(function() {
+    step = step === 4 ? 1 : step = step + 1;
+    sliderContainer.style.cssText
+      = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`; // eslint-disable-line no-use-before-define
+  }, 3000);
+
   const buttonPrev = document.getElementById('next');
   const buttonNext = document.getElementById('prev');
   const sliderContainer = document.querySelector('.header__slider');
@@ -7,12 +14,14 @@
   let step = 1;
 
   buttonNext.onclick = function() {
+    clearInterval(id);
     step = step === 4 ? 1 : step = step + 1;
     sliderContainer.style.cssText
       = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`;
   };
 
   buttonPrev.onclick = function() {
+    clearInterval(id);
     step = step === 1 ? step = 4 : step = step - 1;
     sliderContainer.style.cssText
       = `background: url("https://banduraweb.github.io/layout_dia/images/slider/slide-img-${step}.jpg");`;
