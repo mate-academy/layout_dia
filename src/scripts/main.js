095e82e517768ab38d170c5ea31a09eeea87dcf3
bandura <banduraweb@ukr.net>
2019-11-05 11:33:49 +0200

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
+(() => {
+  const buttonPrev = document.getElementById('next');
+  const buttonNext = document.getElementById('prev');
+  const sliderContainer = document.querySelector('.header__slider');
 
-console.log('hello mate academy!');
+  let step = 1;
+  const id = setInterval(function() {
+    step = step === 4 ? 1 : step = step + 1;
+    sliderContainer.style.cssText
+      = `background: url("../images/slider/slide-img-${step}.jpg");`;
+  }, 3000);
+
+  buttonNext.onclick = function() {
+    clearInterval(id);
+    step = step === 4 ? 1 : step = step + 1;
+    sliderContainer.style.cssText
+      = `transition: none; background: 
+      url("../images/slider/slide-img-${step}.jpg");`;
+  };
+
+  buttonPrev.onclick = function() {
+    clearInterval(id);
+    step = step === 1 ? step = 4 : step = step - 1;
+    sliderContainer.style.cssText
+      = `transition: none; background: 
+      url("../images/slider/slide-img-${step}.jpg");`;
+  };
+})();
