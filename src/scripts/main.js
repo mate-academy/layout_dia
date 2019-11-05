532ad9b290b4e5c47ed3b553c22c5c0fd41275dd
bandura <banduraweb@ukr.net>
2019-11-05 13:34:39 +0200

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,25 +5,20 @@
   const sliderContainer = document.querySelector('.header__slider');
 
   let step = 1;
-  const id = setInterval(function() {
-    step = step === 4 ? 1 : step = step + 1;
-    sliderContainer.style.cssText
-      = `background: url("./images/slider/slide-img-${step}.jpg");`;
-  }, 3000);
 
   buttonNext.onclick = function() {
-    clearInterval(id);
     step = step === 4 ? 1 : step = step + 1;
     sliderContainer.style.cssText
       = `transition: none; background: 
-      url("./images/slider/slide-img-${step}.jpg");`;
+      url("https://banduraweb.github.io/layout_dia/
+      images/slider/slide-img-${step}.jpg");`;
   };
 
   buttonPrev.onclick = function() {
-    clearInterval(id);
     step = step === 1 ? step = 4 : step = step - 1;
     sliderContainer.style.cssText
       = `transition: none; background: 
-      url("./images/slider/slide-img-${step}.jpg");`;
+      url("https://banduraweb.github.io/layout_dia/
+      images/slider/slide-img-${step}.jpg");`;
   };
 })();
