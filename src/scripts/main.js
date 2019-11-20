d8412eb51a54d6d403c1a2f4ebddfaf4b058139c
MMashivskij <mmashivskij@gmail.com>
2019-11-20 10:45:18 +0200

    11-12

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const width = 1146;
+
+const slider = document.getElementById('slider');
+const list = slider.querySelector('ul');
+const listElems = slider.querySelectorAll('li');
+
+let position = 0;
+
+slider.querySelector('.slider__button_left').onclick = function() {
+  position += width;
+  position = Math.min(position, 0);
+  list.style.marginLeft = position + 'px';
+};
+
+slider.querySelector('.slider__button_right').onclick = function() {
+  position -= width;
+  position = Math.max(position, -width * (listElems.length - 1));
+  list.style.marginLeft = position + 'px';
+};
