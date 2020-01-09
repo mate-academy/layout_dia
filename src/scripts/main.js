11fe8dcbe397f344dd95c8ee40b07cf10435013d
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-09 23:24:27 +0200

    1-3 points done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,23 @@
 'use strict';
 
-console.log('hello mate academy!');
+window.onload = function() {
+  const activLink = document.getElementsByClassName('nav__link');
+  const activHire = document.getElementsByClassName('hire__link')[0];
+
+  // console.log(activHire);
+
+  for (let i = 0; i < activLink.length; i++) {
+    activLink[i].addEventListener('click', function() {
+      for (let c = 0; c < activLink.length; c++) {
+        activLink[c].classList.remove('nav__link_active');
+      }
+      this.classList.toggle('nav__link_active');
+    });
+  }
+
+  activHire.addEventListener('click', function() {
+    if (this.classList.contains('hire__link_activ')) {
+      this.classList.remove('hire__link_activ');
+    }
+  });
+};
