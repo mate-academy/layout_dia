b11ee026160d406dae3c1f5b44bce54710f90589
Senya Volkov <formobileprima@gmail.com>
2020-01-09 19:48:57 +0200

    completed adaptive header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const burgerMenu = document.body.querySelector('.burger-menu');
+const headerNavUl = document.body.querySelector('.header__nav-ul');
+const HeaderHireUs = document.body.querySelector('.header__hire-us-link');
+
+burgerMenu.addEventListener('click', (e) => {
+  burgerMenu.classList.toggle('opend-menu');
+  headerNavUl.classList.toggle('move-ul');
+  HeaderHireUs.classList.toggle('move-hire-us');
+});
