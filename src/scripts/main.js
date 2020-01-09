ee4d2b35f367349e4e280088ff1d9de39283b2f6
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-10 00:08:47 +0200

    better 1-3 point

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,20 +4,16 @@ window.onload = function() {
   const activLink = document.getElementsByClassName('nav__link');
   const activHire = document.getElementsByClassName('hire__link')[0];
 
-  // console.log(activHire);
-
   for (let i = 0; i < activLink.length; i++) {
     activLink[i].addEventListener('click', function() {
       for (let c = 0; c < activLink.length; c++) {
-        activLink[c].classList.remove('nav__link_active');
+        activLink[c].classList.remove('nav__link_activ');
       }
-      this.classList.toggle('nav__link_active');
+      this.classList.toggle('nav__link_activ');
     });
   }
 
   activHire.addEventListener('click', function() {
-    if (this.classList.contains('hire__link_activ')) {
-      this.classList.remove('hire__link_activ');
-    }
+    activHire.classList.toggle('hire__link--activ');
   });
 };
