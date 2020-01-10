3c3b3333789970a4d0be41a598bb013b5e1f8d63
Senya Volkov <formobileprima@gmail.com>
2020-01-10 22:04:34 +0200

    added work slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,32 @@ burgerMenu.addEventListener('click', (e) => {
   headerNavUl.classList.toggle('move-ul');
   HeaderHireUs.classList.toggle('move-hire-us');
 });
+
+// SLIDER
+
+const SliderImg = document.body.querySelectorAll('.slider__img');
+const SliderItem = document.body.querySelector('.slider__item');
+
+document.body.addEventListener('click', (e) => {
+  const target = e.target;
+
+  if (target.classList.contains('intro__arrows-item--left')) {
+    const position = parseInt(getComputedStyle(SliderItem).left);
+
+    if ((position + 687) > 0) {
+      SliderItem.style.left = -(SliderImg.length - 1) * 687 + 'px';
+
+      return;
+    }
+    SliderItem.style.left = (position + 687) + 'px';
+  } else if (target.classList.contains('intro__arrows-item--right')) {
+    const position = parseInt(getComputedStyle(SliderItem).left);
+
+    if ((position - 687) < (-(SliderImg.length - 1) * 687)) {
+      SliderItem.style.left = 0 + 'px';
+
+      return;
+    }
+    SliderItem.style.left = (position - 687) + 'px';
+  }
+});
