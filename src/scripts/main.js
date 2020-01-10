17ff4d3aadc9b5e4fc370107868d13779d2e9ad4
Senya Volkov <formobileprima@gmail.com>
2020-01-10 19:26:10 +0200

    fixed some remarks and added WELCOME part plus overlay

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const burgerMenu = document.body.querySelector('.burger-menu');
-const headerNavUl = document.body.querySelector('.header__nav-ul');
+const headerNavUl = document.body.querySelector('.header__nav-list');
 const HeaderHireUs = document.body.querySelector('.header__hire-us-link');
 
 burgerMenu.addEventListener('click', (e) => {
