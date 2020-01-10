75eb1b892c569593399168948fa4375700741930
lutfie <serverieva.lutfie@gmail.com>
2020-01-10 08:15:59 +0200

    added 3 blocks of dia without adaptability

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
-console.log('hello mate academy!');
+const slider = {
+  currentItem: 0,
+  photos: ['slide1.png', 'slide2.png', 'slide3.png'],
+  next: function(el) {
+    el.preventDefault();
+    slider.currentItem++;
+
+    if (slider.currentItem >= slider.photos.length) {
+      slider.currentItem = 0;
+    }
+
+    const currentImage = slider.photos[slider.currentItem];
+
+    document
+      .querySelector('.slider__item > img')
+      .setAttribute('src', 'images/slider/' + currentImage);
+  },
+  prev: function(el) {
+    el.preventDefault();
+
+    if (slider.currentItem === 0) {
+      slider.currentItem = slider.photos.length - 1;
+    } else {
+      slider.currentItem--;
+    }
+
+    const currentImage = slider.photos[slider.currentItem];
+
+    document
+      .querySelector('.slider__item > img')
+      .setAttribute('src', 'images/slider/' + currentImage);
+  },
+};
