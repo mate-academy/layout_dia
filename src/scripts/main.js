745929629bf917a637a1ffb07fabeeb255898f34
Mikhail <mikhail.sapuzhak@gmail.com>
2020-01-12 21:39:32 +0200

    problem with linter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function onClickMenu() {
+  document.getElementById('menu').classList.toggle('change');
+  document.getElementById('nav').classList.toggle('change');
+
+  document.getElementById('menu-bg').classList.toggle('change-bg');
+}
+
+onClickMenu();
