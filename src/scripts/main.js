77d2dac0d46ec460278c9e8f71ad4901da0b4dff
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-01-13 16:10:59 +0200

    added adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const menu = () => {
+  const BURGER = document.querySelector('.header__btn');
+  const MOBILENAV = document.querySelector('.nav');
+
+  BURGER.addEventListener('click', () => {
+    BURGER.classList.toggle('header__btn--active');
+
+    if (BURGER.classList.contains('header__btn--active')) {
+      MOBILENAV.style.display = 'flex';
+    } else {
+      MOBILENAV.removeAttribute('style');
+    }
+  });
+};
+
+menu();
