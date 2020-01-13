40725e0d86e8bbd6cc2ecca116243c901bfe552e
Olexander <kravchenko.sania@gmail.com>
2020-01-13 17:05:45 +0200

    first commit 1-3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log('hello mate academy!');
+// console.log('hello mate academy!');
+
+function initMenu() {
+  const burger = document.querySelector('.switcher');
+  const switcher = document.querySelector('.header__main-nav');
+
+  burger.addEventListener('click', () => {
+    burger.classList.toggle('switcher--close');
+    switcher.classList.toggle('nav--visible');
+  });
+}
+
+initMenu();
