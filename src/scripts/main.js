058d4df2b7f9b57ab0583c627213e7fbbddfdf32
Actireon <eheieiei37@gmail.com>
2020-01-14 16:19:18 +0200

    rafactored some things

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,1 @@
 'use strict';
-
-// trying to implement slider, gonna continue later
-
-// const btnLeft = document.querySelector('#btn-left');
-// const btnRight = document.querySelector('#btn-right');
-// const slider = document.querySelector('.slider');
-
-// const arr = ['slider1', 'slider2'];
-
-// btnLeft.addEventListener('click', function() {
-//   slider.classList.toggle('slider1');
-//   slider.classList.toggle('slider2');
-//   setTimeout(() => slider.classList.toggle('slider2'), 2000);
-// })
