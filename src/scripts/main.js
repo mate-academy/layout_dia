9141b05581310e891660d684b29792ac4f3d4a14
ded-git <sakarailko@gmail.com>
2020-01-14 12:07:20 +0200

    add .js file ad some other thigs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-undef */
 'use strict';
 
 // console.log('hello mate academy!');
+
+$(document).ready(function() {
+  $('.header__mob-icon').on('click', function() {
+    $('.nav').toggleClass('open');
+    $('.info__title-name').toggleClass('open');
+  });
+});
