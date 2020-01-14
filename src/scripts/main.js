6d1b4001e7988bffed4e3ba9233b2e1fc6a2627c
Valeriy-Sorochynskyi <haribol162@gmail.com>
2020-01-14 21:32:32 +0200

    dia header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const burgerButton = document.querySelector('.header__burger');
+const menuCross = document.querySelector('.mobile-menu__cross');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+burgerButton.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu--open');
+});
+
+menuCross.addEventListener('click', () => {
+  mobileMenu.classList.remove('mobile-menu--open');
+});
