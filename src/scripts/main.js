f8bca63f210a6cd46a1247531fbeb37dc7b7da17
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-01-15 16:47:49 +0200

    fix bags

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,13 @@
 'use strict';
 
 const menu = () => {
-  const BURGER = document.querySelector('.header__btn');
+  const BURGER = document.querySelector('.header__button');
   const MOBILENAV = document.querySelector('.nav');
 
   BURGER.addEventListener('click', () => {
-    BURGER.classList.toggle('header__btn--active');
+    BURGER.classList.toggle('header__button--active');
 
-    if (BURGER.classList.contains('header__btn--active')) {
+    if (BURGER.classList.contains('header__button--active')) {
       MOBILENAV.style.display = 'flex';
     } else {
       MOBILENAV.removeAttribute('style');
