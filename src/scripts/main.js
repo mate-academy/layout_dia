9339acae34652905ffa8387d692272e24e53375b
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-15 09:35:05 +0200

    air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,1 @@
 'use strict';
-
-const openMenu = () => {
-  const icon = document.querySelector('.burger');
-  const menu = document.querySelector('.menu');
-
-  icon.addEventListener('click', () => {
-    menu.classList.toggle('menu--opened');
-    menu.classList.toggle('menu--closed');
-    icon.classList.toggle('toggle');
-  });
-};
-
-openMenu();
