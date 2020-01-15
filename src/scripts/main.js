8f9f0e73bf3809d745f0bcd282c12a83bdb7d9cd
Roma <romakoder@gmail.com>
2020-01-15 03:06:36 +0200

    fixed parts 1-3 & add parts 4-6

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello mate academy!');
+// eslint-disable-next-line max-len
+document.querySelector('.toggler').addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if (this.classList.contains('toggler--active')) {
+    this.classList.remove('toggler--active');
+    // eslint-disable-next-line max-len
+    document.querySelector('.nav__mobile').classList.remove('nav__mobile--active');
+  } else {
+    this.classList.add('toggler--active');
+    // eslint-disable-next-line max-len
+    document.querySelector('.nav__mobile').classList.add('nav__mobile--active');
+  }
+});
