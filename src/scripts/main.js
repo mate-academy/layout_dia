104b317db2a08d5f06c2c653253660154e6a779d
Natalia Lypovyk <cardinnata@gmail.com>
2020-01-16 01:31:10 +0200

    adaptive header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const openMenu = () => {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('menu--opened');
+    burger.classList.toggle('burger--toggle');
+  });
+};
+
+openMenu();
