48866fd5f35b7be6a038d9951861f3eff4e0e544
LutsenkoAnV <48694313+LutsenkoAnV@users.noreply.github.com>
2020-01-16 20:20:25 +0200

    fix last mistakes exept white stripe on the right, add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -29,3 +29,41 @@ burger.addEventListener('click', () => {
     burger.classList.remove('header__burger-menu--closed');
   };
 });
+
+let width;
+
+if (document.documentElement.clientWidth > 1000) {
+  width = 43;
+} else {
+  width = 95;
+};
+
+window.addEventListener('resize', function() {
+  if (window.matchMedia('(max-width: 1000px)').matches) {
+    width = 95;
+    mobileNav.classList.remove('header__menu-mobile--opened');
+    mobileNav.classList.remove('header__menu-mobile--closed');
+    headingHeader.classList.remove('header__text-block--hide');
+    headingHeader.classList.remove('header__text-block--visible');
+    burger.classList.remove('header__burger-menu--opened');
+    burger.classList.remove('header__burger-menu--closed');
+  } else {
+    width = 43;
+  }
+});
+
+const carousel = document.querySelector('.slider');
+const list = carousel.querySelector('.slider__images');
+const listElems = carousel.querySelectorAll('.slider__images-item');
+
+let position = 0;
+
+carousel.querySelector('.slider__button-left').onclick = function() {
+  position = Math.min(position + width, 0);
+  list.style.marginLeft = position + 'vw';
+};
+
+carousel.querySelector('.slider__button-right').onclick = function() {
+  position = Math.max(position - width, -width * (listElems.length - 1));
+  list.style.marginLeft = position + 'vw';
+};
