293ca7b3cbeb63dce9575732c8a4f4a77d501df9
Stanislav Mochyr <delenjer@gmail.com>
2020-01-16 09:21:26 +0200

    Air change

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const menuShow = () => {
+  const showMenu = document.querySelector('.show-menu');
+  const hideMenu = document.querySelector('.close-menu');
+  const navItem = document.querySelector('.nav__list');
+  const nav = document.querySelector('.nav-header');
+
+  showMenu.addEventListener('click', () => {
+    nav.classList.add('show-nav');
+    nav.classList.remove('hide-nav');
+  });
+
+  hideMenu.addEventListener('click', () => {
+    nav.classList.add('hide-nav');
+    nav.classList.remove('show-nav');
+  });
+
+  navItem.addEventListener('click', () => {
+    nav.classList.remove('show-nav');
+    nav.classList.add('hide-nav');
+  });
+};
+
+menuShow();
