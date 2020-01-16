56ed1ee17fe506cb8457b84dabe98a5700243f49
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-01-16 17:53:11 +0200

    implemented footer and and mobile responsiveness

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const burgerToggler = () => {
+  const burgerMenu = document.querySelector('.hamburger-block');
+  const header = document.querySelector('.header');
+
+  burgerMenu.addEventListener('click', () => {
+    header.classList.toggle('header--opened');
+    burgerMenu.classList.toggle('hamburger-block--opened');
+  });
+};
+
+burgerToggler();
