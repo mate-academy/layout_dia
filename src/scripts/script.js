d2147102d049fc012d3ef70aeaee0358099d7c72
lutfie <serverieva.lutfie@gmail.com>
2020-01-16 12:09:00 +0200

    changed class names

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,7 +4,7 @@ const burger = document.querySelector('.header__burger');
 
 burger
   .addEventListener('click', function() {
-    const menuList = document.querySelector('.header__nav-menu_list');
+    const menuList = document.querySelector('.header__nav-menu__list');
     const description = document.querySelector('.header__intro-description');
 
     menuList.classList.toggle('active');
