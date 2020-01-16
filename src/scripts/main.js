0afa450fb95de1ac1bddee1daf707b35c8be55aa
Roma <romakoder@gmail.com>
2020-01-16 09:28:15 +0200

    fixed comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,15 @@
 'use strict';
 
-// eslint-disable-next-line max-len
 document.querySelector('.toggler').addEventListener('click', function(e) {
   e.preventDefault();
 
   if (this.classList.contains('toggler--active')) {
     this.classList.remove('toggler--active');
     // eslint-disable-next-line max-len
-    document.querySelector('.nav__mobile').classList.remove('nav__mobile--active');
+    document.querySelector('.nav-responsive').classList.remove('nav-responsive--active');
   } else {
     this.classList.add('toggler--active');
     // eslint-disable-next-line max-len
-    document.querySelector('.nav__mobile').classList.add('nav__mobile--active');
+    document.querySelector('.nav-responsive').classList.add('nav-responsive--active');
   }
 });
