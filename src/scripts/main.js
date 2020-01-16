b07299daaa4c54b1bbe35b39f9302c9491a7c789
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-16 11:21:30 +0200

    slider button hover fixed, align left

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
-'use strict';
+/* eslint-disable no-shadow */
+// eslint-disable-next-line no-unused-vars
+/* global jQuery */
+/* eslint-env jquery */
+// eslint-disable-next-line strict
+$(document).ready(function() {
+  $('#nav').on('click', 'a', function(event) {
+    event.preventDefault();
+
+    const id = $(this).attr('href');
+    const top = $(id).offset().top;
+
+    $('body,html').animate({ scrollTop: top }, 1500);
+  });
+});
