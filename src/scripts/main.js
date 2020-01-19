dd500787585074ebe5c89a8587ddf659e438a461
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-19 20:50:57 +0200

    full done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -20,4 +20,41 @@ window.onload = function() {
     }
     activeHire.classList.toggle('hire__link--activ');
   });
+
+  const menuBtn = document.getElementsByClassName('header__gamb')[0];
+  const gamb = document.getElementsByClassName('nav-icon')[0];
+  const navMobile = document.getElementsByClassName('navMobile')[0];
+  const hederContent = document.getElementsByClassName('header__content')[0];
+
+  menuBtn.addEventListener('click', function() {
+    gamb.classList.toggle('open');
+    navMobile.classList.toggle('navMobile_none');
+    hederContent.classList.toggle('header__content_none');
+  });
+
+  window.onresize = function() {
+    gamb.classList.remove('open');
+    navMobile.classList.add('navMobile_none');
+    hederContent.classList.remove('header__content_none');
+  };
+
+  const activeMobileLink = document.getElementsByClassName('navMobile__link');
+  const actMobHire = document.getElementsByClassName('navMobile__hireLink')[0];
+
+  for (let i = 0; i < activeMobileLink.length; i++) {
+    activeMobileLink[i].addEventListener('click', function() {
+      for (let j = 0; j < activeMobileLink.length; j++) {
+        activeMobileLink[j].classList.remove('navMobile__link_activ');
+      }
+      actMobHire.classList.remove('navMobile__link_activ');
+      this.classList.toggle('navMobile__link_activ');
+    });
+  }
+
+  actMobHire.addEventListener('click', function() {
+    for (let j = 0; j < activeLink.length; j++) {
+      activeMobileLink[j].classList.remove('navMobile__link_activ');
+    }
+    actMobHire.classList.toggle('navMobile__link_activ');
+  });
 };
