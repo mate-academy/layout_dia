fe523cbf8a52e68bdb25c8ecddc56fa1e8edc4d2
Anna <anna.lyrynenko@gmail.com>
2020-01-20 11:01:16 +0200

    comments fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const slider = document.querySelectorAll('.slider .slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slider[currentSlide].className = 'slide';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'slide showing';
+}
+
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
