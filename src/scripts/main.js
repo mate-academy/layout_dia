b3e979b1f119b8818b3f0b70629e302c3aae51de
Actireon <eheieiei37@gmail.com>
2020-01-20 09:24:08 +0200

    adaptive added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const mobileBurger = document.querySelector('.nav__mobile-burger');
+const headerInfo = document.querySelector('.header__info');
+
+mobileBurger.addEventListener('click', function() {
+  mobileBurger.classList.toggle('nav__mobile-burger--active');
+  headerInfo.classList.toggle('header__info--dis');
+});
