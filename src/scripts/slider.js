9d1c15b0c300f0196c2b716e73f6f72df2e46a9b
MMashivskij <mmashivskij@gmail.com>
2020-01-29 18:09:59 +0200

    29/01/20

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,38 +1,44 @@
 'use strict';
 
-const container = document.querySelector('.container');
-const style = window.getComputedStyle(container, null);
-const width = parseInt(style.width);
-
 const slider = document.getElementById('slider');
 const list = slider.querySelector('ul');
 const listElems = slider.querySelectorAll('li');
 
 let position = 0;
 
-slider.querySelector('.slider__button_left').onclick = function() {
+const container = document.querySelector('.container');
+const style = window.getComputedStyle(container, null);
+const width = parseInt(style.width);
+
+const moveLeft = function() {
   position += width;
   if (position > 0) {
     position = -width * (listElems.length - 1);
-  } else {
-    position = Math.min(position, 0);
   }
   list.style.marginLeft = position + 'px';
 };
 
-slider.querySelector('.slider__button_right').onclick = function() {
+const moveRight = function() {
   position -= width;
   if (position < -width * (listElems.length - 1)) {
     position = 0;
-  } else {
-    position = Math.max(position, -width * (listElems.length - 1));
   }
   list.style.marginLeft = position + 'px';
 };
 
-setInterval(
-  slider
-    .querySelector('.slider__button_right')
-    .onclick,
-  4000
-);
+let autoSwitch = setInterval(moveRight, 4000);
+
+function count() {
+  clearInterval(autoSwitch);
+  setTimeout(() => { autoSwitch = setInterval(moveRight, 5000); }, 10000);
+}
+
+slider.querySelector('.slider__button_left').onclick = function() {
+  moveLeft();
+  count();
+};
+
+slider.querySelector('.slider__button_right').onclick = function() {
+  moveRight();
+  count();
+};
