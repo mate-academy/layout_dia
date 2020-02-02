7b40c54c51caeaef1b0597559bd135f5c36e5ab7
Arimazz <bodyaprymyak@icloud.com>
2020-02-02 23:19:50 +0200

    added responsiveness, outline and hover button effects fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars
+function myFunction() {
+  document.querySelector('.hamburger').classList.toggle('change');
+  document.querySelector('.main__info-block').classList.toggle('main__info-block--mobile');
+
+  const x = document.querySelector('.mobile-menu');
+
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
