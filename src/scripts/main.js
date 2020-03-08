f196fa3cd8f06010a6158d4d99a5205f4a43b6e1
Bohdan Salenik <bohdan.salenik@gmail.com>
2020-03-08 12:15:31 +0200

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -22,9 +22,9 @@ const next = document.querySelector('.slider__button--right');
 const previous = document.querySelector('.slider__button--left');
 
 const slides = [
-  'url(../images/slider/slide-img-1.png)',
-  'url(../images/slider/slide-img-2.png)',
-  'url(../images/slider/slide-img-3.png)',
+  'url(../images/slider/slide-img-1.jpg)',
+  'url(../images/slider/slide-img-2.jpg)',
+  'url(../images/slider/slide-img-3.jpg)',
 ];
 
 let currentSlide = 0;
