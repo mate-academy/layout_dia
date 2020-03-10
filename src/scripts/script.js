401d07cdf77ee561ba04717d7d86fb9a9f827c39
vladyslav223 <vladyslav223@gmail.com>
2020-03-10 17:43:34 +0200

    little changes1

---
+++ src/scripts/script.js
@@ -0,0 +1,15 @@
+'use strict';
+const anchors = document.querySelectorAll('a[href*="#"]');
+
+for (const anchor of anchors) {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const blockID = anchor.getAttribute('href').substr(1);
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
+}
