cdd7d6803e91c8c94d49dc8c4d8c7500af61eff0
Arimazz <bodyaprymyak@icloud.com>
2020-03-13 09:40:15 +0200

    added slider and hover added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -15,3 +15,43 @@ function myFunction() {
     x.style.display = 'block';
   }
 }
+
+let currentSlide = 1;
+
+const slider = document.querySelector('.slider');
+const nextButton = document.querySelector('.slider__arrow-box.right');
+const prevButton = document.querySelector('.slider__arrow-box.left');
+
+function setCurrentSlide() {
+  slider
+    .style
+    .backgroundImage = `url(../images/slider/slide-${currentSlide}.png)`;
+}
+
+setCurrentSlide();
+
+function nextSlide() {
+  currentSlide += 1;
+
+  if (currentSlide > 3) {
+    currentSlide = 1;
+  }
+  setCurrentSlide();
+}
+
+function prevSlide() {
+  currentSlide -= 1;
+
+  if (currentSlide < 1) {
+    currentSlide = 3;
+  }
+  setCurrentSlide();
+}
+
+nextButton.addEventListener('click', () => {
+  nextSlide();
+});
+
+prevButton.addEventListener('click', () => {
+  prevSlide();
+});
