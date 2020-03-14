f3cad67bb4cc1a491fc79f8b473840e5b781a10f
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-03-14 14:00:23 +0200

    Updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
-// 'use strict';
+'use strict';
 
-// console.log('hello mate academy!');
+const slider = document.querySelectorAll('.slider .slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slider[currentSlide].className = 'slide';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'slide showing';
+}
+
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
