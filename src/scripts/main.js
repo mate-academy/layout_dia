3f594b98d32af899e41a057b5d86ef6a3824b6d1
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-15 23:07:27 +0200

    Air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-console.log('hello mate academy!');
+const openMenu = () => {
+  const burger = document.querySelector('.burger');
+  const menu = document.querySelector('.menu');
+
+  burger.addEventListener('click', () => {
+    menu.classList.toggle('menu--opened');
+    burger.classList.toggle('burger--toggle');
+  });
+};
+
+openMenu();
