2cbfa5f093a18888e446dce6a04f21656940fef1
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-03-15 16:18:15 +0200

    were changed button behavior, slider, form action and few small changes

--- src/scripts/lib/main.js
+++ src/scripts/lib/main.js
@@ -1,1 +1,29 @@
+'use strict';
 
+const slider = document.querySelectorAll('.slider__list .slider__slide');
+let currentSlide = 0;
+
+function handleSliderButton(n) {
+  slider[currentSlide].className = 'slider__slide';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'slider__slide slider__slide--visible';
+}
+
+function nextSlide() {
+  handleSliderButton(currentSlide + 1);
+}
+
+function previousSlide() {
+  handleSliderButton(currentSlide - 1);
+}
+
+const next = document.querySelector('.slider__button--left');
+const previous = document.querySelector('.slider__button--right');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
