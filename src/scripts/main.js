26728b8d3d3f7d6bbb51df5dcdd6a294df2bb0e3
Stanislav Mochyr <delenjer@gmail.com>
2020-03-15 13:24:13 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -69,3 +69,31 @@ function isPartiallyVisible(el) {
 
   return ((topTo + height >= 0) && (height + window.innerHeight >= bottom));
 }
+
+const slider = document.querySelectorAll('.slider .slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slider[currentSlide].className = 'slide';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'slide showing';
+}
+
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
