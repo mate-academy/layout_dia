59fe409a100d3e98374dea1710be62d9994e044f
dima <tynindima@gmail.com>
2020-03-16 17:42:57 +0200

    reset form 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// const form = document.querySelector('.footer__form');
-//
-// form.addEventListener('submit', (ev) => {
-//   ev.preventDefault();
-// });
+const form = document.querySelector('.footer__form');
+
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+});
