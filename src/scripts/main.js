b460eb65fe4cbb3780ef06438eda5df739303505
Yuliya312 <yu312liya@gmail.com>
2020-03-17 00:01:11 +0200

    add email validation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,17 @@
 'use strict';
 
-// console.log('hello mate academy!');
+const footerForm = document.querySelector('.footer__form');
+const email = document.querySelector('#email');
+
+footerForm.addEventListener('submit', (ev) => {
+  ev.target.reset();
+  ev.preventDefault();
+});
+
+email.addEventListener('input', function() {
+  if (email.validity.typeMismatch) {
+    email.setCustomValidity('An e-mail is expected');
+  } else {
+    email.setCustomValidity('');
+  }
+});
