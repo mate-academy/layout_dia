e0876806d7df1bf95c5408f8bb9ea4214f733d0d
Natalia Lypovyk <cardinnata@gmail.com>
2020-03-16 22:42:49 +0200

    slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,3 +11,31 @@ const openMenu = () => {
 };
 
 openMenu();
+
+const slider = document.querySelectorAll('.slider .slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slider[currentSlide].className = 'slide';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'slide slide--active';
+}
+
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
