5e78dd91de69b4f068cddf61eaf3e87aeba2bcab
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-03-16 15:06:36 +0200

    add carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// console.log('hello mate academy!');
+const slider = document.querySelectorAll('.carousel .carousel-item');
+let currentSlide = 0;
+
+function nextSlide() {
+  goToSlide(currentSlide + 1);
+}
+
+function previousSlide() {
+  goToSlide(currentSlide - 1);
+}
+
+function goToSlide(n) {
+  slider[currentSlide].className = 'carousel-item';
+  currentSlide = (n + slider.length) % slider.length;
+  slider[currentSlide].className = 'carousel-item showing';
+}
+
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
+
+next.onclick = function() {
+  nextSlide();
+};
+
+previous.onclick = function() {
+  previousSlide();
+};
