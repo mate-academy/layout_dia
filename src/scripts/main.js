dffd64a9ee723f12d68fc7b61c74ecb2795ad46b
WincherZ <wincher2016@gmail.com>
2020-03-17 17:44:42 +0200

    dia-header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const func = () => {
+  const header = document.querySelector('.header');
+  const navbar = document.querySelector('.header-navbar__list');
+
+  header.addEventListener('click', e => {
+    const spsMaxLineLength = navbar.style.transform;
+    const targ = e.target.classList;
+
+    if (targ.contains('ham')) {
+      if (spsMaxLineLength === 'translateY(-100%)' || spsMaxLineLength === '') {
+        navbar.style.transform = 'translateY(0)';
+      } else {
+        navbar.style.transform = 'translateY(-100%)';
+      };
+    };
+  });
+};
+
+func();
