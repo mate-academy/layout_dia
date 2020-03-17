6bbac205d79ccbb90c4a4b133cc65f8c5220c809
BorisovecViktor <viktorborisovets@gmail.com>
2020-03-17 16:34:50 +0200

    added header respons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const $btn = document.getElementById('show');
+const $nav = document.getElementById('nav');
+
+$btn.addEventListener('click', function() {
+  $nav.classList.toggle('active');
+});
