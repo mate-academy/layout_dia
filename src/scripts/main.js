53215263ba0616d42a07966bdf1c5e9c08534318
Oleksii Mykhailov <alem.coding@gmail.com>
2020-03-17 07:30:33 +0200

    Added header and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const navBlock = document.querySelector('#header__nav');
+const navList = document.querySelector('#nav__list');
+const navLinks = document.querySelectorAll('.nav__link');
+const btnMenu = document.querySelector('#header__nav-button');
+
+btnMenu.addEventListener('click', () => {
+  navBlock.classList.toggle('header__nav--show');
+  navList.classList.toggle('nav__list--mobile');
+  btnMenu.classList.toggle('header__nav-button--close');
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navBlock.classList.remove('header__nav--show');
+    btnMenu.classList.remove('header__nav-button--close');
+  });
+});
