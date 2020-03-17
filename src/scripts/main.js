058b68336cd039a93a5f839d0405c207187ca290
crash2bash <sachagurov@gmail.com>
2020-03-17 18:44:43 +0200

    section 1-3 done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const sandwich = document.querySelector('.navigation__sandwich');
+const closeMenu = document.querySelector('.navigation__close');
+const mobileMenu = document.querySelector('.navigation__menu-wrapper');
+
+sandwich.addEventListener('click', function() {
+  mobileMenu.classList.remove('hide');
+  mobileMenu.classList.add('show');
+});
+
+closeMenu.addEventListener('click', function() {
+  mobileMenu.classList.remove('show');
+  mobileMenu.classList.add('hide');
+});
