51e769481e4193a90545f6054aea9fa918d4e700
crash2bash <sachagurov@gmail.com>
2020-03-18 21:09:57 +0200

    add smooth scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const sandwich = document.querySelector('.navigation__sandwich');
 const closeMenu = document.querySelector('.navigation__close');
 const mobileMenu = document.querySelector('.navigation__menu-wrapper');
+const upButton = document.querySelector('.up-arrow');
+const links = document.querySelectorAll('.navigation__link');
 
 sandwich.addEventListener('click', function() {
   mobileMenu.classList.remove('hide');
@@ -12,4 +14,32 @@ sandwich.addEventListener('click', function() {
 closeMenu.addEventListener('click', function() {
   mobileMenu.classList.remove('show');
   mobileMenu.classList.add('hide');
+});
+
+window.onscroll = function() {
+  if (window.pageYOffset > 1500) {
+    upButton.classList.add('show');
+  } else {
+    upButton.classList.remove('show');
+  }
+};
+
+upButton.addEventListener('click', function() {
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
+});
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const id = link.getAttribute('href').substr(1);
+
+    document.getElementById(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
+  });
 });
