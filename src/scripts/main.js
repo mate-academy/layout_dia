35611d8e111aad35f11837fb3809863093059252
BorisovecViktor <viktorborisovets@gmail.com>
2020-03-20 15:53:06 +0200

    added all blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,14 @@
 
 const $btn = document.getElementById('show');
 const $nav = document.getElementById('nav');
+const $link = document.querySelector('.nav__list');
 
 $btn.addEventListener('click', function() {
   $nav.classList.toggle('active');
   $btn.classList.toggle('open');
+});
+
+$link.addEventListener('click', function() {
+  $nav.classList.remove('active');
+  $btn.classList.remove('open');
 });
