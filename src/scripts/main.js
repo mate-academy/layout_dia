70164110bf78579018273655517d18e418d8203f
BorisovecViktor <viktorborisovets@gmail.com>
2020-03-21 12:38:25 +0200

    added animations

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,7 @@
 const $btn = document.getElementById('show');
 const $nav = document.getElementById('nav');
 const $link = document.querySelector('.nav__list');
+const $scrollButton = document.querySelector('.scroll-button');
 
 $btn.addEventListener('click', function() {
   $nav.classList.toggle('active');
@@ -12,4 +13,29 @@ $btn.addEventListener('click', function() {
 $link.addEventListener('click', function() {
   $nav.classList.remove('active');
   $btn.classList.remove('open');
+});
+
+// $(document).ready(function(){
+//   $(window).scroll(function () {
+//     if ($(this).scrollTop() > 5000) {
+//       $('#header').css('visibility', 'hidden');
+//     } else {
+//       $('#header').css('visibility', 'visible');
+//     }
+//   });
+// });
+
+window.onscroll = function() {
+  if (window.pageYOffset > 700) {
+    $scrollButton.classList.add('show');
+  } else {
+    $scrollButton.classList.remove('show');
+  }
+};
+
+$scrollButton.addEventListener('click', function() {
+  document.documentElement.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
 });
