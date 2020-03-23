6dbbc5581b1d0666f4252443c5b8649c9ebdb37c
romarios1987 <romios047@gmail.com>
2020-03-23 10:01:42 +0200

    added other sections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+window.addEventListener('scroll', () => {
+  const headerTop = document.querySelector('.header__top');
+
+  headerTop.classList.toggle('sticky', window.scrollY > 0);
+});
+
+const hamburger = document.querySelector('.hamburger');
+const navList = document.querySelector('.header__nav');
+
+const links = document.querySelectorAll('.header__nav .nav__list .nav__item');
+
+hamburger.addEventListener('click', () => {
+  navList.classList.toggle('open');
+
+  links.forEach(link => {
+    link.classList.toggle('fade');
+  });
+});
+
 // Slider
 let slideIndex = 1;
 const slides = document.querySelectorAll('.slide');
