ba34857bf9c9f4acc47297be0c31a1fbcde7e8f7
Oleg Obiukh 2020 <oleg.obiuh.subscribe@gmail.com>
2020-03-26 15:36:44 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burgerMenu = document.getElementById('menu__toggle');
+const links = document.getElementsByClassName('nav__item');
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', () => {
+    burgerMenu.checked = false;
+  });
+}
