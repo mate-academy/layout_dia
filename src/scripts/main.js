6cc750c2c8d37fc56a9f2bdbeaa46ad118fba617
Valentyn Krushynin <krushunin89@gmail.com>
2020-05-15 17:51:48 +0300

    desctop done, add some animation, without mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const buttonSlogan = document.querySelector('.slogan__button');
+const rectangles = document.querySelectorAll('.rectangle');
+
+function rotateRectangles() {
+  for (let i = 0; i < rectangles.length; i++) {
+    rectangles[i].classList.toggle('rectangle--is-active');
+  }
+}
+
+buttonSlogan.onmouseover = rotateRectangles;
+
+buttonSlogan.addEventListener('onmouseover', (e) => {
+  if (e.target === buttonSlogan) {
+    rotateRectangles();
+  }
+});
