cced7234c345bf7b7a5c16efd189a1f35df38186
Valentyn Krushynin <krushunin89@gmail.com>
2020-05-16 18:01:20 +0300

    add toggle menu, add anker link

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,22 @@ buttonSlogan.addEventListener('onmouseover', (e) => {
     rotateRectangles();
   }
 });
+
+const burger = document.querySelector('.header__toggle--toggler');
+const closeMenu = document.querySelector('.nav__toggle--close');
+const menu = document.querySelector('.nav');
+
+burger.onclick = classToggle;
+
+function classToggle() {
+  menu.classList.toggle('nav--is-active');
+};
+
+menu.addEventListener('click', function(e) {
+  if (e.target === closeMenu
+    || e.target.classList.contains('nav__link')) {
+    classToggle();
+  }
+});
+
+// || e.target.classList.contains('nav-toggle__link')
