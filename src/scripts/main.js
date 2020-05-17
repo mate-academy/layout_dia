a249809cd0e2a90760e04ad88003ca75c989703d
YanG-6668 <goch.yannn@gmail.com>
2020-05-17 17:51:40 +0300

    dia 1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+$(document).ready(function(){
+  $('.slider').slick({
+    speed: 1000,
+    autoplay: 2000,
+    fade: true
+  });
+});
+
+$(document).ready(function() {
+  $('a[href^="#"]').click(function() {
+    let target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 1500);
+  });
+});
+
+$(".header__burger, .mobile__close_icon").click(function() {
+  $('.mobile').slideToggle(500);
+});
