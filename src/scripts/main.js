0c7120dfc7b3f119cab1b01f39389f91f9a468ac
denkondratiev <denys.kondratiev.job@gmail.com>
2020-05-17 03:12:45 +0300

    first

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,63 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
 'use strict';
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  const slides = document.getElementsByClassName('gallery__item');
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex - 1].style.display = 'block';
+}
+
+$('.form').submit(function() {
+  $('.success').addClass('success--active').hide().fadeIn();
+
+  setTimeout(function() {
+    $('.success').removeClass('success--active').fadeOut();
+    $('.form').trigger('reset');
+  }, 3000);
+
+  return false;
+});
+
+$('.nav__burger').click(function() {
+  $(this).toggleClass('nav__burger--open');
+  $('.nav__mobile').toggleClass('nav__mobile--open');
+
+  return false;
+});
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > $(window).height()) {
+    $('.top').addClass('top--active');
+  } else {
+    $('.top').removeClass('top--active');
+  }
+});
+
+$('.top').click(function() {
+  $('html, body').stop().animate({ scrollTop: 0 }, 'slow', 'swing');
+});
