634978c0ea41abf185d96d9c8ffe8c1ac1ff747f
Glib Rulev <velzepooz@gmail.com>
2020-05-18 20:31:57 +0300

    First version without slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const nav = document.querySelector('.mob-nav');
+const burger = document.querySelector('.header__burger');
+const closing = nav.querySelector('.mob-nav__close');
+
+burger.onclick = function() {
+  toggleMenu();
+};
+
+nav.addEventListener('click', function(e) {
+  if (e.target === closing || e.target.classList.contains('mob-nav__link')) {
+    toggleMenu(0);
+  }
+});
+
+function toggleMenu(val = 1) {
+  nav.style.opacity = val;
+
+  val ? nav.style.zIndex = 1
+    : nav.addEventListener('transitionend', function toggleZIndex() {
+      nav.style.zIndex = -1;
+      nav.removeEventListener('transitionend', toggleZIndex);
+    });
+}
