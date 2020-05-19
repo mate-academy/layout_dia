89da594c3149fe5dc2250e8342ff7d3b869c167b
Glib Rulev <velzepooz@gmail.com>
2020-05-19 11:54:17 +0300

    New slider

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,20 +1,23 @@
 'use strict';
 
 const slider = document.querySelector('.slider');
-const images = [
-  'url(./images/slider/slide-img-1.png)',
-  'url(./images/slider/slide-img-2.png)',
-  'url(./images/slider/slide-img-3.png)'];
+const gallery = slider.querySelector('.slider__gallery');
 let index = 0;
 
 slider.querySelector('.slider__arrow-left').onclick = function() {
-  if (index > 0) {
-    slider.style.backgroundImage = images[--index];
-  };
+  --index;
+
+  if (index < 0) {
+    index = gallery.children.length - 1;
+  }
+  gallery.style.transform = `translateX(${-index * 100}%)`;
 };
 
 slider.querySelector('.slider__arrow-right').onclick = function() {
-  if (index < images.length - 1) {
-    slider.style.backgroundImage = images[++index];
-  };
+  ++index;
+
+  if (index > gallery.children.length - 1) {
+    index = 0;
+  }
+  gallery.style.transform = `translateX(${-index * 100}%)`;
 };
