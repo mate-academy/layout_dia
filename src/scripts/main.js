47ae30bc9de9b631796fc755880a91e43551ad24
nazmeln <nazik100378@gmail.com>
2020-05-19 21:15:14 +0300

    added mob-menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,4 +3,7 @@
 document.querySelector('.menu-icon').onclick = function() {
   document.querySelector('.menu-icon-line')
     .classList.toggle('menu-icon-active');
+
+  document.querySelector('.mob__nav')
+    .classList.toggle('mob__slide');
 };
