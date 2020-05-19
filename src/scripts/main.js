74258e78a355c6585caabb13030d24faf63258c4
Yura <wa7er14@gmail.com>
2020-05-19 13:49:39 +0300

    no menu animation & favicon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+/*eslint-disable */
+const content = document.getElementById("content");
+let curSlide = 1;
+
+function leftScroll() {
+  const width = document.getElementsByClassName("carousel__slide")[0].width;
+  if(curSlide > 1) {
+    curSlide--;
+    content.style.transform = `translateX(${-width * (curSlide - 1)}px)`;
+  }
+}
+
+function rightScroll() {
+  const width = document.getElementsByClassName("carousel__slide")[0].width;
+  if(curSlide < document.getElementsByClassName("carousel__slide").length) {
+    content.style.transform = `translateX(${-width * (curSlide)}px)`;
+    curSlide++;
+  }
+}
+
+function burger() {
+  const elem = document.getElementById('home');
+
+  elem.classList.toggle('header--active');
+}
+
+function onSubmit() {
+  event.preventDefault();
+  document.querySelector('input[name=email]').value = '';
+  document.querySelector('input[name=name]').value = '';
+  document.querySelector('input[name=message]').value = '';
+}
