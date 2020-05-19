4b2e7158c41a93cd8d3fe0c339e566377f58812a
Yaroslav Andriyenko <andriyenko.yaroslav@gmail.com>
2020-05-19 22:56:18 +0300

    v1.0 First version, without some styling elements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
+/* eslint-disable no-unused-vars */
 'use strict';
+
+let slideIndex = 1;
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName('slider__img');
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex - 1].style.display = 'block';
+}
