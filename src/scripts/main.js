8461268cd79ef10ff08e5d497339dac435d3bc4a
Vovasef <vovasef@gmail.com>
2020-05-19 15:42:27 +0300

    v. 0.9.7 slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const images = [
+  '../images/slider/slide-img-1.jpg',
+  '../images/slider/slide-img-2.jpg',
+  '../images/slider/slide-img-3.jpg',
+];
+let nextimage = 0;
+
+doSlideshow();
+
+function doSlideshow() {
+  if (nextimage >= images.length) {
+    nextimage = 0;
+  } else if (nextimage < 0) {
+    nextimage = images.length - 1;
+  }
+
+  $('.gallery__slider')
+    .css('background-image', 'url(' + images[nextimage++] + ')');
+}
+
+$('.gallery__button-r').click(function() {
+  if (nextimage >= images.length) {
+    nextimage = 0;
+  } else if (nextimage < 0) {
+    nextimage = images.length - 1;
+  }
+  $('.gallery__slider')
+    .css('background-image', 'url(' + images[nextimage++] + ')');
+});
+
+$('.gallery__button-l').click(function() {
+  if (nextimage >= images.length) {
+    nextimage = 0;
+  } else if (nextimage < 0) {
+    nextimage = images.length - 1;
+  }
+  $('.gallery__slider')
+    .css('background-image', 'url(' + images[nextimage--] + ')');
+});
+
