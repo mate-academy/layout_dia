8d50ff5af5eab68c315cc12bf7d8bfcc6e636d0c
VlaDinter <vladyslavdinter@gmail.com>
2020-05-20 23:50:54 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const menu = document.querySelectorAll('.nav');
+const button = document.querySelectorAll('.nav__menu-toggle');
+
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', () => {
+    menu[i].classList.toggle('nav--menu-open');
+    menu[i].classList.toggle('nav--menu-close');
+  });
+}
