c9fb9aae0f02cd3499c931b2c1a35dc7d324ef8a
Yevhenii <DrozhzhynY>
2020-06-08 12:23:35 +0300

    added slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.slider__arrow-left');
+const nextButton = document.querySelector('.slider__arrow-right');
+const countItems = document.querySelectorAll('.slider__item').length;
+const mainLength = 687;
+let transformLength = 0;
+
+prevButton.addEventListener('click', prevButtonClick);
+nextButton.addEventListener('click', nextButtonClick);
+
+function prevButtonClick(e) {
+  if (transformLength === 0) {
+    transformLength = -(countItems - 1) * mainLength;
+    slider.style = `transform: translateX(${transformLength}px);`;
+  } else {
+    slider.style = `transform:
+    translateX(calc(${transformLength}px + ${mainLength}px));`;
+    transformLength += mainLength;
+  }
+}
+
+function nextButtonClick(e) {
+  if (transformLength < -(countItems * mainLength) / 2) {
+    transformLength = 0;
+
+    slider.style = `transform:
+      translateX(calc(${transformLength}px);`;
+  } else {
+    slider.style = `transform:
+      translateX(calc(${transformLength}px - ${mainLength}px));`;
+    transformLength -= mainLength;
+  }
+}
