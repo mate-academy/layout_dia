3ad817489b95ffe0a4d8daee2b0ab23ce61c8682
Slava Polishchuk <dizerllc@gmail.com>
2020-06-15 01:49:39 +0300

    solution with mobile menu js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const hamburger = document.querySelector('.mobile__hamburger');
+const navLinks = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links li');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+
+  links.forEach(link => {
+    link.classList.toggle('fade');
+  });
+});
