98c51623b579f99d2f3a5c7bce3bcefb306f0c02
VlaDinter <vladyslavdinter@gmail.com>
2020-06-18 18:32:45 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,45 @@
 'use strict';
 
-const menu = document.querySelectorAll('.nav');
-const button = document.querySelectorAll('.nav__menu-toggle');
+let isFirstClickHeader = true;
+let isFirstClickFooter = true;
+const header = document.querySelector('.header');
+const footer = document.querySelector('.footer');
+const headerButton = document.querySelector('.header__menu-button');
+const footerButton = document.querySelector('.footer__menu-button');
+const anchorItems = document.querySelectorAll('a[href*="#"]');
 
-for (let i = 0; i < button.length; i++) {
-  button[i].addEventListener('click', () => {
-    menu[i].classList.toggle('nav--menu-open');
-    menu[i].classList.toggle('nav--menu-close');
+footer.classList.remove('footer--nojs');
+header.classList.remove('header--nojs');
+
+headerButton.addEventListener('click', () => {
+  if (isFirstClickHeader) {
+    header.classList.add('header--menu-open');
+    isFirstClickHeader = false;
+  } else {
+    header.classList.toggle('header--menu-open');
+    header.classList.toggle('header--menu-close');
+  }
+});
+
+footerButton.addEventListener('click', () => {
+  if (isFirstClickFooter) {
+    footer.classList.add('footer--menu-open');
+    isFirstClickFooter = false;
+  } else {
+    footer.classList.toggle('footer--menu-open');
+    footer.classList.toggle('footer--menu-close');
+  }
+});
+
+for (const item of anchorItems) {
+  item.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const id = item.getAttribute('href');
+
+    document.querySelector(id).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start',
+    });
   });
 }
