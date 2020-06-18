4c0a17b25a8a41c1da4a90ebe9c1c947e32e6caa
solaryasha <solaryasha@gmail.com>
2020-06-19 00:25:48 +0300

    Add localStorage check for theme

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const lightMode = localStorage.getItem('theme--blue');
 const toggle = document.querySelector('.toggle');
+
+if (lightMode === 'enabled') {
+  document.body.classList.remove('theme--black');
+  document.body.classList.add('theme--blue');
+} else {
+  document.body.classList.remove('theme--blue');
+  document.body.classList.add('theme--black');
+}
 
 toggle.addEventListener('click', () => {
   if (document.body.classList.value === 'theme--black') {
     document.body.classList.remove('theme--black');
     document.body.classList.add('theme--blue');
+    localStorage.setItem('theme--blue', 'enabled');
+    localStorage.setItem('theme--black', null);
+
     toggle.classList.remove('toggle--sun');
     toggle.classList.add('toggle--moon');
   } else {
@@ -13,5 +25,7 @@ toggle.addEventListener('click', () => {
     document.body.classList.add('theme--black');
     toggle.classList.remove('toggle--moon');
     toggle.classList.add('toggle--sun');
+    localStorage.setItem('theme--blue', null);
+    localStorage.setItem('theme--black', 'enabled');
   }
 });
