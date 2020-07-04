fda3918b22250b978b39eee3f8f006abfbe37bd0
nazmeln <nazik100378@gmail.com>
2020-07-04 03:58:22 +0300

    added slider

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 - [DEMO LINK](https://nazmeln.github.io/layout_dia/)
 
+- Large screens 2560px
+- Full HD 1920px
+- The design 1600px
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,3 +19,37 @@ buttonVision.addEventListener('click', () => (
 buttonDescription.addEventListener('click', () => (
   window.location = '#description'
 ));
+
+//  slider
+const slider = document.querySelector('.slider');
+const photo = document.querySelector('.slider__photo');
+const photos = document.querySelectorAll('.slider__photo');
+const photoWidth = photo.offsetWidth;
+
+const nextButton = document.querySelector('#photo__button-right');
+const previousButton = document.querySelector('#photo__button-left');
+
+let position = 0;
+
+nextButton.addEventListener('click', nextPhoto);
+previousButton.addEventListener('click', prevPhoto);
+
+function nextPhoto() {
+  if (position <= -((photos.length - 1) * photoWidth)) {
+    return;
+  }
+
+  position -= photoWidth;
+
+  slider.style.transform = `translateX(${position}px)`;
+}
+
+function prevPhoto() {
+  if (position >= 0) {
+    return;
+  }
+
+  position += photoWidth;
+
+  slider.style.transform = `translateX(${position}px)`;
+}
