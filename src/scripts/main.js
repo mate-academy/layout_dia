c8fa52d3b3849bf27bc0b18f3406dee88778966e
nazmeln <nazik100378@gmail.com>
2020-07-09 15:19:30 +0300

    added scroll-animation

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -71,3 +71,35 @@ function onSubmit() {
 const submitButton = document.querySelector('.contact__form');
 
 submitButton.addEventListener('submit', onSubmit);
+
+// animationScroll
+
+function animation() {
+  let elements;
+  let windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i];
+      const positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+};
+
+animation();
