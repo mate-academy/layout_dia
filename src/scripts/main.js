d9601105fc015f057c27adca528b3110ad9144c8
DimaDP <dimadp95@gmail.com>
2020-07-11 22:56:01 +0300

    some fixes with fonts

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const submitButton = document.getElementById('submitButton');
+
+function onSubmit() {
+  document.getElementById('email').value = '';
+  document.getElementById('name').value = '';
+  document.getElementById('message').value = '';
+};
+
+submitButton.addEventListener('onsubmit', onSubmit);
