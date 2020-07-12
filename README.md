b3687ebe36fc37d1ee695cab8e98efa35ad341b5
German <germandolia@gmail.com>
2020-07-12 14:53:37 +0300

    fixes

--- README.md
+++ README.md
@@ -14,3 +14,4 @@
 - CSS3
 - BEM
 - Sass
+- JavaScript
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// eslint-disable-next-line no-unused-vars
+function onSubmit() {
+  event.preventDefault();
+
+  document.querySelector('input[name=email]').value = '';
+  document.querySelector('input[name=name]').value = '';
+  document.querySelector('input[name=message]').value = '';
+}
