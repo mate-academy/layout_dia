3a4c10edbaa184241080e6d9ac3aae1086065ec4
Slava Polishchuk <dizerllc@gmail.com>
2020-07-13 23:58:38 +0300

    make build

---
+++ dist/scripts/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const hamburger = document.querySelector('.mobile__hamburger');
+const navLinks = document.querySelector('.mobile__navigation');
+const links = document.querySelectorAll('.mobile-navigation__item');
+
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+
+  links.forEach(link => {
+    link.classList.toggle('appear');
+  });
+});
