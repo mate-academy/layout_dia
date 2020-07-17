daa75a77cac1b9ac9d643c59827910e7a4e6ae0d
Dennis Priadka <dennis.priadka@gmail.com>
2020-07-17 22:20:47 +0300

    changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+(function sliding() {
+  const slider = document.querySelector('.slider');
+  const radios = [
+    document.getElementById(`slider__radio--1`),
+    document.getElementById(`slider__radio--2`),
+    document.getElementById(`slider__radio--3`),
+    document.getElementById(`slider__radio--4`),
+  ];
+
+  const timer = setInterval(() => {
+    for (const i in radios) {
+      if (radios[i].checked) {
+        if (radios[i] === radios[radios.length - 1]) {
+          radios[0].checked = true;
+        } else {
+          radios[+i + 1].checked = true;
+        }
+        break;
+      }
+    }
+  }, 10000);
+
+  slider.onmouseenter = () => {
+    clearInterval(timer);
+    slider.onmouseleave = sliding;
+  };
+})();
