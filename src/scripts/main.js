8fd47516bc78127c624de2bfd0ad6a2eac36a0cf
DmJSCourse <dimonua@gmail.com>
2020-07-25 21:26:30 +0300

    slider done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
-'use strict';
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("header__slide");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+		console.log (slides[i], slides[i].style);
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
