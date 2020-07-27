a09f3c19bc3d76ef6f80cafdf2d8d27072105b8f
DmJSCourse <dimonua@gmail.com>
2020-07-27 13:13:54 +0300

    images fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,7 +14,6 @@ function showSlides(n) {
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-		console.log (slides[i], slides[i].style);
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
