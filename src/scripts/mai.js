3f20b866c3bf544302bec14650f867a5c1904d09
Tatiana Ivanchenko <tanyarz1996@gmail.com>
2020-09-11 12:56:22 +0300

    solution

--- src/scripts/mai.js
+++ src/scripts/mai.js
@@ -1,22 +1,18 @@
 // eslint-disable-next-line strict
-let btnRight = document.querySelector(".header__button");
-let slides = document.querySelectorAll(".header__img");
+const btnRight = document.querySelector('.header__button');
+const slides = document.querySelectorAll('.header__img');
 
 let i = 0;
 
-btnRight.addEventListener("click", function () {
-
-  ++i
+btnRight.addEventListener('click', function() {
+  ++i;
 
   if (i >= slides.length) {
-    slides[i - 1].classList.remove("header__img--active");
-
+    slides[i - 1].classList.remove('header__img--active');
     i = 0;
-    slides[i].classList.add("header__img--active");
-
+    slides[i].classList.add('header__img--active');
   } else {
-    slides[i - 1].classList.remove("header__img--active");
-    slides[i].classList.add("header__img--active");
+    slides[i - 1].classList.remove('header__img--active');
+    slides[i].classList.add('header__img--active');
   }
 });
-
