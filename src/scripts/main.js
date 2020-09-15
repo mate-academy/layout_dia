a6ad3ec0ec33f26cfd0edf491cdbb6674d0558f3
Yazheviks <vicsergeyeva@gmail.com>
2020-09-15 20:18:48 +0300

    burger menu alive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,44 @@
 'use strict';
+
+const burger = document.querySelector('.burger');
+const burgerItem = document.querySelectorAll('.burger__item');
+const promo = document.querySelector('.promo');
+const title = document.querySelector('.promo__title');
+const menu = document.querySelector('.nav__list');
+const navItem = document.querySelectorAll('.nav__item');
+const navLink = document.querySelectorAll('.nav__link');
+const headerLink = document.querySelector('.header__link');
+
+burger.addEventListener('click', () => {
+  menuToggle();
+});
+
+navLink.forEach(item => {
+  item.addEventListener('click', () => {
+    menuToggle();
+  });
+});
+
+headerLink.addEventListener('click', () => {
+  menuToggle();
+});
+
+function menuToggle() {
+  burgerItem.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  promo.classList.toggle('menu');
+  title.classList.toggle('menu');
+  menu.classList.toggle('menu');
+
+  navItem.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  navLink.forEach(item => {
+    item.classList.toggle('menu');
+  });
+
+  headerLink.classList.toggle('menu');
+}
