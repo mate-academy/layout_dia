af4d3ebf38ef1b04531977786e003a10aa4661c3
Alona <aionovaionova@gmail.com>
2020-09-16 19:07:45 +0300

    added adaptive menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger');
+
+const mobileNav = document.querySelector('.mobile');
+
+burger.addEventListener('click', function() {
+  mobileNav.classList.toggle('mobile--active');
+  burger.classList.toggle('nav__burger--active');
+});
