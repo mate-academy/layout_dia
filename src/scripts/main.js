4f5be97b2de812148b2df132b519621ed1286ba4
Alona <aionovaionova@gmail.com>
2020-09-17 15:09:51 +0300

    fixed mobile menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,7 +4,16 @@ const burger = document.querySelector('.nav__burger');
 
 const mobileNav = document.querySelector('.mobile');
 
+const mobileLink = document.querySelectorAll('.mobile__link');
+
 burger.addEventListener('click', function() {
   mobileNav.classList.toggle('mobile--active');
   burger.classList.toggle('nav__burger--active');
 });
+
+for (let elem of mobileLink) {
+  elem.addEventListener('click', function() {
+    mobileNav.classList.toggle('mobile--active');
+    burger.classList.toggle('nav__burger--active');
+  })
+}
