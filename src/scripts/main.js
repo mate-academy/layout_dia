afc302d0be3d68c39f162a3ea71de2bc3486ffeb
VoduDay <andriy.maniak@gmail.com>
2020-09-19 16:06:15 +0300

    fixed burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(item => {
+  item.addEventListener('click', () => {
+    hideMenu();
+  });
+});
+
+function hideMenu() {
+  document.getElementById('toggler').checked = false;
+}
