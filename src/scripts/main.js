862a8a295f9594d5d326bb2a4ecbfba44ade3878
Petro <petdavy@1991gmail.com>
2020-09-19 18:05:53 +0300

    replace js decor setting

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,35 +1,5 @@
 'use strict';
 
-// -------------start set up------------
-const decorRectLeftPos = [ [ 82, 248 ],
-  [ 131, 72 ],
-  [ 233, 527 ],
-  [ 269, 254 ],
-  [ 446, 565 ],
-  [ 481, 177 ] ];
-
-const decorRectRightPos = [ [570, 128],
-  [ 351, 213 ],
-  [ 271, 406 ],
-  [ 199, 112 ],
-  [ 148, 523 ],
-  [ 95, 321 ] ];
-
-const decorLeftRects = document.querySelector('.mottos__decor--left-side')
-  .querySelectorAll('.decor__rect');
-const decorRightRects = document.querySelector('.mottos__decor--right-side')
-  .querySelectorAll('.decor__rect');
-
-decorLeftRects.forEach((rect, i) => {
-  rect.style.left = decorRectLeftPos[i][0] + 'px';
-  rect.style.bottom = decorRectLeftPos[i][1] + 'px';
-});
-
-decorRightRects.forEach((rect, i) => {
-  rect.style.right = decorRectRightPos[i][0] + 'px';
-  rect.style.bottom = decorRectRightPos[i][1] + 'px';
-});
-
 // --------------burger-menu---------------
 
 const page = document.querySelector('.page');
