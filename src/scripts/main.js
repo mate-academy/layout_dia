ca0704f388e9e195e016a7ae2ec7dc28b9ba9ed7
unknown <yurastt1@gmail.com>
2020-09-20 21:20:28 +0300

    test

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger');
+
+const mobileNav = document.querySelector('.mobile');
+
+const mobileLink = document.querySelectorAll('.mobile__link');
+
+burger.addEventListener('click', function() {
+  mobileNav.classList.toggle('mobile--active');
+  burger.classList.toggle('nav__burger--active');
+});
+
+for (const elem of mobileLink) {
+  elem.addEventListener('click', function() {
+    mobileNav.classList.toggle('mobile--active');
+    burger.classList.toggle('nav__burger--active');
+  })
+};
