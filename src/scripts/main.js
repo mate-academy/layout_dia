89c27365a9ff49c89f518d02e2c3b197d56bbd01
Alona <aionovaionova@gmail.com>
2020-09-21 22:15:09 +0300

    changed the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,6 +6,9 @@ const mobileNav = document.querySelector('.mobile');
 
 const mobileLink = document.querySelectorAll('.mobile__link');
 
+const form = document.querySelector('.form');
+
+
 burger.addEventListener('click', function() {
   mobileNav.classList.toggle('mobile--active');
   burger.classList.toggle('nav__burger--active');
@@ -17,3 +20,9 @@ for (const elem of mobileLink) {
     burger.classList.toggle('nav__burger--active');
   })
 };
+
+form.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
+
