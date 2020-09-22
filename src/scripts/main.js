520eb2abee5e5caf60f84d7fcf057c8829a73471
KirillLutsenko <mnrovski68@gmail.com>
2020-09-22 16:39:59 +0300

    add script for burger & forms

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const burger = document.querySelector('.nav__burger');
+
+const mobileNav = document.querySelector('.burger');
+
+const mobileLink = document.querySelectorAll('.burger__link');
+
+const form = document.querySelector('.form');
+
+
+burger.addEventListener('click', function() {
+  mobileNav.classList.toggle('burger--active');
+  burger.classList.toggle('nav__burger--active');
+});
+
+for (const elem of mobileLink) {
+  elem.addEventListener('click', function() {
+    mobileNav.classList.toggle('burger--active');
+    burger.classList.toggle('nav__burger--active');
+  })
+};
+
+form.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
