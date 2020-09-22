e0c6d8563ca81406c223f70fabd6b4903e9c51e4
Evgenii <voznukzhenia@gmail.com>
2020-09-22 12:08:14 +0300

    for review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const nav = document.querySelector('.header__nav');
+const burger = document.querySelector('.burger');
+const navItem = document.querySelectorAll('.nav__item');
+
+burger.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  nav.classList.toggle('active');
+  burger.classList.toggle('active');
+  logo.classList.toggle('active');
+}
+
+navItem.forEach(element => {
+  element.addEventListener('click', toggleMenu);
+});
