b2b090bf6b6abdd827d464534d03aba8eb4f9559
KirillLutsenko <mnrovski68@gmail.com>
2020-09-24 16:56:46 +0300

    fixes after mentor review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,23 +2,22 @@
 
 const burger = document.querySelector('.nav__burger');
 
-const mobileNav = document.querySelector('.burger');
+const burgerNav = document.querySelector('.burger');
 
-const mobileLink = document.querySelectorAll('.burger__link');
+const burgerLink = document.querySelectorAll('.burger__link');
 
 const form = document.querySelector('.form');
 
-
 burger.addEventListener('click', function() {
-  mobileNav.classList.toggle('burger--active');
+  burgerNav.classList.toggle('burger--active');
   burger.classList.toggle('nav__burger--active');
 });
 
-for (const elem of mobileLink) {
+for (const elem of burgerLink) {
   elem.addEventListener('click', function() {
-    mobileNav.classList.toggle('burger--active');
+    burgerNav.classList.toggle('burger--active');
     burger.classList.toggle('nav__burger--active');
-  })
+  });
 };
 
 form.addEventListener('submit', event => {
