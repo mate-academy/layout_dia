2c9e3374b0ac1e50dee453afb39f9fa7e202f91f
Your Name <mogolgogol.90@gmail.com>
2020-11-26 14:52:36 +0200

    smooth mob menu appearance, fixeв navbar, parallax drops

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', event => {
+  document.location.reload();
+  event.preventDefault();
+});
+
+const scene = document.querySelector('.vision--scene');
+
+function parallax(event) {
+  this.querySelectorAll('.vision__wrap').forEach(layer => {
+    const speed = layer.getAttribute('data-speed');
+    const halfWidth = scene.clientWidth / 2;
+    const halfHeight = scene.clientHeight / 2;
+
+    // eslint-disable-next-line max-len
+    layer.style.transform = 'translateX(' + -(event.clientX - halfWidth) * speed / 100 + 'px) translateY(' + -(event.clientY - halfHeight) * speed / 100 + 'px) rotate(-65.1deg)';
+  });
+}
+
+document.addEventListener('mousemove', parallax);
