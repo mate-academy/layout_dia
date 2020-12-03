de15706ceaa223568be55bb0bbbbd98d2a42e03a
Dmitry Paramonov <paramonovfrontend@gmail.com>
2020-12-03 17:04:22 +0200

    burger menu fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const menuIcon = document.querySelector('.menu-icon');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerMenuList = document.querySelector('.burger-menu__list');
+
+menuIcon.addEventListener('click', toggleMenu);
+burgerMenuList.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  page.classList.toggle('page--no-scroll');
+  burgerMenu.classList.toggle('header__burger-menu--open');
+
+  menuIcon.classList.toggle('menu-icon--open');
+  menuIcon.classList.toggle('header__menu--screen--small');
+}
