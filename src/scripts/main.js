371f1942d5afaa33e8311a29b5835568c5e913fb
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-03 15:21:11 +0200

    Mobile navigation was repeared

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const nav = document.querySelector('.header__nav');
+const toggler = document.querySelector('.toggler');
+
+toggler.onclick = function toggleSidebar() {
+  nav.classList.toggle('header__nav--has-popup');
+  toggler.classList.toggle('toggler--open');
+};
