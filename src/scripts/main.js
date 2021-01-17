c05c0188ea948987498df5fe24cf7744da5ce0f7
troospieler <troospieler1990@gmail.com>
2021-01-17 19:18:40 +0200

    added shadiw in t-card, removed form error

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 
-document.addEventListener('click', function() {
-  document.getElementById('demo').innerHTML = 'Hello World!';
-});
+function logSubmit(event) {
+  event.preventDefault;
+}
+
+form.addEventListener('submit', logSubmit);
+
+// document.addEventListener('click', function() {
+//   document.getElementById('demo').innerHTML = 'Hello World!';
+// });
