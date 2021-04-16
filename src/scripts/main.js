af280e3d580a215410bf8fea5592e8787a17f67b
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-04-16 06:15:10 +0300

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const toggler = document.querySelector('.header__top__nav__menu-toggler');
+const menu = document.querySelector('.menu');
+
+toggler.addEventListener('click', () => {
+  toggler.classList.toggle('header__top__nav__menu-toggler--active');
+  menu.classList.toggle('menu--active');
+  page.classList.toggle('page--fixed');
+});
+
+menu.addEventListener('click', () => {
+  toggler.classList.toggle('header__top__nav__menu-toggler--active');
+  menu.classList.toggle('menu--active');
+  page.classList.toggle('page--fixed');
+});
