2f513951dc6fdae876f1d1ac85e5c4e4ed57c88d
Minchuk Olga <olga@MacBook-Pro-Olga.local>
2021-04-19 15:32:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,8 @@
 const page = document.querySelector('.page');
 const toggler = document.querySelector('.header__top__nav__menu-toggler');
 
+const toggler1 = document.querySelector('.footer__bottom__nav__menu-toggler');
+
 const menu = document.querySelector('.menu');
 
 toggler.addEventListener('click', () => {
@@ -11,8 +13,15 @@ toggler.addEventListener('click', () => {
   page.classList.toggle('page--fixed');
 });
 
+toggler1.addEventListener('click', () => {
+  toggler1.classList.toggle('footer__bottom__nav__menu-toggler--active');
+  menu.classList.toggle('menu--active');
+  page.classList.toggle('page--fixed');
+});
+
 menu.addEventListener('click', () => {
   toggler.classList.toggle('header__top__nav__menu-toggler--active');
+  toggler1.classList.toggle('footer__bottom__nav__menu-toggler--active');
   menu.classList.toggle('menu--active');
   page.classList.toggle('page--fixed');
 });
