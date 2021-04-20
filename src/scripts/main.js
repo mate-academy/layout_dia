7105f875237a9d6b7f41c09b4bf3b6d3ab88c6dd
avgusta92 <avgusta92@gmail.com>
2021-04-20 09:46:54 +0300

    fix some problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const navigation = document.querySelector('#navigation');
+const toggler = document.querySelector('#toggler');
+const navLinks = document.querySelectorAll('.nav__link');
+
+toggler.addEventListener('click', () => {
+  navigation.classList.toggle('nav__nav-bar--small-screen-active');
+  toggler.classList.toggle('nav__burger-icon--close');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navigation.classList.remove('nav__nav-bar--small-screen-active');
+    toggler.classList.toggle('nav__burger-icon--close');
+  });
+});
