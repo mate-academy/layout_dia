62f4af082bb42a4fa633aad3ba37cfa8f44e8af2
avgusta92 <avgusta92@gmail.com>
2021-04-20 16:09:41 +0300

    fix some problems + add js for slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,12 @@ const navigation = document.querySelector('#navigation');
 const toggler = document.querySelector('#toggler');
 const navLinks = document.querySelectorAll('.nav__link');
 
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
+const images = document.querySelectorAll('.slider__photo');
+
+let counter = 0;
+
 toggler.addEventListener('click', () => {
   navigation.classList.toggle('nav__nav-bar--small-screen-active');
   toggler.classList.toggle('nav__burger-icon--close');
@@ -14,4 +20,26 @@ navLinks.forEach(link => {
     navigation.classList.remove('nav__nav-bar--small-screen-active');
     toggler.classList.toggle('nav__burger-icon--close');
   });
+});
+
+next.addEventListener('click', () => {
+  images[counter].classList.remove('slider__photo--active');
+
+  if (counter >= 2) {
+    counter = -1;
+  }
+  counter++;
+
+  images[counter].classList.add('slider__photo--active');
+});
+
+prev.addEventListener('click', () => {
+  images[counter].classList.remove('slider__photo--active');
+
+  if (counter <= 0) {
+    counter = 2;
+  }
+  counter--;
+
+  images[counter].classList.add('slider__photo--active');
 });
