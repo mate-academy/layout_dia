3a0355f1bab1a35416a3cd4fe8dbae34e4b003c1
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-06-16 09:03:39 +0300

    added adaptive design in mobile version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.myFunction = function() {
+  const list = document.querySelector('.navigation__list');
+  const icon = document.getElementById('burger-button');
+
+  if (list.style.display === '') {
+    list.style.display = 'flex';
+    icon.className = 'navigation__label-active';
+  } else {
+    list.style.display = '';
+    icon.className = 'navigation__label';
+  }
+};
