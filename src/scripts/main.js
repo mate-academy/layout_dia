209c6e18b757491d19e1764814ef01203e55b75b
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-16 13:14:44 +0300

    slider_working

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+let num = 0;
+const slider = document.querySelector('.header__slider-container');
+
+document.querySelector('.header__slider-but--left').addEventListener('click',
+  function() {
+    num += 100;
+
+    if (num === 100) {
+      num = -200;
+    };
+    slider.style.left = num + '%';
+  }
+);
+
+document.querySelector('.header__slider-but--right').addEventListener('click',
+  function() {
+    num -= 100;
+
+    if (num === -300) {
+      num = 0;
+    };
+    slider.style.left = num + '%';
+  }
+);
