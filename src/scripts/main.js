c2af464075376bd2b960b9028c00623549f78bac
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-06-16 17:30:15 +0300

    added buttons transitions, smooth transition to burger menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,14 @@
 'use strict';
 
 window.myFunction = function() {
-  const list = document.querySelector('.navigation__list');
-  const icon = document.getElementById('burger-button');
+  const list = document.getElementById('navigation__sidebar');
+  const icon = document.getElementById('navigation__burger-button');
 
-  if (list.style.display === '') {
-    list.style.display = 'flex';
+  if (icon.className === 'navigation__label') {
+    list.className = 'navigation__sidebar-active';
     icon.className = 'navigation__label-active';
   } else {
-    list.style.display = '';
+    list.className = 'navigation__sidebar';
     icon.className = 'navigation__label';
   }
 };
