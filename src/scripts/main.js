c895cc106e57823fa5a4b568d9054129d490fa07
Kandiak Oleg <kandio1604@gmail.com>
2021-06-18 19:00:56 +0300

    add

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const menu = document.querySelectorAll('.nav__item');
+const form = document.querySelector('.contact__form');
+const inputs = document.querySelectorAll('.value');
+
+document.querySelector('.nav__menu').onclick = function() {
+  menu.forEach(s => {
+    s.classList.toggle('nav__item--hide');
+  });
+
+  menu.classList.toggle('nav__item--hide');
+};
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach(input => {
+    input.value = '';
+  });
+});
