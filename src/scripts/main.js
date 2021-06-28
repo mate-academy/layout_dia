7e17a3b099f8cc7d86da3e8805aff6ec8dc14cdb
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-06-28 17:15:07 +0300

    add js interaction with form and sidebar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,19 @@ icon.addEventListener('click', () => {
   icon.classList.toggle('navigation__label--active');
   list.classList.toggle('navigation__sidebar--active');
 });
+
+list.addEventListener('click', () => {
+  icon.classList.remove('navigation__label--active');
+  list.classList.remove('navigation__sidebar--active');
+});
+
+const form = document.querySelector('.contact-us__form');
+const inputs = document.querySelectorAll('.contact-us__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
