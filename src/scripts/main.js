37f6a937d01f83dcc7867c73fe9b02e8cf618df4
Oleksii Blezniuk <oleksii.blezniuk@gmail.com>
2021-07-13 22:37:29 +0300

    slider was implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const sliderLine = document.querySelector('.slider__line');
+const buttonPrev = document.querySelector('.slider__button--prev');
+const buttonNext = document.querySelector('.slider__button--next');
+let position = 'first';
+
+buttonNext.addEventListener('click', () => {
+  if (position !== 'second') {
+    sliderLine.classList.add('slider__line--switched');
+    position = 'second';
+  }
+});
+
+buttonPrev.addEventListener('click', () => {
+  if (position !== 'first') {
+    sliderLine.classList.remove('slider__line--switched');
+    position = 'first';
+  }
+});
