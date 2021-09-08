ed6703b1d4d84117341811ce4d367ea9ed27b285
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-09-08 21:44:04 +0300

    slider was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
 'use strict';
+
+
+const slider = document.querySelector('.slider__container');
+const left = document.querySelector('.slider__button--left');
+const right = document.querySelector('.slider__button--right');
+
+let position = 0;
+
+left.addEventListener('click', function() {
+  move(-1);
+});
+
+right.addEventListener('click', function() {
+  move(1)
+});
+
+setInterval(function() {
+  move(1);
+}, 5000);
+
+function move(shift) {
+  const count = 3;
+
+  position += shift;
+
+  position = (position + count) % count;
+
+  slider.style.transform = `translateX(${-position * 100}%)`;
+}
