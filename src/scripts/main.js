9c2df3915ea6184a6c69b2e44f7dc5b23eeca1a1
Bogdan <bogdanemails@gmail.com>
2021-09-08 18:02:17 +0300

    header was created, slider buttons were added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const list = document.querySelector('.slider__photo-container');
+const prev = document.querySelector('.slider__button--prev');
+const next = document.querySelector('.slider__button--next');
+let position = 0;
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+  position = (position + count) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
+
+next.addEventListener('click', function() {
+  move(1);
+});
+
+prev.addEventListener('click', function() {
+  move(-1);
+});
