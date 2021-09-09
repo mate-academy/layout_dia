ba3b316a388f74132f79b99af978faf25773e857
anastasiia <theyashkindzhus@gmail.com>
2021-09-09 17:32:21 +0300

    added 4 blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const list = document.querySelector('.slider__list');
+const prev = document.querySelector('.slider__prev');
+const next = document.querySelector('.slider__next');
+
+let position = 0;
+
+move(0);
+
+// Ждём клик от пользователя на кнопке Next
+next.addEventListener('click', function() {
+  move(1);
+});
+
+// Ждём клик от пользователя на кнопке Prev
+prev.addEventListener('click', function() {
+  move(-1);
+});
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+
+  next.disabled = position >= count - 1;
+  prev.disabled = position <= 0;
+
+  position = (position + count) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
