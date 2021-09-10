a5014d9c02238f9a7772b62e6902cd44d7b78177
Ruslan-Mykhalchak <ruslan1993x@gmail.com>
2021-09-10 12:27:35 +0300

    v02

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const list = document.querySelector('.slider__photo_container');
+const prev = document.querySelector('.slider__button--left');
+const next = document.querySelector('.slider__button--right');
+let position = 0;
+
+function move(shift) {
+  const count = list.children.length;
+
+  position += shift;
+  position = (position + count) % count;
+
+  list.style.transform = `translateX(${-position * 100}%)`;
+}
+
+next.addEventListener('click', function() {
+  move(1);
+});
+
+prev.addEventListener('click', function() {
+  move(-1);
+});
+
+setInterval(function() {
+  move(1);
+}, 5000);
