af11097cc5f9dfd65c4c155fc43899ab09bd8eb7
Roman Toritsyn <romantoritsyn@gmail.com>
2021-09-11 13:59:16 +0300

    slider added 1024 1280 responsive adaptive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const images = document.querySelector('.header__slider-images');
+const prev = document.querySelector('.header__slider-button--left');
+const next = document.querySelector('.header__slider-button--right');
+
+let position = 0;
+
+next.addEventListener('click', function() {
+  const count = images.children.length;
+
+  position += 1;
+
+  if (position >= count) {
+    position = 0;
+  }
+
+  images.style.transform = `translateX(${-position * 100}%)`;
+});
+
+prev.addEventListener('click', function() {
+  const count = images.children.length;
+
+  position -= 1;
+
+  if (position < 0) {
+    position = count - 1;
+  }
+
+  images.style.transform = `translateX(${-position * 100}%)`;
+});
