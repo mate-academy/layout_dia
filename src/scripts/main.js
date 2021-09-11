a4150a795412408875037a87a1a8de90a71bd0ec
Iurii Medvedchuk <io.medvedchuk@gmail.com>
2021-09-11 22:06:17 +0300

    sroll removing was disabled for reasons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,31 +1,36 @@
 'use strict';
 
-const menuOpener = document.querySelector('.nav__button--open');
-const menuCloser = document.querySelector('.nav__button--close');
-const footerMenuOpener = document.querySelector('.nav__button--open-footer');
-const footerMenuCloser = document.querySelector('.nav__button--close-footer');
-const body = document.querySelector('.page__body');
+// const menuOpener = document.querySelector('.nav__button--open');
+// const footerMenuOpener = document.querySelector('.nav__button--open-footer');
+// const menuCloser = document.querySelector('.nav__button--close');
+// const footerMenuCloser = document.querySelector('.nav__button--close-footer');
+// const linkMenuCloser = document.querySelector('.nav__link');
+// const body = document.querySelector('.page__body');
 const slider = document.querySelector('.slider__container');
 const left = document.querySelector('.slider__button--left');
 const right = document.querySelector('.slider__button--right');
 
 let position = 0;
 
-menuOpener.addEventListener('click', function() {
-  body.classList.add('page__body--overflow-hidden');
-});
+// menuOpener.addEventListener('click', function() {
+//   body.classList.add('page__body--overflow-hidden');
+// });
 
-menuCloser.addEventListener('click', function() {
-  body.classList.remove('page__body--overflow-hidden');
-});
+// menuCloser.addEventListener('click', function() {
+//   body.classList.remove('page__body--overflow-hidden');
+// });
 
-footerMenuOpener.addEventListener('click', function() {
-  body.classList.add('page__body--overflow-hidden');
-});
+// footerMenuOpener.addEventListener('click', function() {
+//   body.classList.add('page__body--overflow-hidden');
+// });
 
-footerMenuCloser.addEventListener('click', function() {
-  body.classList.remove('page__body--overflow-hidden');
-});
+// linkMenuCloser.addEventListener('click', function() {
+//   body.classList.remove('page__body--overflow-hidden');
+// });
+
+// footerMenuCloser.addEventListener('click', function() {
+//   body.classList.remove('page__body--overflow-hidden');
+// });
 
 left.addEventListener('click', function() {
   move(-1);
