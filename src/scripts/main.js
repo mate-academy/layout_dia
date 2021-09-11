bdf825c224386fb1d4a78ed470346bf90c80f61c
Bogdan <bogdanemails@gmail.com>
2021-09-11 08:34:33 +0300

    smooth slider, active photo

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,14 @@
 const list = document.querySelector('.slider__photo-container');
 const prev = document.querySelector('.slider__button--prev');
 const next = document.querySelector('.slider__button--next');
+// const active = document.querySelector('.slider');
 let position = 0;
 
 function move(shift) {
   const count = list.children.length;
 
   position += shift;
-  position = (position + count) % count;
+  position = Math.abs(position) % count;
 
   list.style.transform = `translateX(${-position * 100}%)`;
 }
@@ -24,4 +25,8 @@ prev.addEventListener('click', function() {
 
 setInterval(function() {
   move(1);
-}, 5000);
+}, 7000);
+
+// active.addEventListener('click', function() {
+//   move === false;
+// });
