ce87508b5729cd0f40d3e6c9ccb8a6ddac158eb5
cyb1ove <cyb1ove.ua@gmail.com>
2021-09-30 23:30:38 +0300

    Landing completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.footer__form');
+
+  form.addEventListener('submit', handleEvent);
+
+  function handleEvent(event) {
+    event.preventDefault();
+    form.reset();
+  }
+});
