74434378984b366cc0de1f32b8bdbf8a13f31950
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-01 19:31:27 +0300

    favicon was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,9 @@
 'use strict';
 
 document.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#home') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
 });
-
