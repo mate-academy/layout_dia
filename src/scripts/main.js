d09e1948079f299c208a46c8090fa155c08e6912
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-09 20:43:10 +0300

    Chsnges was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
-document.addEventListener('hashchange', () => {
-  if (window.location.hash === '#home') {
+document.addEventListener('menu', () => {
+  if (window.location.hash === '#burger') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
