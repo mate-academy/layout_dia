b6d60b79f649d8bb6a02750e4bb976e349d65493
Vitaliy Voytovich <voitovichatque@gmail.com>
2021-11-04 13:22:35 +0200

    Dia completed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const slider = document.querySelector('.slider');
+const left = document.querySelector('.slider__button--left');
+const right = document.querySelector('.slider__button--right');
+
+let count = 1;
+
+right.addEventListener('click', () => {
+  if (count === 3) {
+    count = 0;
+    slider.classList.remove('slider--2');
+    slider.classList.remove('slider--3');
+  }
+  count++;
+  slider.classList.add(`slider--${count}`);
+});
+
+left.addEventListener('click', () => {
+  if (count === 1) {
+    count = 4;
+    slider.classList.add('slider--2');
+    slider.classList.add('slider--3');
+  }
+
+  slider.classList.remove(`slider--${count}`);
+  count--;
+});
+
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
