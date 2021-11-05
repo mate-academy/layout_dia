a3e17dd092b8b49508346ac436fd03baf7a433ea
VasylPerepichai <vperepichay@gmail.com>
2021-11-05 21:04:08 +0200

    fixed js script

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,31 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const sliderToLeft = document.querySelector('.js-slider-click-left');
+const sliderToRight = document.querySelector('.js-slider-click-right');
+
+sliderToLeft.addEventListener('click', () => plusSlides(-1));
+sliderToRight.addEventListener('click', () => plusSlides(1));
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.slider-img');
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  if (n >= slides.length) {
+    slideIndex = 0;
+  };
+
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (const slide of slides) {
+    slide.style.display = 'none';
+  }
+  slides[slideIndex].style.display = 'block';
+}
