9f50d0c846ac4513bd861a21e92d61ae2298b97a
unknown <skirik0117@gmail.com>
2021-11-06 12:53:11 +0200

    page-air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const btnL = document.getElementById('sliderBtnLeft');
+const btnR = document.getElementById('sliderBtnRight');
+const img = document.getElementById('sliderImg');
+
+let curentImg = 1;
+
+btnL.addEventListener('click', () => {
+  img.classList.remove(`header__content-image--${curentImg}`);
+  curentImg -= 1;
+
+  if (curentImg === 0) {
+    curentImg = 4;
+  }
+  img.classList.add(`header__content-image--${curentImg}`);
+});
+
+btnR.addEventListener('click', () => {
+  img.classList.remove(`header__content-image--${curentImg}`);
+  curentImg += 1;
+
+  if (curentImg === 5) {
+    curentImg = 1;
+  }
+  img.classList.add(`header__content-image--${curentImg}`);
+});
