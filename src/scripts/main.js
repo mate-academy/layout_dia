6cb4c58efe28c7c4c102b0fd5a22332cbfc9269f
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2021-11-08 16:58:37 +0200

    Air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const slider = document.querySelector('.header__slider');
+
+const leftButton = document.querySelector('.slider__button-left');
+const rightButton = document.querySelector('.slider__button-right');
+
+let count = 1;
+
+leftButton.addEventListener('click', () => {
+  slider.classList.remove(`header__slider--${count}`);
+
+  if (count === 1) {
+    count = 4;
+  }
+  count--;
+  slider.classList.add(`header__slider--${count}`);
+});
+
+rightButton.addEventListener('click', () => {
+  slider.classList.remove(`header__slider--${count}`);
+
+  if (count === 3) {
+    count = 0;
+  }
+  count++;
+  slider.classList.add(`header__slider--${count}`);
+});
