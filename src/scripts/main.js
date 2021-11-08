7daffb0a48853eca9b0415c1f72c10339d86aa03
lanebx <havchikos@gmail.com>
2021-11-08 22:53:05 +0200

    fix: menu modal block

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const btnMenu = document.querySelector('.btn');
+const menu = document.querySelector('.menu');
+const toggleMenu = function() {
+  menu.classList.toggle('open');
+};
+
+btnMenu.addEventListener('click', function(e) {
+  e.stopPropagation();
+  toggleMenu();
+});
+
+document.addEventListener('click', function(e) {
+  const target = e.target;
+  const itsMenu = target === menu || menu.contains(target);
+  const itsBtnMenu = target === btnMenu;
+  const menuActive = menu.classList.contains('open');
+
+  if (!itsMenu && !itsBtnMenu && menuActive) {
+    toggleMenu();
+  }
+});
