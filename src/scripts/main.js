f422b0a30768d80a21937ec97946385e1d906ffe
lanebx <havchikos@gmail.com>
2021-11-11 15:31:46 +0200

    fixed closing menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,9 +6,18 @@ const toggleMenu = function() {
   menu.classList.toggle('open');
 };
 
+const closeMenu = function() {
+  menu.classList.toggle('close');
+};
+
 btnMenu.addEventListener('click', function(e) {
   e.stopPropagation();
   toggleMenu();
+});
+
+menu.addEventListener('click', function(e) {
+  e.stopPropagation();
+  closeMenu();
 });
 
 document.addEventListener('click', function(e) {
