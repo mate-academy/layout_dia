f68bedec93f4541bb6c5c833a8f70f64d7a46371
SavelovSerhii <savelov12111@gmail.com>
2021-12-07 17:12:46 +0200

    Footer was updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const form = document.querySelector('.footer__form');
+const inputs = document.querySelectorAll('.footer__input');
+const textarea = document.querySelector('.footer__message');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  inputs.forEach((elem) => {
+    elem.value = '';
+  });
+
+  textarea.value = '';
+});
