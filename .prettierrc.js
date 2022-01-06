a97d85798271af72d6b919cb8e77acd7586cd58e
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2022-01-06 15:38:15 +0200

    feat: solution

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  bracketSpacing: true,
+};
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,47 @@
 'use strict';
+
+const slider = document.querySelector('.slider');
+const sliderButtons = slider.querySelectorAll('.slider__button');
+const navBurger = document.querySelector('.nav__burger');
+const navItems = document.querySelector('.nav__items');
+let sliderNumber = 1;
+
+sliderButtons.forEach(button => {
+  if (button.closest('.slider__button--left')) {
+    button.addEventListener('click', () => {
+      slider.classList.remove(`slider--bg-img-${sliderNumber}`);
+
+      sliderNumber--;
+
+      if (sliderNumber < 1) {
+        sliderNumber = 3;
+      }
+
+      slider.classList.add(`slider--bg-img-${sliderNumber}`);
+    });
+  } else {
+    button.addEventListener('click', () => {
+      slider.classList.remove(`slider--bg-img-${sliderNumber}`);
+
+      sliderNumber++;
+
+      if (sliderNumber > 3) {
+        sliderNumber = 1;
+      }
+
+      slider.classList.add(`slider--bg-img-${sliderNumber}`);
+    });
+  }
+});
+
+navBurger.addEventListener('click', () => {
+  const openedNav = document.querySelector('.nav__burger--opened');
+
+  if (!openedNav) {
+    navItems.style.top = '0';
+  } else {
+    navItems.style.top = '-400px';
+  }
+
+  navBurger.classList.toggle('nav__burger--opened');
+});
