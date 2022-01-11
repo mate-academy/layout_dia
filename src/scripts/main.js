9ddfe24fd62d40cf70a6c80de455b2e4896b831a
Dmytro Dsypiuk <dimich578ua@gmail.com>
2022-01-11 17:17:07 +0200

    header in progress

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const page = document.querySelector('.page');
+const togglers = document.querySelectorAll('.menu__toggler');
+const menu = document.querySelector('.menu__list-container');
+const notabs = document.querySelectorAll('.notab *');
+
+togglers.forEach((toggler) => {
+  toggler.addEventListener('click', function() {
+    page.classList.toggle('page--with-modal');
+    menu.classList.toggle('menu__list-container--open');
+
+    notabs.forEach((element) => {
+      if (element.getAttribute('tabindex') === '-1') {
+        element.toggleAttribute('tabindex');
+      } else {
+        element.setAttribute('tabindex', '-1');
+      }
+    });
+  });
+});
