786b0715b6aa65564b67246ba00112901ae67948
Dmytro Dsypiuk <dimich578ua@gmail.com>
2022-01-13 21:53:56 +0200

    feature selector implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,11 @@ const togglers = document.querySelectorAll('.menu__toggler');
 const menu = document.querySelector('.menu__list-container');
 const notabs = document.querySelectorAll('.notab *');
 const noDefault = document.querySelector('.noDefault');
+const featureMenuToggler = document.querySelector('.feature-menu-toggler');
+const featureContainer = document.querySelector('.feature__container');
+const darkFeature = document.querySelector('.feature-dark');
+const blueFeature = document.querySelector('.feature-blue');
+const specialFeature = document.querySelector('.feature-special');
 
 noDefault.addEventListener('submit', function(e) {
   e.preventDefault();
@@ -27,4 +32,20 @@ togglers.forEach((toggler) => {
       }
     });
   });
+});
+
+featureMenuToggler.addEventListener('click', function() {
+  featureContainer.classList.toggle('feature__container--on');
+});
+
+darkFeature.addEventListener('click', function() {
+  page.setAttribute('color-scheme', 'dark');
+});
+
+blueFeature.addEventListener('click', function() {
+  page.setAttribute('color-scheme', 'blue');
+});
+
+specialFeature.addEventListener('click', function() {
+  page.setAttribute('color-scheme', 'special');
 });
