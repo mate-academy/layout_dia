96c637f75909480b1e12a8f15d7dccb87e9b0c52
Dmytro Dsypiuk <dimich578ua@gmail.com>
2022-01-13 20:33:59 +0200

    links were fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,15 @@ const page = document.querySelector('.page');
 const togglers = document.querySelectorAll('.menu__toggler');
 const menu = document.querySelector('.menu__list-container');
 const notabs = document.querySelectorAll('.notab *');
+const noDefault = document.querySelector('.noDefault');
+
+noDefault.addEventListener('submit', function(e) {
+  e.preventDefault();
+
+  document.getElementsByName('name')[0].value = '';
+  document.getElementsByName('email')[0].value = '';
+  document.getElementsByName('message')[0].value = '';
+});
 
 togglers.forEach((toggler) => {
   toggler.addEventListener('click', function() {
