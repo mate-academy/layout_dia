79543aa19358116e4be91e93f7bb55cec55dcd71
Олег Майборода <xa12850446@student.karazin.ua>
2022-01-19 07:30:19 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#dropdown-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('form__name').value = '';
+  document.getElementById('form__message').value = '';
+  document.getElementById('form__email').value = '';
+});
+
+document.myForm.reset();
