0bc91273620d9f0c0a242204518c89c87e9e9e77
Vova Alputov <alputov1998@gmail.com>
2022-02-12 22:14:46 +0200

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const buttonOpen = document.querySelector('.nav__burger');
+const buttonClose = document.querySelector('.nav__burger-close');
+const nav = document.querySelector('.nav--mobile');
+
+buttonOpen.addEventListener('click', () => {
+  nav.style.transform = 'translate(0)';
+});
+
+buttonClose.addEventListener('click', () => {
+  nav.style.transform = 'translate(-100%)';
+});
