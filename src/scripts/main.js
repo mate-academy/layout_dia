eb8a3134599123aed358419b0d6fca58d205a011
Ivan Major <tarstarcas@gmail.com>
2022-02-21 22:14:33 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,39 @@
 'use strict';
+
+const sliderImages = document.querySelectorAll('.slider__image');
+const buttonLeft = document.querySelector('.slider__button--left');
+const buttonRight = document.querySelector('.slider__button--right');
+
+let currentImage = 0;
+
+function nextImage() {
+  sliderImages[currentImage].classList.remove('slider__image--active');
+
+  currentImage += 1;
+
+  if (currentImage > sliderImages.length - 1) {
+    currentImage = 0;
+  };
+
+  sliderImages[currentImage].classList.add('slider__image--active');
+}
+
+function previousImage() {
+  sliderImages[currentImage].classList.remove('slider__image--active');
+
+  currentImage -= 1;
+
+  if (currentImage < 0) {
+    currentImage = sliderImages.length - 1;
+  };
+
+  sliderImages[currentImage].classList.add('slider__image--active');
+}
+
+buttonLeft.addEventListener('click', () => {
+  previousImage();
+});
+
+buttonRight.addEventListener('click', () => {
+  nextImage();
+});
