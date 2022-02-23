1984e2c15ece11a44af99de80809c5ae7f4ecd54
Ivan Major <tarstarcas@gmail.com>
2022-02-23 16:09:01 +0200

    solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -37,3 +37,11 @@ buttonLeft.addEventListener('click', () => {
 buttonRight.addEventListener('click', () => {
   nextImage();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
