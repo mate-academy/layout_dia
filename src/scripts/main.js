8eae79d1940b82438632c68bbc1a4f1820606b84
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-03-18 19:53:42 +0200

    finihsed, but no slider :(

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// function plusSlides() {
+//   const slider = document.querySelector('slider');
+//   let i = 1;
+
+//   slider.style = `background-image: url(../images/slider${i}.jpg)`;
+
+//   i++;
+// }
