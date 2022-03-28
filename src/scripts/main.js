67ba5b74360685066369b8def28cc5dd6f3d5925
yurii837 <gorogotskiy@gmail.com>
2022-03-28 18:41:41 +0300

    add menu & fixed problems

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,3 +10,13 @@ function stopDefAction(event) {
 document.getElementById('form').addEventListener(
   'submit', stopDefAction, false
 );
+
+// burger menu
+
+const nav = document.querySelector('.nav__main');
+const toggler = document.querySelector('.nav__icon--menu-opener');
+
+toggler.addEventListener('click', function() {
+  nav.classList.toggle('nav--open');
+  toggler.classList.toggle('nav--open');
+});
