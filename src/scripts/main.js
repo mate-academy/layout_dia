2089abc46d081af8083774f4a04b278d22c13cfc
Serhii Naumenko <s.naumenko.miy@gmail.com>
2022-04-01 14:37:39 +0300

    with icons in svg

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+function stopDefAction(event) {
+  document.getElementById('contactForm').reset();
+  event.preventDefault();
+}
+
+document.getElementById('contactForm').addEventListener(
+  'submit', stopDefAction, false
+);
