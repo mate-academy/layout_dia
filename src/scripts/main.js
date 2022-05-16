3102efe9864a60399bf0ceee07d742da54f21237
Vitalii Kononets <vitalii.kononets.85@gmail.com>
2022-05-16 18:33:34 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
 'use strict';
+
+const SLIDER_PICTURES
+  = [
+    'slider--1',
+    'slider--2',
+    'slider--3',
+  ];
+
+let pictureId = 0;
+const slideLeft = document.querySelector('#slider-left');
+const slideRight = document.querySelector('#slider-right');
+const sliderPicture = document.querySelector('.slider');
+
+slideLeft.addEventListener('click', e => {
+  sliderPicture.classList.remove(SLIDER_PICTURES[pictureId]);
+
+  if (pictureId === 0) {
+    pictureId = SLIDER_PICTURES.length - 1;
+  } else {
+    pictureId -= 1;
+  };
+
+  sliderPicture.classList.add(SLIDER_PICTURES[pictureId]);
+});
+
+slideRight.addEventListener('click', e => {
+  sliderPicture.classList.remove(SLIDER_PICTURES[pictureId]);
+
+  if (pictureId === SLIDER_PICTURES.length - 1) {
+    pictureId = 0;
+  } else {
+    pictureId += 1;
+  };
+
+  sliderPicture.classList.add(SLIDER_PICTURES[pictureId]);
+});
