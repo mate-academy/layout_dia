368020b4d7fd846197a5109cf543559b23517e2f
Taras Kozii <taras_kozii@ukr.net>
2022-05-19 21:35:56 +0300

    fixed burger

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,4 +7,16 @@ iconMenu.addEventListener('click', function() {
   iconMenu.classList.toggle('icon-menu--close');
   menu.classList.toggle('page-menu');
   document.body.classList.toggle('page__body--with-menu');
+
+  if (iconMenu.classList.contains('icon-menu--close')) {
+    const links = document.querySelectorAll('.menu__link');
+
+    for (const elem of links) {
+      elem.addEventListener('click', function() {
+        iconMenu.classList.remove('icon-menu--close');
+        menu.classList.remove('page-menu');
+        document.body.classList.remove('page__body--with-menu');
+      });
+    }
+  }
 });
