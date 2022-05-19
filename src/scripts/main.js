b6d77ecc781db6f2398142391d03d84a7bc566d8
Taras Kozii <taras_kozii@ukr.net>
2022-05-19 21:02:58 +0300

    air v01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const menu = document.querySelector('.menu');
+const iconMenu = document.querySelector('.icon-menu');
+
+iconMenu.addEventListener('click', function() {
+  iconMenu.classList.toggle('icon-menu--close');
+  menu.classList.toggle('page-menu');
+  document.body.classList.toggle('page__body--with-menu');
+});
