524b29bfc5b2be12770c6a957f737775b9c12f26
Taras Kozii <taras_kozii@ukr.net>
2022-05-22 21:41:27 +0300

    air v03

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,10 @@
 
 const menu = document.querySelector('.menu');
 const iconMenu = document.querySelector('.icon-menu');
+const swiper = document.querySelector('.header__right-sidebar');
+const buttonLeft = document.querySelector('.swiper__button--left');
+const buttonRight = document.querySelector('.swiper__button--right');
+let swiperCards = 2;
 
 iconMenu.addEventListener('click', function() {
   iconMenu.classList.toggle('icon-menu--close');
@@ -18,5 +22,20 @@ iconMenu.addEventListener('click', function() {
         document.body.classList.remove('page__body--with-menu');
       });
     }
+  }
+});
+
+document.getElementById('form-message').addEventListener(
+  'click', function(event) {
+    event.preventDefault();
+  });
+
+buttonRight.addEventListener('click', function() {
+  swiper.classList.add(`header__right-sidebar--${swiperCards++}`);
+});
+
+buttonLeft.addEventListener('click', function() {
+  if (swiperCards >= 2) {
+    swiper.classList.remove(`header__right-sidebar--${swiperCards--}`);
   }
 });
