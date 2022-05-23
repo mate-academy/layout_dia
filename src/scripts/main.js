5e878523f199004e0c93d1e9050a2ae3edd20c3f
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-05-23 13:13:38 +0300

    header done -- prepare menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const slider = document.querySelector('.slider');
+const sliderLeft = document.querySelector('.btn-left');
+const sliderRight = document.querySelector('.btn-right');
+let slideNumber = 1;
+
+function changeSlide(direction) {
+  slider.classList.remove(`slider--bckgrnd-${slideNumber}`);
+
+  if (direction === 'l') {
+    slideNumber === 1
+      ? slideNumber = 4
+      : slideNumber--;
+  }
+
+  if (direction === 'r') {
+    slideNumber === 4
+      ? slideNumber = 1
+      : slideNumber++;
+  }
+
+  slider.classList.add(`slider--bckgrnd-${slideNumber}`);
+}
+
+sliderLeft.addEventListener('click', () => {
+  changeSlide('l');
+}
+);
+
+sliderRight.addEventListener('click', () => {
+  changeSlide('r');
+}
+);
