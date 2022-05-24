5e9691a93ec67caa5b1a6fb3e5af6b8faf82a8b2
Pavlo <Pmoroz2004@gmail.com>
2022-05-24 09:12:03 +0300

    experemental version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const icon = document.querySelector('.top-actions__list');
+const toggler = document.querySelector('.top-actions__toggler');
+
+toggler.addEventListener('click', () => {
+  icon.classList.toggle('top-actions--open');
+});
