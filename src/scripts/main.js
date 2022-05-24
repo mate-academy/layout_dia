4fd2ed1b54318993b798ca820a392b5ea43b25bc
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-05-24 17:47:16 +0300

    prepairing testimonials

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -32,3 +32,11 @@ sliderRight.addEventListener('click', () => {
   changeSlide('r');
 }
 );
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
