cc16251ff080a8a5f4cb2aab4999e34ea9009749
Pavlo <Pmoroz2004@gmail.com>
2022-05-26 23:07:29 +0300

    experemental version 2.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,9 @@
 'use strict';
 
-const topActions = document.querySelector('.top-actions__nav');
-const toggler = document.querySelector('.top-actions__toggler');
-
-toggler.addEventListener('click', () => {
-  topActions.classList.toggle('top-actions--open');
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
