1f3453b6b2cbc513eb7b0fd6af942e7c13545914
Taras Kozii <taras_kozii@ukr.net>
2022-05-29 19:44:06 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,29 +1,9 @@
 'use strict';
 
-const menu = document.querySelector('.menu');
-const iconMenu = document.querySelector('.icon-menu');
 const swiper = document.querySelector('.header__right-sidebar');
 const buttonLeft = document.querySelector('.swiper__button--left');
 const buttonRight = document.querySelector('.swiper__button--right');
 let swiperCards = 2;
-
-iconMenu.addEventListener('click', function() {
-  iconMenu.classList.toggle('icon-menu--close');
-  menu.classList.toggle('page-menu');
-  document.body.classList.toggle('page__body--with-menu');
-
-  if (iconMenu.classList.contains('icon-menu--close')) {
-    const links = document.querySelectorAll('.menu__link');
-
-    for (const elem of links) {
-      elem.addEventListener('click', function() {
-        iconMenu.classList.remove('icon-menu--close');
-        menu.classList.remove('page-menu');
-        document.body.classList.remove('page__body--with-menu');
-      });
-    }
-  }
-});
 
 document.getElementById('form-message').addEventListener(
   'click', function(event) {
@@ -37,5 +17,13 @@ buttonRight.addEventListener('click', function() {
 buttonLeft.addEventListener('click', function() {
   if (swiperCards >= 2) {
     swiper.classList.remove(`header__right-sidebar--${swiperCards--}`);
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
   }
 });
