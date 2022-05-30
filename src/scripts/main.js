0208608c26e24d60a13666525162abdcf27fde30
Pavlo <Pmoroz2004@gmail.com>
2022-05-30 14:12:06 +0300

    demo version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
+/* eslint-disable no-unused-vars */
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-undef
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
