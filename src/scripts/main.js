3445f7c9d6d99744d4d2078925bcfa7b1df58170
Pavlo <Pmoroz2004@gmail.com>
2022-05-30 19:36:26 +0300

    add blue version site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,6 +9,43 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+const rectangle = document.querySelectorAll('.testimonial__rectangle');
+const shape = document.querySelectorAll('.testimonial__shape');
+const bgColor = document.querySelectorAll('.header, .footer');
+const button = document.querySelectorAll('.button');
+const id = document.querySelectorAll('.services__id');
+const buttonArrow = document.querySelectorAll('.services__button');
+const toggler = document.querySelector('.actions__button');
+const bgVision = document.querySelector('.vision');
+
+toggler.addEventListener('click', () => {
+  rectangle[0].classList.toggle('testimonial__rectangle--pale-blue');
+  rectangle[1].classList.toggle('testimonial__rectangle--yellow');
+  rectangle[2].classList.toggle('testimonial__rectangle--pink');
+
+  shape[0].classList.toggle('testimonial__shape--pale-blue');
+  shape[1].classList.toggle('testimonial__shape--yellow');
+  shape[2].classList.toggle('testimonial__shape--pink');
+
+  bgColor[0].classList.toggle('header--blue');
+  bgColor[1].classList.toggle('footer--blue');
+  bgVision.classList.toggle('vision--colorful');
+
+  for (let i = 0; i < button.length; i++) {
+    button[i].classList.toggle('button--pink');
+  }
+
+  id[0].classList.toggle('services__id--blue');
+  id[1].classList.toggle('services__id--yellow');
+  id[2].classList.toggle('services__id--green');
+  id[3].classList.toggle('services__id--pink');
+
+  buttonArrow[0].classList.toggle('services__button--blue');
+  buttonArrow[1].classList.toggle('services__button--yellow');
+  buttonArrow[2].classList.toggle('services__button--green');
+  buttonArrow[3].classList.toggle('services__button--pink');
+});
+
 // eslint-disable-next-line no-unused-vars
 // eslint-disable-next-line no-undef
 const swiper = new Swiper('.swiper', {
