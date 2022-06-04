6439c0a4cc3f3ef77222740bdb8899fab0c804f7
Taras Kozii <taras_kozii@ukr.net>
2022-06-04 12:06:26 +0300

    fixed swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,9 @@
 'use strict';
 
-const swiper = document.querySelector('.header__right-sidebar');
-const buttonLeft = document.querySelector('.swiper__button--left');
-const buttonRight = document.querySelector('.swiper__button--right');
-let swiperCards = 2;
-
 document.getElementById('form-message').addEventListener(
   'click', function(event) {
     event.preventDefault();
   });
-
-buttonRight.addEventListener('click', function() {
-  swiper.classList.add(`header__right-sidebar--${swiperCards++}`);
-});
-
-buttonLeft.addEventListener('click', function() {
-  if (swiperCards >= 2) {
-    swiper.classList.remove(`header__right-sidebar--${swiperCards--}`);
-  }
-});
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
@@ -26,4 +11,21 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const swiperBody = new Swiper('.swiper__body', {
+  navigation: {
+    nextEl: '.swiper__button-next',
+    prevEl: '.swiper__button-prev',
+  },
+
+  loop: true,
+  speed: 1500,
+
+  autoplay: {
+    delay: 3000,
+    stopOnLastSlide: true,
+    disableOnInteraction: true,
+  },
+
 });
