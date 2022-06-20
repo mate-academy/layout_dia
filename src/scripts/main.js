6b3f91e79f6fea91c41e7ec42a9325535eda7cee
Denys Politov <dpolitov2001@gmail.com>
2022-06-20 05:40:43 +0300

    Ready for review

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const menu = document.querySelector('.section--menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    menu.classList.add('section--menu-shown');
+  }
+});
+
+window.addEventListener('wheel', () => {
+  menu.classList.remove('section--menu-shown');
+  window.location.hash = '#';
+});
