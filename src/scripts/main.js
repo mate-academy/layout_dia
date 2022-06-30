739090e05b3881d592d7c2eaf2cb99bb50e1d234
Vitaliy <vitmixgt@icloud.com>
2022-06-30 20:37:30 +0300

    form fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -40,3 +40,19 @@ const checkBtns = () => {
 };
 
 checkBtns();
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.page__section-form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
