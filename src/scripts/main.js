819e060da3613ca2236665585d1f2b8b06e56a38
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-06-30 13:29:49 +0300

    haeder height was fixed && slider was formatted && button link was added && input autocomplete styles was added && footer menu was centered && tablet

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 const form = document.querySelector('.form');
+const openMenuBtn = document.querySelector('#open');
+const closeMenuBtn = document.querySelector('#close');
+const body = document.querySelector('.page__body');
+const links = document.querySelectorAll('.nav__link--scroll');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   form.reset();
 });
+
+const disableScroll = () => {
+  body.classList.toggle('page__body--scroll');
+};
+
+links.forEach(el => {
+  el.addEventListener('click', disableScroll);
+});
+
+openMenuBtn.addEventListener('click', disableScroll);
+closeMenuBtn.addEventListener('click', disableScroll);
