f93ddc3b6b90c25099c0f9a5f6949473866befe3
Tykhon Shuliak <tiandzh2013@gmail.com>
2022-07-01 14:33:49 +0300

    next

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
+
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+
+prev.onclick = () => {
+  document.getElementById('slide').classList.remove('slide__picture-two');
+};
+
+next.onclick = () => {
+  document.getElementById('slide').classList.add('slide__picture-two');
+};
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menus') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
