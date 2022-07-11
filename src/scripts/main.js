d1f2526ffed773081bc31fbed9f56d110bd87481
Dmytro Kretov <dmytro.kretov@gmail.com>
2022-07-11 13:11:25 +0300

    Fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,15 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('body--with-menu');
   }
 });
+
+
+const form = document.querySelector('.send');
+const inputs = document.querySelectorAll('.send__input');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  for (const input of inputs) {
+    input.value = '';
+  }
+});
