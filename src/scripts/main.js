ab56bd8288d33b961ec5348c4fd878771097797d
Boris Kerekesha <kerekesha90@ukr.net>
2022-07-11 11:00:33 +0300

    air 11.07

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,15 @@
 'use strict';
 
-function stopDefAction(event) {
-  event.preventDefault();
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#nav-mobile') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+function stopDefAction(evt) {
+  evt.preventDefault();
 }
 
 document.getElementById('my-form').addEventListener(
