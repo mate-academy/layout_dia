77224092b02e457b5154626e977b6b7d62f0d46d
Andrii Kuzmenko <andriikuzmenko35@gmail.com>
2022-07-20 20:01:29 +0300

    requests complited

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,17 @@
 'use strict';
 
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
+  if (window.location.hash === '#side-menu') {
     document.body.classList.add('page__body--with-menu');
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
 });
