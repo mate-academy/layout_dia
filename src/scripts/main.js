7a16fba6a3752c58a1809dcab65414f9142c13b2
Kolya Movchan <kmovchan261@gmail.com>
2022-07-23 00:10:55 +0300

    Landing is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
+/* eslint-disable no-undef */
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const form = document.querySelector('.message-form');
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
