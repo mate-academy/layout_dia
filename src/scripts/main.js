bb849f7077f1cfca410dc5434f072ffadf130f54
Kolya Movchan <kmovchan261@gmail.com>
2022-07-28 18:43:37 +0300

    JS updated

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 'use strict';
 
 window.addEventListener('hashchange', () => {
@@ -7,4 +6,21 @@ window.addEventListener('hashchange', () => {
   } else {
     document.body.classList.remove('page__body--with-menu');
   }
+});
+
+const form = document.querySelector('.message-form');
+const input1 = document.getElementById('input1');
+const input2 = document.getElementById('input2');
+const input3 = document.getElementById('input3');
+
+function handleForm(event) {
+  event.preventDefault();
+}
+
+form.addEventListener('submit', handleForm);
+
+form.addEventListener('submit', (event) => {
+  input1.value = '';
+  input2.value = '';
+  input3.value = '';
 });
