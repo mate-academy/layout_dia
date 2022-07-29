d1762981a786d31ce9bf9836830a7db5972b4479
Serhii Yunnikov <yunnikov@icloud.com>
2022-07-29 14:21:49 +0300

    without slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__menu--stop-scrolling');
+  } else {
+    document.body.classList.remove('page__menu--stop-scrolling');
+  }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
