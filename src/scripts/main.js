4690fc7324dad2af10e6d14ea333f6d546f02cbf
Andrew Zaitsev <alzay007@gmail.com>
2022-08-03 21:55:09 +0300

    swiper was created

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,20 @@
-'use strict';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+// eslint-disable-next-line no-unused-vars
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+
+  navigation: {
+    nextEl: '.slider__arrow-next',
+    prevEl: '.slider__arrow-prev',
+  },
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
