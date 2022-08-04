8e6bf4a67d946f13d203950eab3c1e8c1fcb7322
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-08-04 21:07:50 +0300

    layout Dia alpha

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,15 @@
-'use strict';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+const swiper = new Swiper('.swiper', {
+  modules: [Navigation],
+  navigation: {
+    prevEl: '.slider__arrow-left--prev',
+    nextEl: '.slider__arrow-right--next',
+  },
+});
+
+swiper.init();
 
 const form = document.getElementById('form');
 
