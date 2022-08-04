86dcab99431d525d5af4303d8962034d15cef9d8
Igor Bondarenko <igorbondarenko35@gmail.com>
2022-08-04 12:56:42 +0300

    part of footer was done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -25,26 +25,9 @@ document.getElementById('mob-logo').addEventListener('click', () => {
   document.body.classList.remove('fixed-position');
 });
 
-// const btn = document.getElementById('button');
+const form = document.getElementById('form');
 
-// btn.addEventListener('click', function handleClick(event) {
-//   event.preventDefault();
-
-//   const firstNameInput = document.getElementById('first_name');
-//   const email = document.getElementById('email');
-//   const message = document.getElementById('message');
-
-//   firstNameInput.value = '';
-//   email.value = '';
-//   message.value = '';
-// });
-
-document.getElementById('toggle-label').addEventListener('click', () => {
-  const checkthis = document.getElementById('toggle');
-
-  if (checkthis.checked) {
-    document.body.classList.remove('fixed-position');
-  } else {
-    document.body.classList.add('fixed-position');
-  }
+form.addEventListener('submit', function handleClick(e) {
+  e.preventDefault();
+  e.target.reset();
 });
