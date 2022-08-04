95621c01321c444fa8da6724eee365fc0e592256
Ziatiuk Serhii <sergey.zyat.tvoy@gmail.com>
2022-08-04 17:47:27 +0300

    added sections footer and header without swiper

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,19 @@
-// import Swiper from 'swiper';
-// import 'swiper/css';
+import Swiper from 'swiper';
+import 'swiper/swiper-bundle.css';
 
-// const swiper = new Swiper();
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+swiper.init();
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
