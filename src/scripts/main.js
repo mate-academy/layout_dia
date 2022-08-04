6c0b0d2d567d293f03cfd817ebc018f074c8a9ed
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-04 18:10:44 +0300

    finish landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
-'use strict';
+import Swiper, { Navigation } from 'swiper';
+import 'swiper/swiper-bundle.min.css';
+
+const swiper = new Swiper('.swiper', {
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  modules: [Navigation],
+});
+
+swiper.init();
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
