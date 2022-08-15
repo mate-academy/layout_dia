997d9800503bc1c302d93674942d49e29a1721eb
Denys Politov <dpolitov2001@gmail.com>
2022-08-15 18:12:22 +0300

    fixed mobile nav menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,10 +5,7 @@ const menu = document.querySelector('.section--menu');
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     menu.classList.add('section--menu-shown');
+  } else {
+    menu.classList.remove('section--menu-shown');
   }
-});
-
-window.addEventListener('wheel', () => {
-  menu.classList.remove('section--menu-shown');
-  window.location.hash = '#';
 });
