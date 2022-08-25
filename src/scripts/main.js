da13cca494feab8274beff89babbadecdffb7057
Andriy Andrieiev <proph7000@gmail.com>
2022-08-25 16:56:57 +0300

    fixes according to the comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,3 +8,24 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const submit = document.getElementById('submit');
+
+submit.addEventListener('click', function handleClick(event) {
+  // if you are submitting a form (prevents page reload)
+  event.preventDefault();
+
+  const emailInput = document.getElementById('email');
+  const nameInput = document.getElementById('name');
+  const messageInput = document.getElementById('message');
+
+  // Send value to server
+  console.log(emailInput.value);
+  console.log(nameInput.value);
+  console.log(messageInput.value);
+
+  // clear input field
+  emailInput.value = '';
+  nameInput.value = '';
+  messageInput.value = '';
+});
