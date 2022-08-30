9a5bf4868f7eb75d0a5cfa6ee481f060fc5188f3
Vitaliy Beketov <beketov2403@gmail.com>
2022-08-30 18:32:04 +0300

    2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+const form = document.getElementById('form');
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+})   
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#burger-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
