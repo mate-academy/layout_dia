b07fb0f6a0d5042cc63d2c021b09b59caf20d8b8
Paul Voronin <voron.paul@gmail.com>
2022-09-03 22:25:22 +0300

    ver1.0

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  // direction: 'vertical',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.agency__slider-arrow.swiper-button-next',
+    prevEl: '.agency__slider-arrow.swiper-button-prev',
+  },
+});
+
+/* eslint-enable no-unused-vars */
+/* eslint-enable no-undef */
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page--with-menu');
+  } else {
+    document.body.classList.remove('page--with-menu');
+  }
+});
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  form.reset();
+});
