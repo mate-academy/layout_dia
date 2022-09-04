faed22580777456bd81a932ab20679fa457487cf
Andriy Andrieiev <proph7000@gmail.com>
2022-09-04 13:19:24 +0300

    changed color of favicon and implemented clearing form fields

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,23 +9,30 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const submit = document.getElementById('submit');
+// const submit = document.getElementById('submit');
 
-submit.addEventListener('click', function handleClick(event) {
-  // if you are submitting a form (prevents page reload)
+// submit.addEventListener('click', function handleClick(event) {
+//   // if you are submitting a form (prevents page reload)
+//   event.preventDefault();
+
+//   const emailInput = document.getElementById('email');
+//   const nameInput = document.getElementById('name');
+//   const messageInput = document.getElementById('message');
+
+//   // Send value to server
+//   // console.log(emailInput.value);
+//   // console.log(nameInput.value);
+//   // console.log(messageInput.value);
+
+//   // clear input field
+//   emailInput.value = '';
+//   nameInput.value = '';
+//   messageInput.value = '';
+// });
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  const emailInput = document.getElementById('email');
-  const nameInput = document.getElementById('name');
-  const messageInput = document.getElementById('message');
-
-  // Send value to server
-  // console.log(emailInput.value);
-  // console.log(nameInput.value);
-  // console.log(messageInput.value);
-
-  // clear input field
-  emailInput.value = '';
-  nameInput.value = '';
-  messageInput.value = '';
+  event.target.reset();
 });
