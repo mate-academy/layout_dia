5803c78178f90e76348df9abea08075448933721
Mariia Hatilova <popova.mariia17@gmail.com>
2022-09-05 16:01:52 +0300

    added code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const contactUsForm = document.querySelector('.form-field--area');
+
+contactUsForm.addEventListener('submit', ev => {
+  ev.preventDefault();
+  contactUsForm.reset();
+});
