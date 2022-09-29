fd2ff0a99807c062b0a3182a3915a46110bf7200
Iryna Konovalova <iruna191020@gmail.com>
2022-09-29 13:42:15 +0200

    Implement Slider, Who we are, Our expertise, Process, Testimonials blocks

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,33 @@
 'use strict';
+
+let slideIndex = 1;
+
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  let i;
+  const x = document.getElementsByClassName('slider__image');
+
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = 'none';
+  }
+  x[slideIndex - 1].style.display = 'block';
+}
+
+const leftBtn = document.getElementsByClassName('slider__link--left')[0];
+const rightBtn = document.getElementsByClassName('slider__link--right')[0];
+
+leftBtn.addEventListener('click', () => plusDivs(-1));
+rightBtn.addEventListener('click', () => plusDivs(1));
