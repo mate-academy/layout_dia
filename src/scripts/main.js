651cdf7d6c5b650b2f9d2ba5836bb1e655ea45f2
Vitaliy Anochin <anuhin.vital@gmail.com>
2022-10-16 21:31:01 +0300

    dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,40 @@
 'use strict';
+
+const next = document.querySelectorAll('.slide__circle--next');
+const prev = document.querySelectorAll('.slide__circle--prev');
+const slides = document.querySelector('.slider__slides');
+const countOfSlides = slides.children.length;
+
+let position = 0;
+
+for (let i = 0; i < countOfSlides; i++) {
+  next[i].addEventListener('click', moveNext);
+  prev[i].addEventListener('click', movePrev);
+}
+
+function moveNext() {
+  position++;
+
+  if (position === countOfSlides) {
+    position = 0;
+  }
+
+  slides.style.transform = `translateX(${-position * 100}%)`;
+}
+
+function movePrev() {
+  position--;
+
+  if (position < 0) {
+    position = countOfSlides - 1;
+  }
+
+  slides.style.transform = `translateX(${-position * 100}%)`;
+}
+
+const form = document.querySelector('.footer__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
