6cda7f0074a71959f5eb6df7110685600f6446fa
victor buldenko <gookidoo@gmail.com>
2022-10-29 01:44:24 +0300

    pre-done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#mobile-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.querySelector('.footer__button').addEventListener('click', e => {
+  e.preventDefault();
+  document.querySelector('.footer__form').reset();
+});
