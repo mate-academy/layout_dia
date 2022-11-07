3808a31b2b720d1493a867a33b49924fed11422a
Mykhailo Mokhoruk <Michael119Nr@gmail.com>
2022-11-07 18:10:37 +0200

    created header and footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,19 @@
 'use strict';
+
+const header = document.querySelector('.header');
+const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.icon-menu');
+
+header.addEventListener('click', (evt) => {
+  if (evt.target.className === 'icon-menu'
+      || evt.target.className === 'icon-menu icon-menu--active') {
+    menu.classList.toggle('header__menu--active');
+    menuIcon.classList.toggle('icon-menu--active');
+  }
+
+  if (evt.target.tagName === 'A'
+      || evt.target.tagName === 'IMG') {
+    menu.classList.remove('header__menu--active');
+    menuIcon.classList.remove('icon-menu--active');
+  }
+});
