74c4ddf8fc47e61234137db10f9bae3899afa3df
Demut <uddarendeli1@gmail.com>
2022-11-10 17:21:41 +0300

    sections are added, checklist is completed except the gallery

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const button = document.getElementById('form-submit');
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  window.location.href = window.location.origin;
+});
