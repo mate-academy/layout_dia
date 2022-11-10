2fe0292818c9c61b620dc8366afef3acde287177
Demut <uddarendeli1@gmail.com>
2022-11-10 17:59:51 +0300

    gallery / slideshow added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,41 @@
 'use strict';
 
-const button = document.getElementById('form-submit');
+window.addEventListener('load', () => {
+  const button = document.getElementById('form-submit');
 
-button.addEventListener('click', (e) => {
-  e.preventDefault();
-  window.location.href = window.location.origin;
+  button.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location.href = window.location.origin;
+  });
+
+  const slideshowItem = document.getElementsByClassName('slideshow__item')[0];
+  const slideshowLeft = document.getElementById('slideshow_left');
+  const slideshowRight = document.getElementById('slideshow_right');
+
+  const slide = (num) => {
+    const currentClass = slideshowItem.classList[1];
+    let newOrder = (Number.parseInt(
+      currentClass[currentClass.length - 1]) + num
+    );
+
+    if (newOrder === 0) {
+      newOrder = 4;
+    } else if (newOrder === 5) {
+      newOrder = 1;
+    }
+
+    slideshowItem.classList.remove(currentClass);
+
+    const newClass = currentClass.substring(0, currentClass.length - 1) + newOrder.toString();
+
+    slideshowItem.classList.add(newClass);
+  };
+
+  slideshowLeft.addEventListener('click', () => {
+    slide(-1);
+  });
+
+  slideshowRight.addEventListener('click', () => {
+    slide(1);
+  });
 });
