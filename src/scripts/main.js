6e4b4a5614e6f98a3fa52a1c1003dce6ed512503
Oleksandr Maslo <98667890az@gmail.com>
2022-11-11 11:28:42 +0200

    Dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu--mobile');
+  } else {
+    document.body.classList.remove('page__body--with-menu--mobile');
+  }
+});
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
+
+const textarea = document.querySelector('textarea');
+
+textarea.addEventListener('keyup', function() {
+  if (this.scrollTop > 0) {
+    this.style.height = this.scrollHeight + 'px';
+  }
+});
