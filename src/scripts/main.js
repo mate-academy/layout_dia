3053e44846004e8530693735b23cbdb4b0f37111
Mykhailo Mokhoruk <Michael119Nr@gmail.com>
2022-11-12 21:21:32 +0200

    created main

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,28 @@
 'use strict';
 
 const header = document.querySelector('.header');
-const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu__nav');
 const menuIcon = document.querySelector('.icon-menu');
+
+const form = document.querySelector('.contact-form__form');
+
+form.addEventListener('submit', buttonClick, false);
+
+function buttonClick(event) {
+  event.preventDefault();
+  form.reset();
+};
 
 header.addEventListener('click', (evt) => {
   if (evt.target.className === 'icon-menu'
       || evt.target.className === 'icon-menu icon-menu--active') {
-    menu.classList.toggle('header__menu--active');
+    menuNav.classList.toggle('menu__nav--active');
     menuIcon.classList.toggle('icon-menu--active');
   }
 
   if (evt.target.tagName === 'A'
       || evt.target.tagName === 'IMG') {
-    menu.classList.remove('header__menu--active');
+    menuNav.classList.remove('menu__nav--active');
     menuIcon.classList.remove('icon-menu--active');
   }
 });
