1ffbc4ac1dad9487ccc07e433f7c6d4bd0d1c2f9
NataliiaHen <nataliehenda1296@gmail.com>
2022-11-16 00:09:40 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(evt) {
+  evt.preventDefault();
+  form.reset();
+  window.scrollTo(0, 0);
+}
