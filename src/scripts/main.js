1d3d41883208655d9f28c64b1f9b6e922f661dac
Andriy Andrieiev <proph7000@gmail.com>
2022-11-16 12:14:21 +0200

    Added slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Overflow: hidden for .header__nav
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#nav') {
     document.body.classList.add('page__body--with-menu');
@@ -9,30 +8,40 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// const submit = document.getElementById('submit');
-
-// submit.addEventListener('click', function handleClick(event) {
-//   // if you are submitting a form (prevents page reload)
-//   event.preventDefault();
-
-//   const emailInput = document.getElementById('email');
-//   const nameInput = document.getElementById('name');
-//   const messageInput = document.getElementById('message');
-
-//   // Send value to server
-//   // console.log(emailInput.value);
-//   // console.log(nameInput.value);
-//   // console.log(messageInput.value);
-
-//   // clear input field
-//   emailInput.value = '';
-//   nameInput.value = '';
-//   messageInput.value = '';
-// });
-
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   event.target.reset();
 });
+
+const slider = [...document.querySelectorAll('.slider__image')];
+const arrowRight = document.querySelector('.slider__button--right');
+const arrowLeft = document.querySelector('.slider__button--left');
+
+arrowRight.addEventListener('click', () => moveRight(slider));
+arrowLeft.addEventListener('click', () => moveLeft(slider));
+
+function moveRight(images) {
+  for (let i = 0; i < images.length; i++) {
+    if (i > 0 && images[i].classList.contains('slider__image--center')) {
+      images[i].classList.remove('slider__image--center');
+      images[i].classList.add('slider__image--to-right');
+      images[i - 1].classList.remove('slider__image--to-left');
+      images[i - 1].classList.add('slider__image--center');
+    }
+  }
+}
+
+function moveLeft(images) {
+  for (let i = images.length - 1; i >= 0; i--) {
+    if (i < images.length - 1 && images[i].classList.contains(
+      'slider__image--center'
+    )) {
+      images[i].classList.remove('slider__image--center');
+      images[i].classList.add('slider__image--to-left');
+      images[i + 1].classList.remove('slider__image--to-right');
+      images[i + 1].classList.add('slider__image--center');
+    }
+  }
+}
