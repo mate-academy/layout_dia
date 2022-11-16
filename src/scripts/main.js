68cf440293742a5fcbff6b4ea003a2c0a32e73c4
Andriy Andrieiev <proph7000@gmail.com>
2022-11-16 20:22:56 +0200

    Added disabled-mode for slider buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,7 +23,13 @@ arrowRight.addEventListener('click', () => moveRight(slider));
 arrowLeft.addEventListener('click', () => moveLeft(slider));
 
 function moveRight(images) {
+  arrowLeft.removeAttribute('disabled');
+
   for (let i = 0; i < images.length; i++) {
+    if (i === 1 && images[i].classList.contains('slider__image--center')) {
+      arrowRight.setAttribute('disabled', 'true');
+    }
+
     if (i > 0 && images[i].classList.contains('slider__image--center')) {
       images[i].classList.remove('slider__image--center');
       images[i].classList.add('slider__image--to-right');
@@ -34,10 +40,17 @@ function moveRight(images) {
 }
 
 function moveLeft(images) {
+  arrowRight.removeAttribute('disabled');
+
   for (let i = images.length - 1; i >= 0; i--) {
-    if (i < images.length - 1 && images[i].classList.contains(
-      'slider__image--center'
-    )) {
+    if (
+      i === images.length - 2
+      && images[i].classList.contains('slider__image--center')
+    ) {
+      arrowLeft.setAttribute('disabled', 'true');
+    }
+
+    if (images[i].classList.contains('slider__image--center')) {
       images[i].classList.remove('slider__image--center');
       images[i].classList.add('slider__image--to-left');
       images[i + 1].classList.remove('slider__image--to-right');
