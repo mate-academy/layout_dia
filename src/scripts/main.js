6181ae3ed84869e93ab57283806eb7786c8dd642
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-11-20 00:25:21 +0100

    slider is added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Toggle Menu
 const menuButton = document.querySelector('#menu');
 const closeButton = document.querySelector('#cross');
 const nav = document.querySelector('.nav');
@@ -27,3 +28,65 @@ navLinks.forEach(link => {
     document.body.classList.remove('page__body--with-menu');
   });
 });
+
+// Header Slider
+createSlider(document.querySelector('#slider'));
+
+function createSlider(slider) {
+  // Find required parts inside a given slider element
+  const list = slider.querySelector('.slider__list');
+  const prev = slider.querySelector('.slider__arrow-left');
+  const next = slider.querySelector('.slider__arrow-right');
+
+  // Find the number of slides
+  const count = list.children.length;
+
+  // Index of an active slide
+  let position = 0;
+
+  // Initilize the slider to show slide 0
+  moveTo(0);
+
+  function moveTo(newPosition) {
+    // If we want to scroll to the first slide after the last
+    // or to the last slide after the first
+    position = normalizePozition(newPosition);
+
+    // scroll the slides by required number of slides
+    list.style.transform = `translateX(${-position * 100}%)`;
+  }
+
+  function moveRight() {
+    moveTo(position + 1);
+  }
+
+  function moveLeft() {
+    moveTo(position - 1);
+  }
+
+  function normalizePozition(newPosition) {
+    let finalPositions = newPosition % count;
+
+    if (finalPositions < 0) {
+      finalPositions += count;
+    }
+
+    return finalPositions;
+  }
+
+  // when clicked, scroll in the desired direction
+  next.addEventListener('click', function() {
+    moveRight();
+  });
+
+  prev.addEventListener('click', function() {
+    moveLeft();
+  });
+
+  // return an object with control methods
+  return {
+    moveTo,
+    moveRight,
+    moveLeft,
+  };
+}
