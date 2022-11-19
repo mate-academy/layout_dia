738ac67dd988eca9ea4848ffd8c9b700e814644d
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-11-19 19:00:59 +0100

    initial commit with header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const menuButton = document.querySelector('#menu');
+const closeButton = document.querySelector('#cross');
+const nav = document.querySelector('.nav');
+const navLinks = document.querySelectorAll('.nav__link');
+
+menuButton.addEventListener('click', () => {
+  document.body.classList.add('page__body--with-menu');
+  nav.classList.add('nav--active');
+  menuButton.classList.add('hidden');
+  closeButton.classList.remove('hidden');
+});
+
+closeButton.addEventListener('click', () => {
+  nav.classList.remove('nav--active');
+  menuButton.classList.remove('hidden');
+  closeButton.classList.add('hidden');
+  document.body.classList.remove('page__body--with-menu');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('nav--active');
+    menuButton.classList.remove('hidden');
+    closeButton.classList.add('hidden');
+    document.body.classList.remove('page__body--with-menu');
+  });
+});
