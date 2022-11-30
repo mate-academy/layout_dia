8f752921cae4872d76ced82f0cbf881dee5fc3d9
KseniyaGonchar <kseniyadf888@gmail.com>
2022-11-30 21:39:09 +0200

    changes added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,67 @@
 'use strict';
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
-  } else {
-    document.body.classList.remove('page__body--with-menu');
+document.addEventListener('DOMContentLoaded', function() {
+  window.addEventListener('hashchange', () => {
+    if (window.location.hash === '#menu') {
+      document.body.classList.add('page__body--with-menu');
+    } else {
+      document.body.classList.remove('page__body--with-menu');
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const images = document
+    .querySelectorAll('.header__slider.header__slider--line .img');
+  const sliderLine = document.querySelector('.header__slider--line');
+  let count = 0;
+  let width;
+
+  function init() {
+    width = document.querySelector('.header__slider').offsetWidth;
+    sliderLine.style.width = width * images.length + 'px';
+
+    images.forEach(item => {
+      item.style.width = width + 'px';
+      item.style.height = 'auto';
+    });
+    rollSlider();
+  }
+
+  window.addEventListener('resize', init);
+  init();
+
+  document.querySelector('.slider__control-prev')
+    .addEventListener('click', function() {
+      count--;
+
+      if (count < 0) {
+        count = images.length - 1;
+      }
+      rollSlider();
+    });
+
+  document.querySelector('.slider__control-next')
+    .addEventListener('click', function() {
+      count++;
+
+      if (count >= images.length) {
+        count = 0;
+      }
+      rollSlider();
+    });
+
+  function rollSlider() {
+    sliderLine.style.transform = 'translate(-' + count * width + 'px)';
   }
 });
 
-const form = document.querySelector('.form');
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.querySelector('.form');
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
 
-  form.reset();
+    form.reset();
+  });
 });
