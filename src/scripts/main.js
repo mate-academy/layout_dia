667085d88de9cb365f802d9b7d281bfbf5a681a3
NataliiaHen <nataliehenda1296@gmail.com>
2022-12-04 21:15:10 +0200

    Add mobile menu, change styles for  footer, form doesn`t lead to the top fo page after submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,13 @@
 'use strict';
 
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', formSubmit);
@@ -7,5 +15,4 @@ form.addEventListener('submit', formSubmit);
 function formSubmit(evt) {
   evt.preventDefault();
   form.reset();
-  window.scrollTo(0, 0);
 }
