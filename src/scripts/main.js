6c1c1c1b9169f8efbdb8880c9ebffb5a9f722da8
Дмитро Ніколенко <dmitronikolenko@MacBook-Air-Dmitro.local>
2022-12-08 16:13:37 +0200

    created slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,45 @@
 'use strict';
+import baseSliderImg from '../images/slider/slide-img-0.png';
+import firstSliderImg from '../images/slider/slide-img-1.jpg'
+import secondSliderImg from '../images/slider/slide-img-2.jpg'
+import thirdSliderImg from '../images/slider/slide-img-3.jpg'
 
-window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--menu-open');
+const paginationButtons = document.querySelector('.slider_arrows');
+const slider = document.querySelector('.slider');
+
+const images = [baseSliderImg, firstSliderImg, secondSliderImg, thirdSliderImg];
+let visibleImgIndex = 0;
+
+const moveLeft = () => {
+  if (visibleImgIndex === 0) {
+    visibleImgIndex = images.length - 1;
   } else {
-    document.body.classList.remove('page__body--menu-open');
+    visibleImgIndex -= 1;
+  }
+};
+
+const moveRight = () => {
+  if (visibleImgIndex === images.length - 1) {
+    visibleImgIndex = 0
+  } else {
+    visibleImgIndex += 1;
+  }
+};
+
+paginationButtons.addEventListener('click', (e) => {
+  if (e.target.dataset.direction) {
+    const { direction } = e.target.dataset;
+
+    switch (direction) {
+      case 'left':
+        moveLeft();
+        break;
+
+      case 'right':
+        moveRight();
+        break;
+    }
+
+    slider.style.backgroundImage = `url("${images[visibleImgIndex]}")`;
   }
 });
