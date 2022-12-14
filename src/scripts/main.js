753ed5d5b49f070efc482c17275132dbf567b618
Nadiya Ovsiyenko <nadia.ovsienko1009@gmail.com>
2022-12-14 22:19:22 +0100

    Air layout initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', submitClick, false);
+
+function submitClick(event) {
+  event.preventDefault();
+}
