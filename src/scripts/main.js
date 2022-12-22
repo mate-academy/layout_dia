6249002cdd5ddb7346131aa432d6d15bd1dbd680
Taras Bida <tatasbida@gmail.com>
2022-12-23 00:32:44 +0200

    added Dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#slide2') {
+    document.getElementById('slider').classList.add('slider--slide2');
+  } else {
+    document.getElementById('slider').classList.remove('slider--slide2');
+  }
+});
