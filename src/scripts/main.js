55968bd0d1c4af41746601b48e2121891db04f90
metin <metinbicaksiz@gmail.com>
2022-12-26 19:47:00 +0300

    applied the changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('.form__form');
+const formField = document.querySelectorAll('.form__field');
+const submitButton = document.querySelector('.button--submit');
+
+function setRequired() {
+  formField.forEach(field => {
+    field.setAttribute('required', '');
+  });
+}
+
+submitButton.addEventListener('click', setRequired);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
