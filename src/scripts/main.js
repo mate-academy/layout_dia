3eed822d20aeb158e6db3c5530215c93285fba4f
Oleksandr Kulibaba <alks.aesthetic@gmail.com>
2022-12-31 15:00:40 +0200

    added slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,45 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const form = document.querySelector('.contact-us__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
+
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+const images = document.querySelector('.slider__content').children;
+const totalImages = images.length;
+let index = 0;
+
+prev.addEventListener('click', () => {
+  nextImage('next');
+});
+
+next.addEventListener('click', () => {
+  nextImage('prev');
+});
+
+function nextImage(direction) {
+  if (direction === 'next') {
+    index++;
+
+    if (index === totalImages) {
+      index = 0;
+    }
+  } else {
+    if (index === 0) {
+      index = totalImages - 1;
+    } else {
+      index--;
+    }
+  }
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.remove('slider__slide--active');
+  }
+  images[index].classList.add('slider__slide--active');
+}
