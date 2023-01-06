5afff4166117b425e10a41faf465092e97586b61
metin <metinbicaksiz@gmail.com>
2023-01-06 20:28:54 +0300

    applied the changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,11 @@ form.addEventListener('submit', (event) => {
   event.preventDefault();
   form.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--no-scroll');
+  } else {
+    document.body.classList.remove('page__body--no-scroll');
+  }
+});
