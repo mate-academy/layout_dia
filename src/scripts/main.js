ebf5e067f8116c8a71cbf0d090e79b91287eedd0
GUSILLUS <zebela2002gmail.com>
2023-01-09 21:11:24 +0200

    header v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+/* eslint-disable no-unused-vars */
+const slider = new window.Swiper('.slider', {
+  navigation: {
+    nextEl: '.slider__button-next',
+    prevEl: '.slider__button-prev',
+  },
+});
+/* eslint-disable no-unused-vars */
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu');
+  } else {
+    document.body.classList.remove('page__body--menu');
+  }
+});
+
+const form = document.querySelector('form');
+
+function submit(event) {
+  event.preventDefault();
+  form.reset();
+}
+
+form.addEventListener('submit', submit);
