4cb90580fdd80f65807d286ee840f2a7a767a2d3
ivanm2706 <ivanmorhun664@gmail.com>
2023-01-24 17:57:17 +0200

    added menu, finished header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const icon = document.querySelector('.icon');
+const menu = document.querySelector('.header__menu');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash !== '#menu') {
+    icon.classList.remove('icon--close');
+    menu.classList.remove('header__menu--open');
+  }
+});
+
+icon.addEventListener('click', (e) => {
+  icon.classList.toggle('icon--close');
+  menu.classList.toggle('header__menu--open');
+});
