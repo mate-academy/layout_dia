eab437f79b620eb5a081163d5e376e8e3a9d7d68
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-01-31 20:58:14 +0100

    complete header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const slides = document.querySelectorAll('.slider__slide');
+const nextSlide = document.querySelector('.slider__btn--next');
+
+let curSlide = 0;
+
+const maxSlide = slides.length - 1;
+
+nextSlide.addEventListener('click', function() {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+
+    if (slide.style.transform !== `translateX(${0}%)`) {
+      slide.style.display = `none`;
+    } else {
+      slide.style.display = `block`;
+    }
+  });
+});
+
+const prevSlide = document.querySelector('.slider__btn--prev');
+
+prevSlide.addEventListener('click', function() {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+
+    if (slide.style.transform !== `translateX(${0}%)`) {
+      slide.style.display = `none`;
+    } else {
+      slide.style.display = `block`;
+    }
+  });
+});
