d2d8267d77dbd86c19aed4480a6c178de7b3dba8
Roman Kulchytskyi-Dashynych <kdrs0812@gmail.com>
2023-02-01 19:37:48 +0200

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -45,3 +45,16 @@ if (window.location.hash === '#menu') {
 } else {
   document.body.classList.remove('page__body--with-menu');
 };
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form-field--input');
+const textarea = document.querySelector('.form-field--textarea');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+  textarea.value = '';
+});
