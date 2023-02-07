780bd40a4ab8b3605a64c9218b595f9c404f3173
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-07 22:06:35 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function stopDefAction(evt) {
+  evt.preventDefault();
+  document.form.reset();
+}
+
+document.getElementById('form').addEventListener(
+  'submit', stopDefAction, false);
