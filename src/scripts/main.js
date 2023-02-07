2b94a4993139d32eac0373897b062b88d13b3b28
Natalia Matash <natalimatash@gmail.com>
2023-02-07 17:35:16 +0200

    Build an image carousel

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+const slides = document.querySelectorAll('.slider__image');
+
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+let curSlide = 0;
+
+const nextSlide = document.querySelector('.slider__arrow-right');
+
+nextSlide.addEventListener('click', function() {
+  curSlide++;
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const maxSlide = 3;
+
+nextSlide.addEventListener('click', function() {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else if (curSlide === 1) {
+    curSlide = 1;
+  } else if (curSlide === 2) {
+    curSlide = 2;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+const prevSlide = document.querySelector('.slider__arrow-left');
+
+prevSlide.addEventListener('click', function() {
+  if (curSlide === 0) {
+    curSlide = 2;
+  } else {
+    curSlide--;
+  }
+
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
