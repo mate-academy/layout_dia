61127bf7d8b639b0b3e5d1c450bc97251fa019f1
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-08 20:58:35 +0200

    menu added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -9,3 +9,11 @@ function stopDefAction(evt) {
   evt.preventDefault();
   document.form.reset();
 }
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
