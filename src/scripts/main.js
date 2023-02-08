8bfe9ba1015c7c8bf30828c30b4e7f8ccb603c2e
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-08 16:01:22 +0200

    comments implemented

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
+window.onload = function() {
+  document.getElementById('form').addEventListener(
+    'submit', stopDefAction, false);
+};
+
 function stopDefAction(evt) {
   evt.preventDefault();
   document.form.reset();
 }
-
-document.getElementById('form').addEventListener(
-  'submit', stopDefAction, false);
