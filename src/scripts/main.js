e8e0b8b0e865b73b67d7b1d87509e7781ae079f1
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-02-11 13:42:32 +0100

    change OS, dont make full task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -44,3 +44,11 @@ prevSlide.addEventListener('click', function() {
     }
   });
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
