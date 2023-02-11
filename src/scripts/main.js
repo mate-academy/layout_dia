138320d400c4fb509a653c06cbe790a993b66dae
Natalia Matash <natalimatash@gmail.com>
2023-02-11 10:00:51 +0200

    Created MENU section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -53,5 +53,13 @@ prevSlide.addEventListener('click', function(event) {
 
 document.getElementById('form').addEventListener('submit', (event) => {
   event.preventDefault();
-  document.getElementById('git commit -amform').reset();
+  document.getElementById('form').reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
