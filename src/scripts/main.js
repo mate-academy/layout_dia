54dcd5e1fa78263bdf2f8ccc1b2286d6e4b8edc9
Dmytro Yevtyeryev <begoodeee@gmail.com>
2023-02-16 12:56:26 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,3 +7,17 @@ window.addEventListener('hashchange', () => {
     document.body.classList.remove('page__body--with-menu');
   }
 });
+
+const formSubmit = document.querySelector('#send-us-message-form');
+
+formSubmit.addEventListener('submit', formSubmitSuccess, false);
+
+function formSubmitSuccess(event) {
+  const warn = 'Thank you for sending you message!<br>';
+
+  document.getElementById('output-box').innerHTML = warn;
+
+  document.getElementById('send-us-message-form').reset();
+
+  event.preventDefault();
+}
