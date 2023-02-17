b0848f7750dc6de71db024abca585d65f69dc013
Maksym Shevelin <shevmax01@gmail.com>
2023-02-17 12:42:46 +0200

    VPR, footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#side-menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+// const arrows = document.querySelectorAll('.slider__arrow');
+
+// let counter = 1;
+
+// arrows.forEach(arrow => {
+//   arrow.addEventListener('click', function() {
+//     const slider = arrow.closest('.slider');
+//     counter++;
+
+//     if (counter > 5) {
+//       counter = 1;
+//     }
+
+//     slider.style.backgroundImage = `url(./images/slider/img-${counter}.png)`;
+//   });
+// });
