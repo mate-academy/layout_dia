df5ba09105c3b72dfec763d00ab0e3be40e9ece8
Igor Galkin <igor_galkin@ukr.net>
2023-03-01 00:10:07 +0200

    3rd commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,5 @@
 'use strict';
 
-// const faviconEl = document.querySelector('link[rel="icon"]');
-
-// // watch for changes 🕵️
-// const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
-
-// mediaQuery.addEventListener('change', themeChange);
-
-// function themeChange(event) {
-//   if (event.matches) {
-//     faviconEl.setAttribute('href', 'favicon-dark.png');
-//   } else {
-//     faviconEl.setAttribute('href', 'favicon-light.png');
-//   };
-// }
-
 const form = document.querySelector('.contact-us__form');
 const inputs = document.querySelectorAll('.contact-us__input');
 const textarea = document.querySelector('.form-field--textarea');
@@ -29,20 +14,10 @@ form.addEventListener('submit', function(event) {
   textarea.value = '';
 });
 
-// // Get a reference to the button element
-// const button = document.querySelector('.contact-us__button');
-
-// // Add an event listener to the button element
-// button.addEventListener('click', function(event) {
-//   // Prevent the default form submission behavior
-//   event.preventDefault();
-
-//   // Your custom code here...
-// });
-
-// const form = document.querySelector('.contact-us__form');
-
-// form.addEventListener('submit', (e) => {
-//   e.preventDefault();
-//   form.reset();
-// });
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
