15fe296fc44db37c0c74c5d53e732f3cb1c12432
EhorSaprykin <astralik1993@gmail.com>
2023-03-02 14:34:52 +0200

    Gallery done&block's sort

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,43 @@
 'use strict';
+
+const nextButton = document.querySelector('.header__gallery-button-next');
+const previousButton = document
+  .querySelector('.header__gallery-button-previous');
+
+const galleryImage1 = document.querySelector('.header__gallery-image-1');
+const galleryImage2 = document.querySelector('.header__gallery-image-2');
+const galleryImage3 = document.querySelector('.header__gallery-image-3');
+
+function nextImage() {
+  if (galleryImage1.style.display === 'block') {
+    galleryImage1.style.display = 'none';
+    galleryImage2.style.display = 'block';
+  } else if (galleryImage2.style.display === 'block') {
+    galleryImage2.style.display = 'none';
+    galleryImage3.style.display = 'block';
+  } else {
+    galleryImage3.style.display = 'none';
+    galleryImage1.style.display = 'block';
+  };
+};
+
+function previousImage() {
+  if (galleryImage1.style.display === 'block') {
+    galleryImage1.style.display = 'none';
+    galleryImage3.style.display = 'block';
+  } else if (galleryImage3.style.display === 'block') {
+    galleryImage3.style.display = 'none';
+    galleryImage2.style.display = 'block';
+  } else {
+    galleryImage2.style.display = 'none';
+    galleryImage1.style.display = 'block';
+  };
+};
+
+nextButton.addEventListener('click', () => {
+  nextImage();
+});
+
+previousButton.addEventListener('click', () => {
+  previousImage();
+});
