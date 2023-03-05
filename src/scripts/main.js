b3a6d4640d1d413afb421340aef276723e4feb62
Nazar Vorona <voron1765@gmail.com>
2023-03-05 16:54:45 +0200

    first commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,46 @@
 'use strict';
+
+const sliderWrapper = document.querySelector('.slider__wrapper');
+const prevButton = document.querySelector('.slider__prev');
+const nextButton = document.querySelector('.slider__next');
+const slideWidth = document.querySelector('.slider__slide').clientWidth;
+
+let slideIndex = 0;
+
+prevButton.addEventListener('click', () => {
+  slideIndex--;
+
+  if (slideIndex < 0) {
+    slideIndex = 2;
+  }
+  sliderWrapper.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+nextButton.addEventListener('click', () => {
+  slideIndex++;
+
+  if (slideIndex > 2) {
+    slideIndex = 0;
+  }
+  sliderWrapper.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+setInterval(() => {
+  slideIndex++;
+
+  if (slideIndex > 2) {
+    slideIndex = 0;
+  }
+  sliderWrapper.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}, 5000);
+
+const form = document.querySelector('.form');
+const inputs = document.querySelectorAll('.form__input');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  inputs.forEach(function(elem) {
+    elem.value = '';
+  });
+});
