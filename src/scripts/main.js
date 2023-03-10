e52a1077e0c338a9ff26b0b7aa52dd4083e1cf45
EhorSaprykin <astralik1993@gmail.com>
2023-03-10 08:11:45 +0200

    all animation and size's done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,46 +8,46 @@ const galleryImage1 = document.querySelector('.header__gallery-image-1');
 const galleryImage2 = document.querySelector('.header__gallery-image-2');
 const galleryImage3 = document.querySelector('.header__gallery-image-3');
 
-function changeImage() {
-  const image = document.querySelector('.header__gallery');
+galleryImage1.style.right = '0%';
 
-  image.style.backgroundImage = 'url(../images/gallery-2.jpg)';
+function nextImage() {
+  if (galleryImage1.style.right === '0%') {
+    galleryImage1.style.right = '100%';
+    galleryImage2.style.right = '0%';
+    galleryImage3.style.right = '-100%';
+  } else if (galleryImage2.style.right === '0%') {
+    galleryImage1.style.right = '200%';
+    galleryImage2.style.right = '100%';
+    galleryImage3.style.right = '0%';
+  } else {
+    galleryImage1.style.right = '0%';
+    galleryImage2.style.right = '-100%';
+    galleryImage3.style.right = '-200%';
+  };
+};
 
-}
-
-// function nextImage() {
-//   if (galleryImage1.style.display === 'block') {
-//     galleryImage1.style.display = 'none';
-//     galleryImage2.style.display = 'block';
-//   } else if (galleryImage2.style.display === 'block') {
-//     galleryImage2.style.display = 'none';
-//     galleryImage3.style.display = 'block';
-//   } else {
-//     galleryImage3.style.display = 'none';
-//     galleryImage1.style.display = 'block';
-//   };
-// };
-
-// function previousImage() {
-//   if (galleryImage1.style.display === 'block') {
-//     galleryImage1.style.display = 'none';
-//     galleryImage3.style.display = 'block';
-//   } else if (galleryImage3.style.display === 'block') {
-//     galleryImage3.style.display = 'none';
-//     galleryImage2.style.display = 'block';
-//   } else {
-//     galleryImage2.style.display = 'none';
-//     galleryImage1.style.display = 'block';
-//   };
-// };
+function previousImage() {
+  if (galleryImage1.style.right === '0%') {
+    galleryImage1.style.right = '200%';
+    galleryImage2.style.right = '100%';
+    galleryImage3.style.right = '0%';
+  } else if (galleryImage3.style.right === '0%') {
+    galleryImage1.style.right = '100%';
+    galleryImage2.style.right = '0%';
+    galleryImage3.style.right = '-100%';
+  } else {
+    galleryImage1.style.right = '0%';
+    galleryImage2.style.right = '-100%';
+    galleryImage3.style.right = '-200%';
+  };
+};
 
 nextButton.addEventListener('click', () => {
-  // nextImage();
-  changeImage();
+  nextImage();
 });
 
 previousButton.addEventListener('click', () => {
-  // previousImage();
+  previousImage();
 });
 
 const body = document.querySelector('body');
@@ -75,3 +75,39 @@ function changePosition(width) {
 }
 
 changePosition(1600);
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+});
+
+const openMenu = document.querySelector('.header__menu-open');
+const closeMenu = document.querySelector('.header__menu-close');
+const nav = document.querySelector('.header__nav--opened');
+
+closeMenu.style.opacity = '0';
+closeMenu.style.pointerEvents = 'none';
+
+openMenu.addEventListener('click', () => {
+  nav.style.opacity = '1';
+  nav.style.pointerEvents = 'all';
+
+  openMenu.style.opacity = '0';
+  openMenu.style.pointerEvents = 'none';
+
+  closeMenu.style.opacity = '1';
+  closeMenu.style.pointerEvents = 'all';
+});
+
+closeMenu.addEventListener('click', () => {
+  openMenu.style.opacity = '1';
+  openMenu.style.pointerEvents = 'all';
+
+  nav.style.opacity = '0';
+  nav.style.pointerEvents = 'none';
+
+  closeMenu.style.opacity = '0';
+  closeMenu.style.pointerEvents = 'none';
+});
