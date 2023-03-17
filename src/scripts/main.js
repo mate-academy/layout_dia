9969a9f99879a525f596c0fe45319c9d925891f3
Mykola Plekan <zikzar@gmail.com>
2023-03-17 16:53:37 +0200

    air page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
-'use strict';
+// eslint-disable-next-line
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
