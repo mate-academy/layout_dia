feb1458d2be97bf072729d85c1d96230f8cb3c3d
JulyaPetrovskaya <119932772+JulyaPetrovskaya@users.noreply.github.com>
2023-03-20 12:37:53 +0200

    js slider was added

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const slides = document.querySelectorAll(".slider__slide");
+  const prevButton = document.querySelector(".icon-arrow--left");
+  const nextButton = document.querySelector(".icon-arrow--right");
+
+  let currentSlideIndex = 0;
+
+  slides[currentSlideIndex].classList.add("slider__slide--active");
+
+  prevButton.addEventListener("click", () => {
+    slides[currentSlideIndex].classList.remove("slider__slide--active");
+
+    if (currentSlideIndex > 0) {
+      currentSlideIndex--;
+    } else {
+      currentSlideIndex = slides.length - 1;
+    }
+
+    slides[currentSlideIndex].classList.add("slider__slide--active");
+  });
+
+  nextButton.addEventListener("click", () => {
+    slides[currentSlideIndex].classList.remove("slider__slide--active");
+
+    if (currentSlideIndex < slides.length - 1) {
+      currentSlideIndex++;
+    } else {
+      currentSlideIndex = 0;
+    }
+    slides[currentSlideIndex].classList.add("slider__slide--active");
+  });
+});
