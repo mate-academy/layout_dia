533f9d86bbbdcd28037c5e5453da94506360fc13
Mykola Plekan <zikzar@gmail.com>
2023-03-20 16:16:24 +0200

    air page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,19 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--hidden');
+  } else {
+    document.body.classList.remove('page__body--hidden');
+  }
+});
+
+const form = document.querySelector('.send__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
