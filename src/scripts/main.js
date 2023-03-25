23a7a0264d0604371a22eb8bc01f569355d60746
Novyk Anton <novyk.anton@gmail.com>
2023-03-25 22:54:40 +0200

    Fix slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Menu
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('page__body--with-menu');
@@ -8,42 +9,48 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('.form');
-const slider = document.querySelector('.slider');
+// Slider
+const prevButton = document.querySelector('.slider__button--left');
+const nextButton = document.querySelector('.slider__button--right');
+let slideIndex = 0;
 
-form.addEventListener('submit', function(event) {
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll('.slider__image');
+
+  if (n > slides.length - 1) {
+    slideIndex = 0;
+  }
+
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
+
+prevButton.addEventListener('click', () => plusSlide(-1));
+nextButton.addEventListener('click', () => plusSlide(1));
+
+// setInterval(function() {
+//   plusSlide(1);
+// }, 4000);
+
+// Form
+const form = document.querySelector('.contact__form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
   event.preventDefault();
-
-  form.reset();
-});
-
-slider.addEventListener('click', (obj) => {
-  obj.preventDefault();
-
-  const arraySliders = [...slider.children];
-
-  if (obj.target.classList.contains('slider__img-left')) {
-    getNewSlider(obj.target, arraySliders, 'left');
-  }
-
-  if (obj.target.classList.contains('slider__img-right')) {
-    getNewSlider(obj.target, arraySliders, 'right');
-  }
-});
-
-function getNewSlider(target, arraySliders, side) {
-  const currentSlider = target.closest('.slider__background');
-
-  const i = arraySliders.indexOf(currentSlider);
-
-  const maxIndex = arraySliders.length - 1;
-  const nextSlider = side === 'left' ? (
-    i > 0 ? arraySliders[i - 1] : arraySliders[maxIndex])
-    : (i < maxIndex ? arraySliders[i + 1] : arraySliders[0]);
-
-  currentSlider.style.visibility = 'hidden';
-  currentSlider.style.position = 'absolute';
-
-  nextSlider.style.visibility = 'visible';
-  nextSlider.style.position = 'static';
+  this.reset();
 }
