b63c88b6c2fce526af4a0c26cfb11b81c6689cea
Vadivel-Babu <vadivelbabu1996@gmail.com>
2023-03-29 12:30:09 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,33 +1,35 @@
 /* eslint-disable no-console */
-"use strict";
+'use strict';
 
-window.addEventListener("hashchange", () => {
-  if (window.location.hash === "#menu") {
-    document.body.classList.add("menu__open");
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('menu__open');
   } else {
-    document.body.classList.remove("menu__open");
+    document.body.classList.remove('menu__open');
   }
 });
 
-const btn = document.querySelector(".button-submit");
-const Name = document.querySelector(".input__name");
-const Email = document.querySelector(".input__email");
-const comment = document.querySelector(".input__message");
+const btn = document.querySelector('.button-submit');
+const Name = document.querySelector('.input__name');
+const Email = document.querySelector('.input__email');
+const comment = document.querySelector('.input__message');
+
 console.log(btn);
-const slides = document.querySelectorAll(".slide");
-const nextBtn = document.querySelector(".next-slider");
-const prevBtn = document.querySelector(".prev-slider");
+
+const slides = document.querySelectorAll('.slide');
+const nextBtn = document.querySelector('.next-slider');
+const prevBtn = document.querySelector('.prev-slider');
 
 let currSlide = 0;
 const maxSlide = slides.length;
 
-const goToSlide = function (slide) {
+const goToSlide = function(slide) {
   slides.forEach(
     (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
   );
 };
 
-nextBtn.addEventListener("click", () => {
+nextBtn.addEventListener('click', () => {
   if (currSlide === maxSlide - 1) {
     currSlide = 0;
   } else {
@@ -36,7 +38,7 @@ nextBtn.addEventListener("click", () => {
   goToSlide(currSlide);
 });
 
-prevBtn.addEventListener("click", () => {
+prevBtn.addEventListener('click', () => {
   if (currSlide === 0) {
     currSlide = maxSlide - 1;
   } else {
@@ -45,15 +47,15 @@ prevBtn.addEventListener("click", () => {
   goToSlide(currSlide);
 });
 
-btn.addEventListener("click", (e) => {
+btn.addEventListener('click', (e) => {
   if (
-    Name.value.length > 0 &&
-    Email.value.indexOf("@") !== -1 &&
-    Email.value.length > 0
+    Name.value.length > 0
+    && Email.value.indexOf('@') !== -1
+    && Email.value.length > 0
   ) {
     e.preventDefault();
-    Name.value = "";
-    Email.value = "";
-    comment.value = "";
+    Name.value = '';
+    Email.value = '';
+    comment.value = '';
   }
 });
