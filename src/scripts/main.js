c8834b4f8c3060474723d4c12b789485d0b0908a
Denys Stronin <den.stronin@gmail.com>
2023-03-31 15:43:50 +0300

    Dia landing - First commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,49 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+const photos = document.querySelectorAll('.slider .slider__images .slider__image');
+const sliderLine = document.querySelector('.slider .slider__images');
+let count = 0;
+let width;
+
+function init() {
+  width = document.querySelector('.slider').offsetWidth;
+  sliderLine.style.width = width * photos.length + 'px';
+
+  photos.forEach(item => {
+    item.style.width = width + 'px';
+  });
+  rollSlider();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.slider__button--right').addEventListener('click', function() {
+  count++;
+
+  if (count >= photos.length) {
+    count = 0;
+  }
+  rollSlider();
+});
+
+document.querySelector('.slider__button--left').addEventListener('click', function() {
+  count--;
+
+  if (count < 0) {
+    count = photos.length - 1;
+  }
+  rollSlider();
+});
+
+function rollSlider() {
+  sliderLine.style.transform = 'translate(-' + count * width + 'px)';
+}
