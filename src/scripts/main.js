43fb0d47c30ae713c470e79746ecfc28da132776
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-04-04 03:05:36 +0300

    add header and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('air__body--with-menu');
+  } else {
+    document.body.classList.remove('air__body--with-menu');
+  }
+});
