328229368467142237b2f41261a85141ac82824a
Natalia Tsiutsiura <89_22@ukr.net>
2023-04-06 11:15:49 +0300

    Layout_air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+
+// header-link active
+
+window.onload = function() {
+  const chk = document.getElementById('header__vector');
+
+  const menuLinks = document.querySelectorAll('.menu li a');
+
+  menuLinks.forEach(function(item) {
+    item.addEventListener('click', function() {
+      chk.checked = false;
+    });
+  });
+};
+
+// form loading
+
+window.onload = function() {
+  const pageReload = document.querySelector('#form');
+
+  pageReload.addEventListener('submit', (event) => {
+    event.preventDefault();
+    pageReload.reset();
+  });
+};
