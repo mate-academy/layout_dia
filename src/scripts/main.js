9932041115582b3f7d8b682e08fbe20a3744146e
Victor Hrygorash <viktor12041991@i.ua>
2023-04-11 12:02:39 +0300

    done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,42 +8,6 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-// Slider
-const prevButton = document.querySelector('.slider__button--left');
-const nextButton = document.querySelector('.slider__button--right');
-let slideIndex = 0;
-
-showSlides(slideIndex);
-
-function plusSlide(n) {
-  showSlides(slideIndex += n);
-}
-
-function showSlides(n) {
-  const slides = document.querySelectorAll('.slider__image');
-
-  if (n > slides.length - 1) {
-    slideIndex = 0;
-  }
-
-  if (n < 0) {
-    slideIndex = slides.length - 1;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = 'none';
-  }
-
-  slides[slideIndex].style.display = 'block';
-}
-
-prevButton.addEventListener('click', () => plusSlide(-1));
-nextButton.addEventListener('click', () => plusSlide(1));
-
-setInterval(function() {
-  plusSlide(1);
-}, 4000);
-
 // Form
 const form = document.querySelector('.contact__form');
 
