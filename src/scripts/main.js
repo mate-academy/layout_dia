19bb6a1a3d0b104dbe624b57c31b8c344ec9902f
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-15 15:43:19 +0300

    fixed the path

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,8 +34,9 @@ const slider = document.querySelector('.slider__window');
 const leftBtn = document.querySelector('.button-left');
 const rightBtn = document.querySelector('.button-right');
 const arrOfPhoto
-= ['/images/slider/slide-1.jpg',
-  '/images/slider/slide-2.jpg', '/images/slider/slide-3.jpg'];
+= ['layout_dia/src/images/slider/slide-1.jpg',
+  'layout_dia/src/images/slider/slide-2.jpg',
+  'layout_dia/src/images/slider/slide-3.jpg'];
 let clickNumber = 0;
 
 rightBtn.addEventListener('click', nextPhoto);
