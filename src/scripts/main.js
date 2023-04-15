70a486671f0c4e8f57e254b5909e7a4775d59fb7
Iryna Kostromytska <neopoznannyi.enot@gmail.com>
2023-04-15 17:04:56 +0300

    fixed path

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -34,9 +34,9 @@ const slider = document.querySelector('.slider__window');
 const leftBtn = document.querySelector('.button-left');
 const rightBtn = document.querySelector('.button-right');
 const arrOfPhoto
-= ['./images/slider/slide-1.jpg',
-  '/images/slider/slide-2.jpg',
-  'images/slider/slide-3.jpg'];
+= ['slide-1.86458e7a.jpg',
+  'slide-2.68d0d673.jpg',
+  'slide-3.40344566.jpg'];
 let clickNumber = 0;
 
 rightBtn.addEventListener('click', nextPhoto);
