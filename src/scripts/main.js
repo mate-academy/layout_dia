724a573844af9958135471dee8c989acef065df1
Vasiliy22 <hitretsukrnet@gmail.com>
2023-04-17 21:00:15 +0300

    fix all

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,23 +1,36 @@
 'use strict';
 
+// window.addEventListener('hashchange', () => {
+//   if (window.location.hash === '#menu') {
+//     document.body.classList.add('header--with-menu');
+//   } else {
+//     document.body.classList.remove('header--with-menu');
+//   }
+// });
+
+const form = document.querySelector('form');
+
 window.addEventListener('hashchange', () => {
-  if (window.location.hash === '#menu') {
-    document.body.classList.add('header--with-menu');
-  } else {
-    document.body.classList.remove('header--with-menu');
-  }
+  window.location.hash === '#menu'
+    ? document.body.classList.add('page__body--with-menu')
+    : document.body.classList.remove('page__body--with-menu');
 });
 
-const myForm = document.querySelector('.my-form');
-
-const sendButton = document.getElementById('clicks');
-
-sendButton.addEventListener('click', function(event) {
-  event.preventDefault();
-  myForm.reset();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
 });
 
-myForm.addEventListener('clicks', sendButton);
+// const myForm = document.querySelector('.my-form');
+
+// const sendButton = document.getElementById('clicks');
+
+// sendButton.addEventListener('click', function(event) {
+//   event.preventDefault();
+//   myForm.reset();
+// });
+
+// myForm.addEventListener('clicks', sendButton);
 
 // function validateForm() {
 //   let userEmail = document.getElementById('email').value;
