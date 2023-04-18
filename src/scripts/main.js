8b8d74ae30a76dc6593a123406f2e7d97c35cd68
Vasiliy22 <hitretsukrnet@gmail.com>
2023-04-18 19:25:31 +0300

    fix slider, close icon and autocomplete, disable scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,36 @@
 'use strict';
 
-// window.addEventListener('hashchange', () => {
-//   if (window.location.hash === '#menu') {
-//     document.body.classList.add('header--with-menu');
-//   } else {
-//     document.body.classList.remove('header--with-menu');
-//   }
-// });
-
-const form = document.querySelector('form');
-
 window.addEventListener('hashchange', () => {
-  window.location.hash === '#menu'
-    ? document.body.classList.add('page__body--with-menu')
-    : document.body.classList.remove('page__body--with-menu');
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('header--with-menu');
+  } else {
+    document.body.classList.remove('header--with-menu');
+  }
 });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  e.target.reset();
-});
+// const form = document.querySelector('form');
 
-// const myForm = document.querySelector('.my-form');
-
-// const sendButton = document.getElementById('clicks');
-
-// sendButton.addEventListener('click', function(event) {
-//   event.preventDefault();
-//   myForm.reset();
+// window.addEventListener('hashchange', () => {
+//   window.location.hash === '#menu'
+//     ? document.body.classList.add('page__body--with-menu')
+//     : document.body.classList.remove('page__body--with-menu');
 // });
 
-// myForm.addEventListener('clicks', sendButton);
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   e.target.reset();
+// });
+
+const myForm = document.querySelector('.my-form');
+
+const sendButton = document.getElementById('clicks');
+
+sendButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  myForm.reset();
+});
+
+myForm.addEventListener('clicks', sendButton);
 
 // function validateForm() {
 //   let userEmail = document.getElementById('email').value;
