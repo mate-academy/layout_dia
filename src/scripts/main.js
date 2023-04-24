72eef1839fef023728a027c48e7f2932b43f2233
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2023-04-24 19:47:29 +0300

    dia version 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,43 +1,9 @@
-// 'use strict';
-// let slideIndex = 1;
+'use strict';
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+const frm = document.getElementById('submitForm');
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// const imgContainer = document.getElementById("slider");
-// const sliders = [
-//   "images/header-bg-image.png",
-//   "images/img1.png",
-//   "images/img2.png"
-// ];
-
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   //let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   // for (i = 0; i < dots.length; i++) {
-//   //   dots[i].className = dots[i].className.replace(" active", "");
-//   // }
-//   slides[slideIndex-1].style.display = "block";
-//   imgContainer.style.backgroundImage = `url(${sliders[slideIndex-1]})`;
-//   //dots[slideIndex-1].className += " active";
-// }
-
-// showSlides(slideIndex);
-// const prevBtn = document.getElementById("sliderPrevBtn");
-// const nextBtn = document.getElementById("sliderNextBtn");
-
-// prevBtn.onclick = () => plusSlides(-1);
-// nextBtn.onclick = () => plusSlides(1);
+frm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  // frm.submit(); // Submit the form
+  frm.reset(); // Reset all form data
+});
