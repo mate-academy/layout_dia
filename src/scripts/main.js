4d0bbbb715d37b0fbd0ae8d50012fe06446767c5
Vitalii Bachynskyi <vitalii.bach@gmail.com>
2023-04-26 11:52:19 +0300

    v03

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', onSubmit, false);
+
+function onSubmit(event) {
+  event.preventDefault();
+  form.reset();
+};
