5d4661c300d311e566f04a4a32ea9b7417210794
Pavel Maistrenko <maystrenko.p@gmail.com>
2023-04-26 13:50:59 +0200

    implement header slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,40 @@ form.addEventListener('submit', function(event) {
   event.preventDefault();
   form.reset();
 });
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slider = document.querySelector('.header__slider');
+let sliderPostId = 1;
+
+prevButton.addEventListener('click', () => {
+  sliderPostId--;
+  slider.style.color = '#fff';
+
+  if (sliderPostId < 1) {
+    slider.classList.remove(`slider-img--1`);
+    sliderPostId = 3;
+  }
+
+  if (sliderPostId > 1) {
+    slider.style.color = '#2060F6';
+  }
+  slider.classList.remove(`slider-img--${sliderPostId + 1}`);
+  slider.classList.add(`slider-img--${sliderPostId}`);
+});
+
+nextButton.addEventListener('click', () => {
+  sliderPostId++;
+  slider.style.color = '#fff';
+
+  if (sliderPostId > 3) {
+    slider.classList.remove(`slider-img--3`);
+    sliderPostId = 1;
+  }
+
+  if (sliderPostId > 1) {
+    slider.style.color = '#2060F6';
+  }
+  slider.classList.remove(`slider-img--${sliderPostId - 1}`);
+  slider.classList.add(`slider-img--${sliderPostId}`);
+});
