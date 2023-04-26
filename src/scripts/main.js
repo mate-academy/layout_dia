fe8b3b5ed33749e06b21915b3814ccf3bd0a8134
Vasiliy22 <hitretsukrnet@gmail.com>
2023-04-26 20:32:15 +0300

    fix header and section and form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,34 @@
 'use strict';
 
+window.addEventListener('submit', (e) => {
+  e.preventDefault();
+  e.target.reset();
+});
+
 window.addEventListener('hashchange', () => {
   window.location.hash === '#menu'
     ? document.body.classList.add('body--nav-menu')
     : document.body.classList.remove('body--nav-menu');
 });
 
-const form = document.querySelector('form');
+window.addEventListener('load', () => {
+  window.location.hash === '#menu'
+    ? document.body.classList.add('body--nav-menu')
+    : document.body.classList.remove('body--nav-menu');
+});
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  event.target.reset();
+const slide = document.getElementById('right');
+const prev = document.getElementById('left-btn');
+const next = document.getElementById('right-btn');
+
+prev.addEventListener('click', () => {
+  if (!slide.classList.contains('show')) {
+    slide.classList.add('show');
+  }
+});
+
+next.addEventListener('click', () => {
+  if (slide.classList.contains('show')) {
+    slide.classList.remove('show');
+  }
 });
