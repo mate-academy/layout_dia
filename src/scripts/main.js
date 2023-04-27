c9b3196fff76eb7d9dea0b5ede22140ccf39247c
Vasiliy22 <hitretsukrnet@gmail.com>
2023-04-27 11:04:54 +0300

    fix changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,18 @@ window.addEventListener('submit', (e) => {
   e.target.reset();
 });
 
+const ellipse = document.querySelector('.block-ellipse');
+const infoText = document.querySelector('.slider-info');
+
 window.addEventListener('hashchange', () => {
+  window.location.hash === '#menu'
+    ? ellipse.style.display = 'none'
+    : ellipse.style.display = 'flex';
+
+  window.location.hash === '#menu'
+    ? infoText.style.display = 'none'
+    : infoText.style.display = 'block';
+
   window.location.hash === '#menu'
     ? document.body.classList.add('body--nav-menu')
     : document.body.classList.remove('body--nav-menu');
