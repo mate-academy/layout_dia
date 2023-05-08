7419952279f3323e6a5adb72e44d9a3d117e8be3
Denis Kononenko <deniskononenko88@gmail.com>
2023-05-08 11:34:43 +0300

    add requested changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,17 @@
 'use strict';
 
+const menuOpener = document.querySelector('.header__menu-opener');
+const menu = document.querySelector('.menu');
+const menuCross = document.querySelector('.menu__cross');
 const form = document.querySelector('.footer__form');
+
+menuOpener.addEventListener('click', () => {
+  menu.classList.add('menu--open');
+});
+
+menuCross.addEventListener('click', () => {
+  menu.classList.remove('menu--open');
+});
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
