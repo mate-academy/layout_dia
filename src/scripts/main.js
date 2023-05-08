1e8ef9ca6ca10d14a479074e972ea8dbb1ebc7ea
Denis Kononenko <deniskononenko88@gmail.com>
2023-05-08 14:03:36 +0300

    add requested changes 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,14 +4,17 @@ const menuOpener = document.querySelector('.header__menu-opener');
 const menu = document.querySelector('.menu');
 const menuCross = document.querySelector('.menu__cross');
 const form = document.querySelector('.footer__form');
+const menuLinks = document.querySelectorAll('.nav-link--menu');
+
+const closeMenu = () => menu.classList.remove('menu--open');
 
 menuOpener.addEventListener('click', () => {
   menu.classList.add('menu--open');
 });
 
-menuCross.addEventListener('click', () => {
-  menu.classList.remove('menu--open');
-});
+menuCross.addEventListener('click', closeMenu);
+
+menuLinks.forEach(item => item.addEventListener('click', closeMenu));
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
