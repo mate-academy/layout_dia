11fd05d7e2cf231310115a8b4d32275dca4c1046
Nazar Hotko <nazar.hotko@outlook.com>
2023-05-12 22:05:43 +0200

    changes made

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,23 @@
 'use strict';
+'';
 
 const Form = document.getElementById('main-form');
+
+const MenuButton = document.getElementsByClassName('header__menu')[0];
+const Menu = document.getElementsByClassName('menu')[0];
+let menuStatus = false;
 
 Form.addEventListener('submit', (e) => {
   e.target.reset();
   e.preventDefault();
+});
+
+MenuButton.addEventListener('click', (e) => {
+  if (menuStatus) {
+    Menu.style.display = 'none';
+    menuStatus = false;
+  } else {
+    Menu.style.display = 'flex';
+    menuStatus = true;
+  }
 });
