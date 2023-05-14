8f43e647496eef54057b13a563f0175ebf55c328
Eugene Tykhonov <sargosbest@gmail.com>
2023-05-14 04:09:11 +0300

    My landing #2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--menu-opener');
+  } else {
+    document.body.classList.remove('page__body--menu-opener');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  document.getElementById('form').reset();
+});
