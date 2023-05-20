e6abb606b423118b2e6d6a9a3171f47b8199ae93
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-20 15:39:43 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// window.addEventListener('hashchange', () => {
-//   if (window.location.hash === '#menu') {
-//     document.body.classList.add('page__body--with-menu');
-//   } else {
-//     document.body.classList.remove('page__body--with-menu');
-//   }
-// });
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu-burger') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
 
 window.addEventListener('submit', (event) => {
   document.getElementById('name').value = '';
