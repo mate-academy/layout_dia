bc4db0617ddcbda85ba2cb36d4ccda0f8f9bf2d3
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-24 14:21:15 +0300

    add footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,11 @@
+/* eslint-disable space-before-function-paren */
 /* eslint-disable quotes */
 "use strict";
+
+// form submit
+const form = document.querySelector(".footer__form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  this.reset();
+});
