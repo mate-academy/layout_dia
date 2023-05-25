ba751a54e243432524e1bf519b7193f42824ee52
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-25 15:16:02 +0300

    add header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,25 @@
 /* eslint-disable quotes */
 "use strict";
 
+// header menu
+const header = document.querySelector(".header");
+
+if (header) {
+  const burgerBtn = header.querySelector(".header__burger");
+  const openMenu = () => {
+    document.body.classList.toggle("page__body--status-lock");
+    header.classList.toggle("header--menu-open");
+  };
+
+  burgerBtn.addEventListener("click", openMenu);
+}
+
 // form submit
 const form = document.querySelector(".footer__form");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-  this.reset();
-});
+if (form) {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    this.reset();
+  });
+}
