d4e36ca66611b90e000288b664b854707071154e
Kristina Bekher <bekherkr@gmail.com>
2023-05-26 00:02:13 +0200

    improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,7 @@
 'use strict';
 
 function submitForm(event) {
-  event.preventDefault();
   document.querySelector('#form').reset();
-  // window.scrollTo('#header');
 }
 
 document.querySelector('#form').addEventListener('submit', submitForm);
