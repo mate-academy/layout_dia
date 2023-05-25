9cddaa003eac4e9453d35098fe3c0aa6dca1c10a
Kristina Bekher <bekherkr@gmail.com>
2023-05-25 16:40:27 +0200

    add hover and transition effects

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+function submitForm(event) {
+  event.preventDefault();
+  document.querySelector('#form').reset();
+  // window.scrollTo('#header');
+}
+
+document.querySelector('#form').addEventListener('submit', submitForm);
