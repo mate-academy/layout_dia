4cc60b35c0db7ba940b24b41d04ebebf4cce378f
dima_rogkov <dima.rogkov@cheitgroup.com>
2023-05-26 14:30:39 +0300

    some fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,12 +7,18 @@ const header = document.querySelector(".header");
 
 if (header) {
   const burgerBtn = header.querySelector(".header__burger");
+  const links = header.querySelectorAll(".header__link");
   const openMenu = () => {
     document.body.classList.toggle("page__body--status-lock");
     header.classList.toggle("header--menu-open");
   };
+  const closeMenu = () => {
+    document.body.classList.remove("page__body--status-lock");
+    header.classList.remove("header--menu-open");
+  };
 
   burgerBtn.addEventListener("click", openMenu);
+  links.forEach((link) => link.addEventListener("click", closeMenu));
 }
 
 // form submit
