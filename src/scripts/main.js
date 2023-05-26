4598f0137b85e22d523a380affed48fd74c5c192
Kristina Bekher <bekherkr@gmail.com>
2023-05-26 11:29:48 +0200

    add form improvement

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function submitForm(event) {
+  event.preventDefault();
   document.querySelector('#form').reset();
+  window.location.hash = '#home';
 }
 
 document.querySelector('#form').addEventListener('submit', submitForm);
