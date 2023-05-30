ba0096e41ddc207bfc7462da04b32621c4e3ba31
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-05-30 21:27:20 +0300

    third version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,3 +6,11 @@ form.addEventListener('submit', event => {
   event.preventDefault();
   event.target.reset();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#float-menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
