3684f1d80c1074b31c3ca26b2e47572c6d4835fc
Veronika Pastushkova <d9169393507@gmai.com>
2023-06-05 18:14:20 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,9 +2,9 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+    document.body.classList.add('page--with-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.body.classList.remove('page--with-menu');
   }
 });
 
