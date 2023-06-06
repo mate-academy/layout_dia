292af1dcf9e757898907847c4c569581dd107022
Sergey Arnautov <sirius9312@ukr.net>
2023-06-06 16:15:39 +0300

    AIR-LANDING-PAGE

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+});
+
+const textarea = document.querySelector('textarea');
+const limitCols = 35;
+const limitRows = 3;
+
+textarea.addEventListener('keyup', (e) => {
+  const lines = e.target.value.split('\n');
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].length < limitCols) {
+      continue;
+    }
+
+    lines[i] = lines[i].substring(0, limitCols);
+    lines[i + 1] = `${lines[i].substring(limitCols + 1)}${lines[i + 1] || ''}`;
+  }
+
+  textarea.value = lines.slice(0, limitRows).join('\n');
+});
