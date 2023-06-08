0883abf3858dc7dca875bc61d88e1776fa1f02c6
Andriy Antonuk <Batick3737@ukr.net>
2023-06-08 14:05:12 +0300

    change code

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.elements.email.value = '';
+  form.elements.name.value = '';
+  form.elements.text.value = '';
+});
+
+const kek = document.querySelector('.page__body');
+
+const menu = document.querySelector('.menu_open');
+const closedd = document.querySelectorAll('.menu__link--header');
+
+menu.addEventListener('click', () => {
+  kek.style.overflow = 'hidden';
+});
+
+[...closedd].map(a => a.addEventListener('click', () => {
+  kek.style.overflow = 'visible';
+}));
