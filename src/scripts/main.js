57264854d6a386ceb84cd0fdd54460a71615ecc8
Volodymyr Alieksieiev <125214087+vovaAlieksieiev@users.noreply.github.com>
2023-06-08 12:21:27 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,24 @@
 'use strict';
+
+let offset = 0;
+const sliderLine = document.querySelector('.slider__line');
+
+document.querySelector('.slider__button-right')
+  .addEventListener('click', function() {
+    offset = offset + 33.333333;
+
+    if (offset > 66.6666666) {
+      offset = 0;
+    }
+    sliderLine.style.transform = 'translateX(-' + offset + '%)';
+  });
+
+document.querySelector('.slider__button-left')
+  .addEventListener('click', function() {
+    offset = offset - 33.333333;
+
+    if (offset < 0) {
+      offset = 66.6666666;
+    }
+    sliderLine.style.transform = 'translateX(-' + offset + '%)';
+  });
