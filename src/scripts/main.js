51f4f981d583594906182e8a44afb5146a490a21
Yana Drobna <yana.drobnaya@gmail.com>
2023-06-13 22:22:18 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,17 @@
 
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
-    document.body.classList.add('page__body--with-menu');
+    document.body.classList.add('page--with-menu');
   } else {
-    document.body.classList.remove('page__body--with-menu');
+    document.body.classList.remove('page--with-menu');
   }
+});
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
 });
 
 const page = document.documentElement;
