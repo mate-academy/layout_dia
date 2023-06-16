7080510af9725dc3f0850ba81ef02f58ae72abba
Mari <kap.mebel.a@gmail.com>
2023-06-16 11:55:25 +0300

    error correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,9 +8,9 @@ window.addEventListener('hashchange', () => {
   }
 });
 
-const form = document.querySelector('.contacts__form');
+const form = document.querySelector('.contact-us__form');
 
-form.addEventListener('submit', (e) => {
+form.addEventListener('send', (e) => {
   e.preventDefault();
   form.reset();
 });
