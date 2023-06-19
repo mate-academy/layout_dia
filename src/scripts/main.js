29e739bf9b45a78e8f81689cc385ccbfa72b45d0
Olena Hapon <olenagapon90gmail.com>
2023-06-19 18:03:05 +0200

    Added changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,6 +8,7 @@ window.addEventListener('hashchange', () => {
   }
 });
 
+// form
 const form = document.querySelector('form');
 
 form.addEventListener('submit', (event) => {
@@ -15,6 +16,8 @@ form.addEventListener('submit', (event) => {
   form.reset();
 });
 
+// slider with click
+const sliderWrapper = document.querySelector('.slider__wrapper');
 const sliderImages = document.querySelectorAll('.slider__image');
 const sliderContainer = document.querySelector('.slider__container');
 const buttonNext = document.querySelector('.slider__button--right');
@@ -38,23 +41,49 @@ window.addEventListener('resize', init);
 init();
 
 buttonPrev.addEventListener('click', () => {
+  clearInterval(intervalId);
   count--;
 
   if (count < 0) {
     count = sliderImages.length - 1;
   }
   rollSlider();
+  autoSlide();
 });
 
 buttonNext.addEventListener('click', () => {
+  clearInterval(intervalId);
   count++;
 
   if (count >= sliderImages.length) {
     count = 0;
   }
   rollSlider();
+  autoSlide();
 });
 
 function rollSlider() {
   sliderContainer.style.transform = 'translate(-' + count * width + 'px)';
 }
+
+// autoslide
+let imageIndex = 1;
+let intervalId;
+
+function autoSlide() {
+  intervalId = setInterval(() => slideImages(++imageIndex), 2000);
+};
+
+autoSlide();
+
+function slideImages() {
+  imageIndex = imageIndex === sliderImages.length
+    ? 0 : imageIndex < 0 ? sliderImages.length - 1 : imageIndex;
+  sliderContainer.style.transform = 'translate(-' + imageIndex * width + 'px)';
+}
+
+sliderWrapper.addEventListener('mouseover', () => {
+  clearInterval(intervalId);
+});
+
+sliderWrapper.addEventListener('mouseleave', autoSlide);
