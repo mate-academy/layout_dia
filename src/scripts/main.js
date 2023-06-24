a482ba06dca5d6e67aeaddf3d73b3a9cd34fbecd
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2023-06-24 15:30:40 +0300

    create Air landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const navElement = document.querySelector('.header__nav');
+const menu = document.querySelector('.menu');
+const btnMenu = document.querySelector('.icon--menu');
+const btnCross = document.querySelector('.icon--cross');
+
+btnMenu.addEventListener('click', () => {
+  navElement.classList.add('header__nav--active');
+});
+
+btnMenu.addEventListener('click', () => {
+  menu.classList.add('menu--active');
+});
+
+btnCross.addEventListener('click', () => {
+  navElement.classList.remove('header__nav--active');
+});
+
+btnCross.addEventListener('click', () => {
+  menu.classList.add('menu--active');
+});
+
+const photo = document.querySelector('.header__slider');
+const arrowBack = document.querySelector('.header__slider--arrow-back');
+const arrowForward = document.querySelector('.header__slider--arrow-forward');
+
+arrowBack.addEventListener('click', () => {
+  photo.classList.add('header__slider-one');
+});
+
+arrowForward.addEventListener('click', () => {
+  photo.classList.add('header__slider-two');
+});
