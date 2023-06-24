3b9a51a71994148fc08743bc1d1ca3dde5fc98fa
Юрий Мисюра <apriory13@meta.ua>
2023-06-24 22:24:55 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,29 @@
 'use strict';
 
 const header = document.querySelector('.header');
+const buttonPage = document.querySelectorAll('.button');
 const switcher = document.querySelector('.theme-switcher');
 const menu = document.querySelector('.menu');
 const hire = document.querySelectorAll('.top-bar__hire');
-const headerButton = document.querySelector('.header__button');
 const sliderBorder = document.querySelectorAll('.slider__box-content-photo');
 const navLinks = document.querySelectorAll('.nav__link');
+const footer = document.querySelector('.footer');
+const field = document.querySelectorAll('.form__field');
+const visionBcground = document.querySelectorAll('.vision');
 
 switcher.addEventListener('click', () => {
   if (header.classList.contains('header--theme--dark')) {
     header.classList.remove('header--theme--dark');
+
+    visionBcground.forEach((element) => {
+      element.classList.remove('vision--theme--dark');
+    });
+
+    field.forEach((input) => {
+      input.classList.remove('form__field--theme--dark');
+    });
+
+    footer.classList.remove('footer--theme--dark');
 
     switcher.classList.remove('theme-switcher--theme--dark');
 
@@ -20,7 +33,9 @@ switcher.addEventListener('click', () => {
       hireElement.classList.remove('top-bar__hire--theme--dark');
     });
 
-    headerButton.classList.remove('header__button--theme--dark');
+    buttonPage.forEach((buttonElement) => {
+      buttonElement.classList.remove('button--theme--dark');
+    });
 
     sliderBorder.forEach(photo => {
       photo.classList.remove('slider__box-content-photo--theme--dark');
@@ -32,11 +47,23 @@ switcher.addEventListener('click', () => {
   } else {
     header.classList.add('header--theme--dark');
 
+    visionBcground.forEach((element) => {
+      element.classList.add('vision--theme--dark');
+    });
+
+    field.forEach((input) => {
+      input.classList.add('form__field--theme--dark');
+    });
+
+    footer.classList.add('footer--theme--dark');
+
+    buttonPage.forEach((buttonElement) => {
+      buttonElement.classList.add('button--theme--dark');
+    });
+
     switcher.classList.add('theme-switcher--theme--dark');
 
     menu.classList.add('menu--theme--dark');
-
-    headerButton.classList.add('header__button--theme--dark');
 
     sliderBorder.forEach(photo => {
       photo.classList.add('slider__box-content-photo--theme--dark');
