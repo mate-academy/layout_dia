fe816c04bd659fc7bc5165af61ab44922949b82d
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2023-06-28 15:17:57 +0300

    corected task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,7 +5,7 @@ const navElement = document.querySelector('.header__nav');
 const menu = document.querySelector('.menu');
 const btnMenu = document.querySelector('.icon--menu');
 const btnCross = document.querySelector('.icon--cross');
-const btnLink = document.querySelector('.menu__link');
+const btnLink = document.querySelector('ul');
 
 btnMenu.addEventListener('click', () => {
   navElement.classList.add('header__nav--active');
