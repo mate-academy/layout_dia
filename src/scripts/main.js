a1466f2d65035a66827ffec3016d05bfd9984562
Vladyslav Korobka <vladseipa@gmail.com>
2023-06-28 16:13:07 +0300

    project is done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const form = document.querySelector('.contact__form');
+const formTextarea = document.querySelector('.contact__form--message');
+
+formTextarea.addEventListener('input', () => {
+  formTextarea.style.height = 'auto';
+  formTextarea.style.height = `${formTextarea.scrollHeight}px`;
+});
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  form.reset();
+  window.scrollTo({ top: 0 });
+
+  formTextarea.style.height = 'auto';
+});
