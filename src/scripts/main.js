a582f237e8221ce89a08cfdd58b1a2795b26da2b
Volodymyr Sarabun <vov4ukcool25@gmail.com>
2023-06-28 13:30:24 +0300

    corected task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,16 +1,38 @@
 'use strict';
 
+const overflowHidden = document.querySelector('.page__body');
 const navElement = document.querySelector('.header__nav');
 const menu = document.querySelector('.menu');
 const btnMenu = document.querySelector('.icon--menu');
 const btnCross = document.querySelector('.icon--cross');
+const btnLink = document.querySelector('.menu__link');
 
 btnMenu.addEventListener('click', () => {
   navElement.classList.add('header__nav--active');
 });
 
 btnMenu.addEventListener('click', () => {
+  overflowHidden.classList.add('page__body--active');
+});
+
+btnMenu.addEventListener('click', () => {
   menu.classList.add('menu--active');
+});
+
+btnCross.addEventListener('click', () => {
+  overflowHidden.classList.remove('page__body--active');
+});
+
+btnLink.addEventListener('click', () => {
+  overflowHidden.classList.remove('page__body--active');
+});
+
+btnLink.addEventListener('click', () => {
+  navElement.classList.remove('header__nav--active');
+});
+
+btnLink.addEventListener('click', () => {
+  menu.classList.remove('menu--active');
 });
 
 btnCross.addEventListener('click', () => {
@@ -18,5 +40,5 @@ btnCross.addEventListener('click', () => {
 });
 
 btnCross.addEventListener('click', () => {
-  menu.classList.add('menu--active');
+  menu.classList.remove('menu--active');
 });
