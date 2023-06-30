1b70a43129b90e7b775c97b18fa517f69c574649
Zhekich123 <axelb380@gmail.com>
2023-06-30 22:09:17 +0300

    ready site

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,22 @@
-'use strict';
+/* eslint-disable no-undef */
+/* eslint-disable prefer-const */
+/* eslint-disable padding-line-between-statements */
+/* eslint-disable strict */
+// eslint-disable-next-line max-len
+
+// #region burger
+let headerNav = document.querySelector('.header__nav');
+let burgerIcon = document.querySelector('.header__burger-icon');
+
+burgerIcon.addEventListener('click', function() {
+  this.classList.toggle('active');
+  headerNav.classList.toggle('open');
+});
+
+document.querySelectorAll('.header__nav a.nav__link').forEach(function(link) {
+  link.addEventListener('click', function() {
+    headerNav.classList.remove('open');
+    burgerIcon.classList.remove('active');
+  });
+});
+// #endregion
