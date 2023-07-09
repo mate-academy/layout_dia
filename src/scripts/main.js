55e1eaf2e85d5c73e3a308c1d52af448ffd3b1dd
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-07-09 18:22:05 +0300

    add blue theme

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Get the checkbox and label elements
+const themeSwitcher = document.getElementById('themeSwitcher');
+
+// Add event listener for the theme switcher
+themeSwitcher.addEventListener('change', () => {
+  // Toggle the dark-theme class on the body element
+  document.body.classList.toggle('blue-theme');
+});
+
+// Check the initial state of the theme switcher
+if (window.matchMedia
+  && window.matchMedia('(prefers-color-scheme: light)').matches) {
+  themeSwitcher.checked = true;
+}
