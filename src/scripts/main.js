0937f62f02d0553b827b1d6a7bf8f39527bacacd
TetianaTokar <tatyana@tokar.net.ua>
2023-07-10 17:56:45 +0300

    Layout_Dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const swiper = new Swiper('.slider-main-block', {
+  // Optional parameters
+  loop: true,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.navigation-main-block__arrow.swiper-button-next',
+    prevEl: '.navigation-main-block__arrow.swiper-button-prev',
+  },
+});
