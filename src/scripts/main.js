9c6e5bc5f0cf0d92fa595f6222e2df4f7b873947
ihorivna007 <emmakorovnik@gmail.com>
2023-07-14 15:54:36 +0300

    added updates

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const contactsForm = document.querySelector('.contacts__form');
+const formFields = document.querySelectorAll('.contacts__form__field');
+
+contactsForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  formFields.forEach(field => {
+    field.value = '';
+  });
+});
