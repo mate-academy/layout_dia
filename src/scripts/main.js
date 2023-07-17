1f90e5268bea15583d115bc877d66d00ba528a69
Hlib Vakhrameev@icloud.com <g.vakhrameev@icloud.com>
2023-07-17 15:41:22 +0300

    disable page scroll with use JS and center block services on mobile media

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,12 @@
 'use strict';
 
-const app = document.querySelector('.info__list');
-const form = app.querySelector('.info__list');
+const OpenAside = document.querySelector('.header__side-bar');
+const removeHidden = document.querySelector('.side-bar');
 
-form.addEventListener('submit', () => {});
+OpenAside.addEventListener('click', () => {
+  document.body.classList.add('body__hidden');
+});
+
+removeHidden.addEventListener('click', () => {
+  document.body.classList.remove('body__hidden');
+});
