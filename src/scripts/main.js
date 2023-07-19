3b7321005faf4a53bd5e07c09067fe9300eebf3b
Mykhailo Hubko <mixelios@gmail.com>
2023-07-19 21:53:52 +0200

    complet landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const form = document.querySelector('.footer__form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  form.reset();
+  location.reload();
+});
