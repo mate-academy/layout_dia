7a2b70b25bd6d50a0086f04ac845bd83bc3d0507
Mykhailo Hubko <mixelios@gmail.com>
2023-07-20 16:23:01 +0200

    corection landing

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,3 +19,11 @@ form.addEventListener('submit', (e) => {
   form.reset();
   location.reload();
 });
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
