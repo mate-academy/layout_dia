132526d830b77d1fba6089c3703763ae88f4a90d
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-07-22 15:04:34 +0300

    slider done and added hover efects to links in menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
 'use strict';
+
+let position = 0;
+const slidesToScroll = 1;
+const prevArrow = document.querySelector('.slider__container-arrows-prev');
+const nextArrow = document.querySelector('.slider__container-arrows-next');
+const list = document.querySelector('.slider__gallery-list');
+const itemWidth = 320;
+const movePosition = slidesToScroll * itemWidth;
+
+prevArrow.addEventListener('click', () => {
+  if (position === 0) {
+    return;
+  }
+
+  position += movePosition;
+  list.style.transform = `translateX(${position}px)`;
+});
+
+nextArrow.addEventListener('click', () => {
+  if (position <= -320) {
+    return;
+  }
+
+  position -= movePosition;
+  list.style.transform = `translateX(${position}px)`;
+});
