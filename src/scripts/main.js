27f02060beb8fa4c95791877d6d7581d06b964de
Kakerlaken <woody.falcone.00@gmail.com>
2023-08-04 13:13:48 +0300

    Fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  event.target.reset();
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#float-menu') {
+    document.body.classList.add('body--with-menu');
+  } else {
+    document.body.classList.remove('body--with-menu');
+  }
+});
