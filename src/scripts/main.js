741e88fa0a8f22bd6d08c9f12c463b1c9cc74fe1
BodyaKuts <mkkbodya@gmail.com>
2023-08-18 15:35:01 +0300

    implemented menu, centered who-we-are block, disabled page refreshing after submiting form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.getElementById('formId').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('inputId-name').value = '';
+  document.getElementById('inputId-email').value = '';
+  document.getElementById('inputId-message').value = '';
+});
