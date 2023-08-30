e4ad162e287ca66f24213a663fd7a3ed99db175f
Bahtijar <no4kar@gmail.com>
2023-08-30 21:01:23 +0200

    stage-1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
 'use strict';
+
+// const page = document.querySelector('.page');
+const slider = document.querySelector('.slider');
+const btnSlideNext = document.querySelector('.button__arrow--next');
+const btnSlidePrev = document.querySelector('.button__arrow--prev');
+const slideQuantity = 3;
+
+btnSlideNext.addEventListener('click', (evt) => {
+  if (slider.classList[1] !== undefined) {
+    const talkens = (slider.classList[1]).match(/(.*)(\d+)(.*)/);
+
+    if (talkens.length !== 4) {
+      throw new Error('talkens.length !== 4');
+    }
+
+    slider.classList.remove(slider.classList[1]);
+
+    slider.classList.add(
+      `${talkens[1]}`
+      + `${(+talkens[2] + 1) % slideQuantity}`
+      + `${talkens[3]}`);
+  }
+});
+
+btnSlidePrev.addEventListener('click', (evt) => {
+  if (slider.classList[1] !== undefined) {
+    const talkens = (slider.classList[1]).match(/(.*)(\d+)(.*)/);
+
+    if (talkens.length !== 4) {
+      throw new Error('talkens.length !== 4');
+    }
+
+    slider.classList.remove(slider.classList[1]);
+
+    slider.classList.add(
+      `${talkens[1]}`
+      + `${(+talkens[2] + (slideQuantity - 1)) % slideQuantity}`
+      + `${talkens[3]}`);
+  }
+});
