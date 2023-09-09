b8bc3273860a993461db15378e5c5f68bc6a4cb8
Tima Savich <tima.savich@gmail.com>
2023-09-09 18:25:50 +0300

    Sliderwork

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 'use strict';
 
 window.addEventListener('hashchange', () => {
@@ -14,3 +15,41 @@ window.addEventListener('hashchange', () => {
 //   event.preventDefault();
 //   pageReload.reset();
 // });
+document.addEventListener('DOMContentLoaded', () => {
+  const sliderImages = document.querySelectorAll('.slider__img');
+
+  const prevButton = document.querySelector('.slider__prev-button');
+  const nextButton = document.querySelector('.slider__next-button');
+
+  let currentImageIndex = 0;
+
+  function showCurrentImage() {
+    sliderImages.forEach((image, index) => {
+      if (index === currentImageIndex) {
+        image.style.opacity = '1';
+      } else {
+        image.style.opacity = '0';
+      }
+    });
+  }
+
+  prevButton.addEventListener('click', () => {
+    currentImageIndex -= 1;
+
+    if (currentImageIndex < 0) {
+      currentImageIndex = sliderImages.length - 1;
+    }
+    showCurrentImage();
+  });
+
+  nextButton.addEventListener('click', () => {
+    currentImageIndex += 1;
+
+    if (currentImageIndex >= sliderImages.length) {
+      currentImageIndex = 0;
+    }
+    showCurrentImage();
+  });
+
+  showCurrentImage();
+});
