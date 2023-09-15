eeb39a81c9544883c794653b419a612415bc2cef
sheva <vladislavshevchenko10@gmail.com>
2023-09-15 19:59:38 +0300

    added improvements

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,24 @@ window.addEventListener('hashchange', () => {
 
 const form = document.getElementById('form');
 
-function onSubmit(e) {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
+
+  const emailInput = form.querySelector('input[name="email"]');
+  const nameInput = form.querySelector('input[name="name"]');
+  const messageInput = form.querySelector('textarea[name="message"]');
+
+  if (
+    !emailInput.value.trim()
+    || !nameInput.value.trim()
+    || !messageInput.value.trim()
+  ) {
+    // eslint-disable-next-line no-undef
+    alert('Please fill in all the fields before submitting the form.');
+
+    return;
+  }
+
   form.reset();
   window.scrollTo(0, 0);
-}
-
-form.addEventListener('submit', onSubmit);
+});
