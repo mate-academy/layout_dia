93861389a964bdabe6f0912e0d65020ca0a16176
sheva <vladislavshevchenko10@gmail.com>
2023-09-22 16:27:29 +0300

    fixed slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -31,3 +31,34 @@ form.addEventListener('submit', (e) => {
   form.reset();
   window.scrollTo(0, 0);
 });
+
+const prevButton = document.querySelector('.slider__button--left');
+const nextButton = document.querySelector('.slider__button--right');
+let slideIndex = 0;
+
+showSlides(slideIndex);
+
+function plusSlide(n) {
+  showSlides((slideIndex += n));
+}
+
+function showSlides(n) {
+  const slides = document.querySelectorAll('.slider__image');
+
+  if (n > slides.length - 1) {
+    slideIndex = 0;
+  }
+
+  if (n < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+
+  slides[slideIndex].style.display = 'block';
+}
+
+prevButton.addEventListener('click', () => plusSlide(-1));
+nextButton.addEventListener('click', () => plusSlide(1));
