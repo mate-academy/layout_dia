fd80e4a02a2e5dcc9561ec8b5bf898646c81d1e1
YevgeniySavchuk <savchukevgeniy1@gmail.com>
2023-09-24 12:09:50 +0300

    fixed incorrect scroll behavior

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,9 @@ const slider = document.querySelector('.slider');
 window.addEventListener('hashchange', () => {
   if (window.location.hash === '#aside-menu') {
     document.body.classList.add('body__with-aside-menu');
-    slider.style.display = 'none';
+    slider.style.opacity = 0;
   } else {
     document.body.classList.remove('body__with-aside-menu');
-    slider.style.display = 'block';
+    slider.style.opacity = 1;
   }
 });
