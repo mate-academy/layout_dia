e6bdd10c61d306871503881ed6ce0c6e76eb37ce
yanatoto <yanas@bluewin.ch>
2023-09-29 18:32:07 +0200

    finished block expertise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider__inner');
+const prevButton = document.querySelector('.slider__prev-btn');
+const nextButton = document.querySelector('.slider__next-btn');
+const slides = Array.from(slider.querySelectorAll('.slider__img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
