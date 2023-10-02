94ef72f3ce80560dfd66b4bc08e0e7d048e6ed83
Nazar Levkovych <nazar.levkovich.2018@gmail.com>
2023-10-02 18:33:20 +0300

    fix v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,39 +1,5 @@
 'use strict';
 
-// const prevBtn = document.getElementById('prevBtn');
-// const nextBtn = document.getElementById('nextBtn');
-// const slider = document.querySelector('.slider');
-// const images = slider.querySelectorAll('img');
-
-// let currentImageIndex = 0;
-
-// function showImage(index) {
-//   if (index < 0) {
-//     currentImageIndex = images.length - 1;
-//   } else if (index >= images.length) {
-//     currentImageIndex = 0;
-//   }
-
-//   images.forEach((img, idx) => {
-//     if (idx === currentImageIndex) {
-//       img.style.opacity = '1';
-//     } else {
-//       img.style.opacity = '0';
-//     }
-//   });
-// }
-
-// prevBtn.addEventListener('click', () => {
-//   currentImageIndex--;
-//   showImage(currentImageIndex);
-// });
-
-// nextBtn.addEventListener('click', () => {
-//   currentImageIndex++;
-//   showImage(currentImageIndex);
-// });
-
-// showImage(currentImageIndex);
 document.addEventListener('DOMContentLoaded', function() {
   const slider = document.querySelector('.header__slide');
   let currentIndex = 0;
@@ -55,7 +21,6 @@ document.addEventListener('DOMContentLoaded', function() {
     showSlide(currentIndex);
   }
 
-  // Optional: Add event listeners for navigation
   const nextButton = document.getElementById('nextButton');
   const prevButton = document.getElementById('prevButton');
 
@@ -73,4 +38,12 @@ form.addEventListener('submit', (event) => {
   userEmail.value = '';
   userName.value = '';
   userMessage.value = '';
+});
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
 });
