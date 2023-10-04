925fce45dcaf28bd6270ad84a6dabe26eb032882
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-10-04 02:03:52 +0200

    --

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.getElementById('sendMessage').addEventListener('submit', function(e) {
+  // Скасовуємо стандартну дію відправки на сервер
+  e.preventDefault();
+
+  // Опціонально очищаємо поля форми
+  this.reset();
+});
