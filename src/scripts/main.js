9939d01cef7f452078920e14348cdfb0231d409c
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-10-04 02:54:27 +0200

    ----

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,19 @@
 'use strict';
 
 document.getElementById('sendMessage').addEventListener('submit', function(e) {
-  // Скасовуємо стандартну дію відправки на сервер
   e.preventDefault();
-
-  // Опціонально очищаємо поля форми
   this.reset();
 });
 
 const showMenu = document.getElementById('show-menu');
 const navList = document.getElementById('nav-list');
 
-showMenu.addEventListener('click', () => {
-  if (navList.style.display === 'none') {
-    navList.style.cssText = `
-      display: flex;
-      flex-direction: column;
-      gap: 24px;
-      padding: 30px;
-    `;
+document.addEventListener('click', (event) => {
+  if (event.target === showMenu) {
+    showMenu.addEventListener('click', () => {
+      navList.style.right = 0;
+    });
   } else {
-    navList.style.display = 'none';
+    navList.style.right = '100vw';
   }
 });
