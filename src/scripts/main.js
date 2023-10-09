ba14c4a85434ab576529d72fb39bc8ba45756ef2
Oleksii Pishchuhin <aliliolek@gmail.com>
2023-10-09 22:09:12 +0200

    initial commit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -17,3 +17,28 @@ document.addEventListener('click', (event) => {
     navList.style.right = '100vw';
   }
 });
+
+const goLeft = document.getElementById('go-left');
+const goRight = document.getElementById('go-right');
+const sliderImage = document.getElementById('slider-image');
+
+const slides = ['slider-1', 'slider-2', 'slider-3'];
+let currentIndex = 0;
+
+goLeft.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+
+  const currentSlide = slides[currentIndex];
+
+  sliderImage.style.backgroundImage
+    = `url(../images/slider/${currentSlide}.jpg)`;
+});
+
+goRight.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % slides.length;
+
+  const currentSlide = slides[currentIndex];
+
+  sliderImage.style.backgroundImage
+    = `url(../images/slider/${currentSlide}.jpg)`;
+});
