6a40ccffc4876cedcef7a0037022c8f5662a1385
Oleksandr Kosenko <d1izel@ukr.net>
2023-10-12 11:16:35 +0300

    add landing dia

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// Slider
+import $ from 'jquery';
+import 'slick-carousel';
+
+$('.header-slider').slick({
+  arrows: true,
+  dots: false,
+  // slidesToShow: 1, // к-сть для показу
+  speed: 500,
+  easing: 'ease',
+  initialSlide: 0,
+  autoplay: false,
+  autoplaySpeed: 3000,
+});
