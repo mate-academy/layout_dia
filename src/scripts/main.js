a57190ab75241336d44534925cf4fa80f0cd7f4c
nadia vlasenko <nadiavlasenko@MacBook-Pro-nadia.local>
2023-11-01 20:08:59 +0100

    added slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 'use strict';
+
+document.getElementById('MyForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  document.getElementById('email').value = '';
+  document.getElementById('name').value = '';
+  document.getElementById('message').value = '';
+});
