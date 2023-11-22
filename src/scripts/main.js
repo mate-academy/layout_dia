f501e563711f3c9b9cf2446121de553230aaf261
Denys209818 <deniskravcuk63@gmail.com>
2023-11-22 15:20:37 +0200

    Fixed bugs. take1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,4 +4,10 @@ document.getElementById('feedbackForm').addEventListener('submit',
   function(event) {
     event.preventDefault();
     event.stopPropagation();
+
+    const inputs = document.getElementsByClassName('footer__form-input');
+
+    for (const input of inputs) {
+      input.value = '';
+    }
   });
