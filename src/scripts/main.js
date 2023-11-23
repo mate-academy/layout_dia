7371a81e8713e2f6ccd074b2d7bc3e64f8d59d68
Yevhen Podobedo <yevhen.podobedo@gmail.com>
2023-11-23 14:38:23 +0200

    error correction

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const page = document.querySelector('.page');
+
+const locking = document.querySelector('.burger-menu--icon');
+const unlockingList = document.querySelector('.burger-menu__list');
+const unlockingIconClose = document.querySelector('.burger-menu__close');
+
+locking.addEventListener('click', function() {
+  page.classList.add('page__lock');
+});
+
+unlockingList.addEventListener('click', function() {
+  page.classList.remove('page__lock');
+});
+
+unlockingIconClose.addEventListener('click', function() {
+  page.classList.remove('page__lock');
+});
+
+const textarea = document.querySelector('.feedback-form__message');
+
+textarea.addEventListener('scroll', function(event) {
+  if (this.scrollHeight < 50) {
+    textarea.classList.remove('feedback-form__message--active');
+  } else {
+    textarea.classList.add('feedback-form__message--active');
+  }
+});
