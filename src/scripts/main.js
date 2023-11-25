16bd2a187ae9e5a702d1d767d46d527230276401
Vladyslav Starodub <starodubvlad08@gmail.com>
2023-11-25 16:36:24 +0200

    add mockup

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+let countClick = 1;
+
+const slider = document.getElementById('slider');
+
+document.querySelector('.slider__arrow--right')
+  .addEventListener('click', () => sliderChanged('right'));
+
+document.querySelector('.slider__arrow--left')
+  .addEventListener('click', () => sliderChanged('left'));
+
+function sliderChanged(arrow) {
+  slider.classList.remove(`slider-${countClick}`);
+
+  if (arrow === 'right') {
+    countClick++;
+  } else {
+    countClick--;
+  }
+
+  if (countClick < 1) {
+    countClick = 4;
+  }
+
+  if (countClick > 4) {
+    countClick = 1;
+  }
+
+  slider.classList.add(`slider-${countClick}`);
+}
