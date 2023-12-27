fe8ae76c885de4d0211111b4997be5483395ad99
Adrian Yevchuk <adrintik@gmail.com>
2023-12-27 12:51:27 +0200

    page done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+});
+
+const body = document.querySelector('.page__body');
+
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    body.classList.add('page__body--with-menu');
+  } else {
+    body.classList.remove('page__body--with-menu');
+  }
+});
