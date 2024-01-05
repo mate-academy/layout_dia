8350c0966f68699e28962b2ccdbae587df64ae08
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-01-05 14:26:04 +0200

    add slider function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -26,3 +26,38 @@ function switchMenuIcon() {
     menuButton.setAttribute('href', '#home');
   }
 }
+
+const sliderButton = document.querySelectorAll('.slider__button');
+const sliderHolder = document.querySelector('.slider__holder');
+
+let index = 0;
+
+sliderButton[0].addEventListener('click', () => {
+  if (index !== 0) {
+    index--;
+    sliderHolder.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  if (index === 0) {
+    sliderButton[0].classList.add('slider__button--disable');
+  }
+
+  if (index === 2) {
+    sliderButton[1].classList.remove('slider__button--disable');
+  }
+});
+
+sliderButton[1].addEventListener('click', () => {
+  if (index !== 3) {
+    index++;
+    sliderHolder.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  if (index === 3) {
+    sliderButton[1].classList.add('slider__button--disable');
+  }
+
+  if (index === 1) {
+    sliderButton[0].classList.remove('slider__button--disable');
+  }
+});
