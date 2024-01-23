735d74d6fed89d795244609c62f5600d95b30959
Denis Reznik <reznik.d.o12@gmail.com>
2024-01-23 03:15:30 +0200

    add form submit

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,24 +1,21 @@
 import { menuToggle } from './lib/menu';
+import { resetForm } from './lib/form';
 
 const menuBtnRef = document.querySelector('[data-menu-button]');
 const menuLinks = document.querySelectorAll('.nav__link');
+const form = document.querySelector('.form');
 
 menuBtnRef.addEventListener('click', menuToggle);
+form.addEventListener('submit', resetForm);
 
 if (menuLinks.length > 0) {
   menuLinks.forEach(menuBurgerLink => {
     menuBurgerLink.addEventListener('click', function(e) {
-      // const linkId = menuBurgerLink.getAttribute('href').replace('#', '');
-      // const curentView = document.getElementById(linkId);
       const expanded = menuBtnRef
         .getAttribute('aria-expanded') === 'true' || false;
 
       if (expanded) {
         menuToggle();
-
-        // curentView.scrollIntoView({ behavior: 'smooth' });
-      // } else {
-      //   curentView.scrollIntoView({ behavior: 'smooth' });
       }
     });
   });
