8770895646b23c31036b26fba0bd9db865691b5c
Denis Reznik <reznik.d.o12@gmail.com>
2024-01-23 02:35:19 +0200

    add mobile, tablet and small desktop version

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-'use strict';
+import { menuToggle } from './lib/menu';
+
+const menuBtnRef = document.querySelector('[data-menu-button]');
+const menuLinks = document.querySelectorAll('.nav__link');
+
+menuBtnRef.addEventListener('click', menuToggle);
+
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuBurgerLink => {
+    menuBurgerLink.addEventListener('click', function(e) {
+      // const linkId = menuBurgerLink.getAttribute('href').replace('#', '');
+      // const curentView = document.getElementById(linkId);
+      const expanded = menuBtnRef
+        .getAttribute('aria-expanded') === 'true' || false;
+
+      if (expanded) {
+        menuToggle();
+
+        // curentView.scrollIntoView({ behavior: 'smooth' });
+      // } else {
+      //   curentView.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  });
+}
