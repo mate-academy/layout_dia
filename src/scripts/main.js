7b608fc270a6a9054ba2cba62b7e192e0afc7d00
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-02-06 10:47:34 +0100

    created header top and menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const burger = document.querySelector('.header__burger');
+  const menu = document.querySelector('.header__menu');
+  const body = document.body;
+
+  burger.addEventListener('click', function() {
+    burger.classList.toggle('active');
+    menu.classList.toggle('active');
+    body.classList.toggle('lock');
+  });
+});
