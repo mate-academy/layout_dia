994dc92185e1d7be7e202ed590dbcadf7b1817b2
Roman Ostrous <ostrous.roman@gmail.com>
2024-02-17 21:59:56 +0200

    add slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -16,3 +16,48 @@ function onFormSubmit(e) {
 const form = document.getElementById('contact-us-form');
 
 form.addEventListener('submit', onFormSubmit);
+
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slider__slide');
+const arrowLeft = document.querySelector('.slider__arrow-left');
+const arrowRight = document.querySelector('.slider__arrow-right');
+
+function showSlide(slideIndex) {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.classList.add('active');
+    } else {
+      slide.classList.remove('active');
+    }
+  });
+}
+
+function nextSlide() {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide--;
+
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  showSlide(currentSlide);
+}
+
+arrowLeft.addEventListener('click', () => {
+  prevSlide();
+});
+
+arrowRight.addEventListener('click', () => {
+  nextSlide();
+});
+
+setInterval(nextSlide, 5000);
+
+showSlide(currentSlide);
