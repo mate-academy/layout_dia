963f5a3fe3b36d03a752fa74eed6a71d83a0a783
Valentyn Sobol <valiksobol14@gmail.com>
2024-02-19 22:08:06 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// eslint-disable-next-line
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
