7383ea86b33d7d9662c7c5d9c62ad42fbf5893f6
Misha Vynnyk <vinnikmisha94@gmail.com>
2024-02-21 19:18:53 +0100

    finish header without slider

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -11,31 +11,3 @@ document.addEventListener('DOMContentLoaded', function() {
     body.classList.toggle('lock');
   });
 });
-
-const prevBtn = document.querySelector('.prev-btn');
-const nextBtn = document.querySelector('.next-btn');
-const slides = document.querySelector('.slides').children;
-const slideWidth = slides[0].getBoundingClientRect().width;
-let index = 0;
-
-nextBtn.addEventListener('click', () => {
-  index++;
-  changeSlide();
-});
-
-prevBtn.addEventListener('click', () => {
-  index--;
-  changeSlide();
-});
-
-function changeSlide() {
-  if (index < 0) {
-    index = slides.length - 1;
-  } else if (index >= slides.length) {
-    index = 0;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.transform = `translateX(-${slideWidth * index}px)`;
-  }
-}
