770a55fe3c988b5f703e7b696478fbb06a52f289
MatheusStefan <roccomatheus0@gmail.com>
2024-02-29 15:35:44 -0300

    update html and BEM issues

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,7 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
-const nextButton = document.querySelector('.carousel__button--right');
-const prevButton = document.querySelector('.carousel__button--left');
+const nextButton = document.querySelector('.carousel__button-right');
+const prevButton = document.querySelector('.carousel__button-left');
 
 const slideWith = slides[0].getBoundingClientRect().width;
 
