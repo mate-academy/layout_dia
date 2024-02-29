3d76561caee3328cdff2a3c060ee76a32a1cc7c2
Oleksandr Kovalchuk <kov280396@gmail.com>
2024-02-29 19:07:58 +0200

    implement mobile header section

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,9 @@
 'use strict';
+
+const buttons = document.querySelectorAll('.btn');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', () => {
+    alert('The feature is still under development');
+  });
+}
