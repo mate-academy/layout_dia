d5f926dd9fa5d2222add90580fa5e99d3abbe332
Anton Polishchuk <hardixxxxx@gmail.com>
2024-03-11 18:53:09 +0100

    check npm run deploy work

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,15 +3,18 @@
 const switcher = document.querySelector('.nav__theme-switcher');
 const header = document.querySelector('.header');
 const slider = document.querySelector('.slider');
+const footer = document.querySelector('.footer');
 
 switcher.addEventListener('click', () => {
   if (header.classList.contains('header--theme-light')) {
     header.classList.remove('header--theme-light');
     slider.classList.remove('slider--theme-light');
+    footer.classList.remove('footer--theme-light');
     switcher.innerHTML = 'to light theme';
   } else {
     header.classList.add('header--theme-light');
     slider.classList.add('slider--theme-light');
+    footer.classList.add('footer--theme-light');
     switcher.innerHTML = 'to dark theme';
   }
-})
+});
--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,22 +2,24 @@
 
 let currentIndex = 0;
 const slides = document.querySelectorAll('.slider__image');
+const prevSliderButton = document.querySelector('.slider__bt-left');
+const nextSliderButton = document.querySelector('.slider__bt-right');
 
 function showSlide() {
-  for (var i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
   slides[currentIndex].style.display = 'block';
 }
 
-function prevImage() {
+prevSliderButton.addEventListener('click', () => {
   currentIndex = (currentIndex - 1 + slides.length) % slides.length;
   showSlide();
-}
+});
 
-function nextImage() {
+nextSliderButton.addEventListener('click', () => {
   currentIndex = (currentIndex + 1) % slides.length;
   showSlide();
-}
+});
 
 showSlide();
