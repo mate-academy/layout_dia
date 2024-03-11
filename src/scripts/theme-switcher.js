fc24a5c4d56b24fc0656a229602fcac989effb7c
Anton Polishchuk <hardixxxxx@gmail.com>
2024-03-11 22:19:44 +0100

    fix autocomplete and change theme switcher

--- src/scripts/theme-switcher.js
+++ src/scripts/theme-switcher.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const switcher = document.querySelector('.nav__theme-switcher');
+const switcher = document.querySelector('.theme-switcher');
 const header = document.querySelector('.header');
 const slider = document.querySelector('.slider');
 const footer = document.querySelector('.footer');
@@ -10,11 +10,11 @@ switcher.addEventListener('click', () => {
     header.classList.remove('header--theme-light');
     slider.classList.remove('slider--theme-light');
     footer.classList.remove('footer--theme-light');
-    switcher.innerHTML = 'to light theme';
+    switcher.classList.remove('theme-switcher--theme-light');
   } else {
     header.classList.add('header--theme-light');
     slider.classList.add('slider--theme-light');
     footer.classList.add('footer--theme-light');
-    switcher.innerHTML = 'to dark theme';
+    switcher.classList.add('theme-switcher--theme-light');
   }
 });
