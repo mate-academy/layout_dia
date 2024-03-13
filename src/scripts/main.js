99e17750172bd37c59064e9dc13631e2492bfbcc
MatheusStefan <roccomatheus0@gmail.com>
2024-03-13 15:26:17 -0300

    update on js to load the carrousel when resized

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,13 +3,17 @@ const slides = Array.from(track.children);
 const nextButton = document.querySelector('.carousel__button-right');
 const prevButton = document.querySelector('.carousel__button-left');
 
-const slideWith = slides[0].getBoundingClientRect().width;
+const slideWidth = slides[0].getBoundingClientRect().width;
 
 const setSlidePosition = (slide, index) => {
-  slide.style.left = slideWith * index + 'px';
+  slide.style.left = slideWidth * index + 'px';
 };
 
 slides.forEach(setSlidePosition);
+
+window.addEventListener('resize', (e) => {
+  slides.forEach(setSlidePosition);
+});
 
 // eslint-disable-next-line no-shadow
 const moveToSlide = (track, currentSlide, targetSlide) => {
