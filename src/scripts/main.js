8899ccf4e615b738ef8c4143c81214a12332288b
Yuliia <kotyuliya24@gmail.com>
2024-04-04 13:01:36 -0400

    corrections

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,10 @@
 'use strict';
 
 const slider = document.querySelector('.slideshow__content');
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
 const slides = Array.from(slider.querySelectorAll('.slideshow__content--photo'));
 const slideCount = slides.length;
 let slideIndex = 0;
-
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
-
-function showPreviousSlide() {
-  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-  updateSlider();
-}
+let intervalId;
 
 function showNextSlide() {
   slideIndex = (slideIndex + 1) % slideCount;
@@ -30,6 +21,25 @@ function updateSlider() {
   });
 }
 
-updateSlider();
+function startAutoplay() {
+  intervalId = setInterval(showNextSlide, 3000);
+}
+
+function stopAutoplay() {
+  clearInterval(intervalId);
+}
+
+startAutoplay();
+
+document.querySelector('.prev-button').addEventListener('click', () => {
+  showPreviousSlide();
+  stopAutoplay();
+});
+
+document.querySelector('.next-button').addEventListener('click', () => {
+  showNextSlide();
+  stopAutoplay();
+});
+
 
 
