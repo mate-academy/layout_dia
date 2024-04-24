7a0ac5f09df200976a17dd4137374e315170df9f
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-04-24 16:34:28 +0300

    fix errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,15 @@
 'use strict';
 let iconMenu = document.querySelector('.icon-menu');
 if (iconMenu) {
-  document.addEventListener('click', function (e) {
+  iconMenu.addEventListener('click', function (e) {
     document.documentElement.classList.toggle('menu-open');
   });
 }
-if (!document.documentElement.classList.contains('menu-open')) {
-  document.documentElement.classList.remove('menu-open');
-}
+
+document.addEventListener('click', (e) => {
+  if (
+    e.target !== iconMenu &&
+    document.documentElement.classList.contains('menu-open')
+  )
+    document.documentElement.classList.remove('menu-open');
+});
