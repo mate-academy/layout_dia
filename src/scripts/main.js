bdedf43237d4a09f15591f3f9c4b9d2be69b0c9d
Oleksii Bidiak <oleksii.bidiak@gmail.com>
2024-04-24 14:55:24 +0300

    finish layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,10 @@
 'use strict';
+let iconMenu = document.querySelector('.icon-menu');
+if (iconMenu) {
+  document.addEventListener('click', function (e) {
+    document.documentElement.classList.toggle('menu-open');
+  });
+}
+if (!document.documentElement.classList.contains('menu-open')) {
+  document.documentElement.classList.remove('menu-open');
+}
