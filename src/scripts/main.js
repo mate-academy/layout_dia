57e3bc80e9c844c3889ed9874b3174a6fabaa629
beliyandr <beliyandr@gmail.com>
2024-05-05 23:33:11 +0300

    1st variant

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const burgerMenuButton = document.querySelector('.header__burger');
+const burgerMenu = document.querySelector('.header__menu');
+
+const menuList = document.querySelector('.menu__list');
+
+burgerMenuButton.addEventListener('click', openMenu);
+
+function openMenu() {
+  burgerMenu.classList.toggle('header__menu--active');
+
+  if (burgerMenu.classList.contains('header__menu--active')) {
+    document.body.style.overflow = 'hidden';
+  } else {
+    document.body.style.overflow = 'auto';
+  }
+}
+
+menuList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('menu__link')
+    && burgerMenu.classList.contains('header__menu--active')) {
+    openMenu();
+  }
+});
