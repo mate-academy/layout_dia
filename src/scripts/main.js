9b3d22158088a1bedaae5af28dba48f534726a49
Viktor Kniahnytskyi <vcutler02@gmail.com>
2024-05-07 11:35:15 +0300

    1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,42 @@
 'use strict';
+
+const arrowLeft = document.getElementById('arrow-left');
+const arrowRight = document.getElementById('arrow-right');
+const slides = document.getElementById('slides');
+
+arrowLeft.addEventListener('click', () => {
+  return move('left');
+});
+
+arrowRight.addEventListener('click', () => {
+  return move('right');
+});
+
+function move(direction) {
+  const slidesClasses = slides.className;
+
+  switch (true) {
+    case slidesClasses.includes(1) && direction === 'right':
+      slides.classList.remove('slider__slides--1-slide');
+      slides.classList.add('slider__slides--2-slide');
+      break;
+
+    case slidesClasses.includes(2) && direction === 'right':
+      slides.classList.remove('slider__slides--2-slide');
+      slides.classList.add('slider__slides--3-slide');
+      break;
+
+    case slidesClasses.includes(2) && direction === 'left':
+      slides.classList.remove('slider__slides--2-slide');
+      slides.classList.add('slider__slides--1-slide');
+      break;
+
+    case slidesClasses.includes(3) && direction === 'left':
+      slides.classList.remove('slider__slides--3-slide');
+      slides.classList.add('slider__slides--2-slide');
+      break;
+
+    default:
+      break;
+  }
+}
