38700697b543626f211b12d16e30ef69fcea1c55
Vlad <vladkygot@gmail.com>
2024-05-21 21:51:39 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,41 @@
-'use strict';
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+const totalSlides = slides.length;
+
+function showSlide(n) {
+  slides.forEach(slide => {
+    slide.classList.remove('active');
+    slide.classList.remove('next');
+    slide.classList.remove('prev');
+  });
+
+  slides[n].classList.add('active');
+  const nextSlideIndex = (n + 1) % totalSlides;
+  const prevSlideIndex = (n - 1 + totalSlides) % totalSlides;
+
+  slides[nextSlideIndex].classList.add('next');
+  slides[prevSlideIndex].classList.add('prev');
+}
+
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
+}
+
+function prevSlide() {
+  currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+let root = document.querySelector(":root");
+let button = document.querySelector("#dark");
+let cardNumber = document.querySelector(".card__number")
+let cardClick = document.querySelector(".card__click")
+
+button.addEventListener('click', () => {
+  event.preventDefault();
+  root.classList.toggle('dark');
+  })
