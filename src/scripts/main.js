6e35809687da7b96a15c24262c89c0807208685e
Hanna <hmatviienko@gmail.com>
2024-05-28 18:23:01 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const navLink = document.querySelector('a[href="#navigation"]');
+const navCloseBtn = document.querySelector('.header__cover--top--wrap--nav--links.close');
+const body = document.documentElement;
+
+navLink.addEventListener('click', () => {
+  body.classList.add('no-scroll');
+});
+
+navCloseBtn.addEventListener('click', () => {
+  body.classList.remove('no-scroll');
+});
