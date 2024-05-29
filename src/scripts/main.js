791fbb40679ecd4b1ea943f788b9a1c07ecf4651
Hanna <hmatviienko@gmail.com>
2024-05-29 12:57:36 +0300

    fixed scroll

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const navLink = document.querySelector('a[href="#navigation"]');
-const navCloseBtn = document.querySelector('.header__cover--top--wrap--nav--links.close');
 const body = document.documentElement;
 
 navLink.addEventListener('click', () => {
   body.classList.add('no-scroll');
 });
 
-navCloseBtn.addEventListener('click', () => {
-  body.classList.remove('no-scroll');
+const links = document.querySelectorAll('.header__cover--top--wrap--nav--links');
+links?.forEach(element => {
+  element.addEventListener('click', function() {
+    body.classList.remove('no-scroll');
+  });
 });
