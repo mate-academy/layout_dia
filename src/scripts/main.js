61511c2655ca60fbba53dce0f6656df2978e437e
Bagrunivskiy Stepan <bagrunivskitstepan@gmail.com>
2024-05-31 16:31:22 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,45 @@
 'use strict';
+document.addEventListener("DOMContentLoaded", function() {
+  const slides = document.querySelectorAll(".slide");
+  const prevBtn = document.querySelector(".prev-btn");
+  const nextBtn = document.querySelector(".next-btn");
+
+  let currentIndex = 0;
+
+  function showSlide(index) {
+      slides.forEach((slide, i) => {
+          if (i === index) {
+              slide.classList.add("active");
+          } else {
+              slide.classList.remove("active");
+          }
+      });
+  }
+
+  prevBtn.addEventListener("click", function() {
+      currentIndex = (currentIndex === 0) ? slides.length - 1 : currentIndex - 1;
+      showSlide(currentIndex);
+  });
+
+  nextBtn.addEventListener("click", function() {
+      currentIndex = (currentIndex === slides.length - 1) ? 0 : currentIndex + 1;
+      showSlide(currentIndex);
+  });
+});
+
+document.getElementById('first__button').addEventListener('click', function() {
+  // Перехід до другого блоку при натисканні кнопки
+  document.getElementById('expertise').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('second__button').addEventListener('click', function() {
+  // Перехід до другого блоку при натисканні кнопки
+  document.getElementById('expertise').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('third__button').addEventListener('click', function() {
+  // Перехід до другого блоку при натисканні кнопки
+  document.getElementById('expertise').scrollIntoView({ behavior: 'smooth' });
+});
+document.getElementById('fourth__button').addEventListener('click', function() {
+  // Перехід до другого блоку при натисканні кнопки
+  document.getElementById('expertise').scrollIntoView({ behavior: 'smooth' });
+});
