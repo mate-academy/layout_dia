3e3057b270eaa4e3eee238ef04a24e5e2e3a1d84
hardaira <hardaira@yahoo.com>
2024-06-23 23:45:57 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,2 @@
 'use strict';
-function next() {
-  document.getElementById('slider').style.background =
-    "url('./images/new-photos/bg2.jpg')";
-};
 
-function previous() {
-  document.getElementById('slider').style.background = "url('./images/new-photos/slider-xl.png')" ;
-};
