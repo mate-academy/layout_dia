bf440c7b6459ccd744a3a2da0f9abbcb167483b0
Fylhtq dfsg <galosaandrew@gmail.com>
2024-06-25 18:34:42 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
 'use strict';
+
+const imgPaths = [
+  'https://images.unsplash.com/photo-1434494878577-86c23bcb06b9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1512486130939-2c4f79935e4f?fm=jpg&w=3000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHBsYW50cyUyMGluJTIwb2ZmaWNlfGVufDB8fDB8fHww',
+  'https://images.unsplash.com/photo-1630233313373-a03df7d139c9?q=80&w=1938&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  'https://images.unsplash.com/photo-1718220216044-006f43e3a9b1?q=80&w=1980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+];
+
+let currentIndex = 0;
+
+const slider = document.querySelector('.slider__content--image');
+const arrowLeft = slider.querySelector('.prev');
+const arrowRight = slider.querySelector('.next');
+
+function updateSlider(index) {
+  slider.style.backgroundImage = `url(${imgPaths[index]})`;
+  console.log(`Background image set to: url(${imgPaths[index]})`);
+}
+
+arrowLeft.addEventListener('click', () => {
+  currentIndex = (currentIndex - 1 + imgPaths.length) % imgPaths.length;
+  updateSlider(currentIndex);
+});
+
+arrowRight.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % imgPaths.length;
+  updateSlider(currentIndex);
+});
+
+updateSlider(currentIndex);
