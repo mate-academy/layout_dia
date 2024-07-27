8d2c3d463fd777222955b4c1d11561032a01ea16
Bohdan Smachylo <smachyloooo@gmail.com>
2024-07-27 11:43:07 +0200

    add part

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,10 +1,17 @@
 'use strict';
 const swiper = new Swiper('.swiper', {
-    loop: true,
+  loop: true,
 
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+document
+  .getElementById('contactForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Form submitted!');
   });
