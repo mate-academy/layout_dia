651b9a9aa96faf51081e996d068d8b93e3584829
Anna <annanicky90gmail.com>
2024-08-09 00:45:18 +0400

    cleate landing AIR

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const slider = document.querySelector('.slides');
+const arrowLeft = document.querySelector('.slider__arrow--left');
+const arrowRight = document.querySelector('.slider__arrow--right');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+arrowLeft.addEventListener('click', showPreviousSlide);
+arrowRight.addEventListener('click', showNextSlide);
+
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+
+  updateSlider();
+}
+
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+updateSlider();
+
