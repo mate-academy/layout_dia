72690ba773cc6d6fa1cae169c3b7947cc2cfe790
Valentyn Radobenko <valikradobenko@gmail.com>
2024-08-24 22:19:58 +0300

    add all sections for phones before footer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,18 @@
-'use strict';
+const slider = document.querySelector('.slider');
+const images = [
+  "D:/projects/HTML_CSS/layout_dia/src/images/slider/slide-img-1.jfif",
+  "D:/projects/HTML_CSS/layout_dia/src/images/slider/slide-img-2.jfif",
+  "D:/projects/HTML_CSS/layout_dia/src/images/slider/slide-img-3.jfif"
+];
+
+let currentImageIndex = 0;
+
+document.querySelector('.slider__arrow-button--left').addEventListener('click', () => {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  slider.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+});
+
+document.querySelector('.slider__arrow-button--right').addEventListener('click', () => {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  slider.style.backgroundImage = `url('${images[currentImageIndex]}')`;
+});
