45538a7a84240751f13debd427eaa133336a4e1d
Valentyn Radobenko <valikradobenko@gmail.com>
2024-09-03 19:21:28 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,10 @@
+// #region slider
+
 const slider = document.querySelector('.slider');
 const images = [
-  'https://s3-alpha-sig.figma.com/img/df81/f355/fc941fc1356e0f607b1d633de094539e?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=TtK038YmGViVX49IFC6N1rxmi88iP58GKTmfrBvtoEohSCX0Xfhfs-cuyt01WZvtJV7jP-Wor2IU1Baz5ruh0ZJ3Mg5yuOuvomURczgHGN4ouNFdaJ1t6mNzumraq0hG0g~AmPmvPgMnjXdH7OvNkqimmforXPWIK97jIa8nXI6CbNq8tn~X8YDDDR-AZw1ZR84OW9zjo7UwbZdETyAiaz69QPlgd8D1Lk0Kk-TJw8PbszQOCU3-csRLknSXttJPAH4Ee22RNE5RyGw4sOrefRfADxVe5zRofM-s9uZn9bYEHbMU-nYHdPAZgInUIVC6hLxW~A0O5vdlSqSWKhaSig__',
-  'https://s3-alpha-sig.figma.com/img/8603/70f4/db2a054305f1b37c0ba617d02ccf6339?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ayEYAEtoLkyUYh6Rhqvg8XKFq5y8SIudRSqxtUX2ufHSVVMHDDU5RC4mFc1l6v~WG~1z9pS0o4vO0nrMiznJOERUl0wBHIMRTFUYudgkIQ1RvWWSBaMAlUk0QbprFkJfeyMuZ-WLGXQJn5TemQIYG4ey0UCABbEzD8-5yeWHiq8cranhnuxyiDOatkhHFR9kYO4wlLxZ-mX0ten-~~LxlweemF7LfrKyIdKpmx2Uyw38xiJJghvlIeRhLXTEM1eCWL41MSspFB-MMj4j8cMfylSWhRSbJzNxWaiFHZcs5rUldPLgzfuOO867hfhsVwxn1W4IQTgald9TuKv~Sr~U~Q__',
-  'https://s3-alpha-sig.figma.com/img/e6fb/cdee/100cb60dcea8715c8baf9c0b81225476?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=XySXvzBsa-SX6GRJpdKDGuS2sRu0YOTwwyKeZ~GHr-LTVX74YbHouyvFvlEZM7aVa4jc2~TnKE-2yRwBolIu-KhZjWIXMmw1hU16pT8S9dY-2TKCT60Ml6XPAt4tYXiGIWGZ9rRigciTSSriLvfRTZPiUym0-C4660j0SCQ-inrizAZszcKJiLi3pNrpCQlt3GY~8cVUiH3mTD~rq0HXMAo-1IDbMnEwtzC-aYh4Ctb17GxpJRHaqL2hDM04FIx-3GIFxOZZK~yG5X3E-f64W0sRGSD41pYtg1tVkyDESxGlYPpl6vOdr4xKP~nLwwyB9aUtkaO9PraySFepmBx0jA__',
+  'https://snipboard.io/VpZ8aF.jpg',
+  'https://snipboard.io/t0b962.jpg',
+  'https://snipboard.io/BzlZSp.jpg',
 ];
 
 let currentImageIndex = 0;
@@ -20,3 +22,110 @@ document
     currentImageIndex = (currentImageIndex + 1) % images.length;
     slider.style.backgroundImage = `url('${images[currentImageIndex]}')`;
   });
+
+// #endregion
+
+// #region icon
+
+const icons = document.querySelectorAll('.nav__icon');
+const menu = document.querySelector('.menu');
+const menuStyle = window.getComputedStyle(menu);
+const navLogos = document.querySelectorAll('.nav__logo');
+const navLinks = document.querySelectorAll('.nav__link');
+
+const toggleMenu = () => {
+  if (menuStyle.display === 'none' || menu.style.display === 'none') {
+    menu.style.display = 'block';
+    document.body.classList.add('menu-open');
+  } else if (menu.style.display === 'block') {
+    menu.style.display = 'none';
+    document.body.classList.remove('menu-open');
+  }
+};
+
+for (const icon of icons) {
+  icon.addEventListener('click', toggleMenu);
+}
+
+for (const navLogo of navLogos) {
+  navLogo.addEventListener('click', () => {
+    menu.style.display = 'none';
+    document.body.classList.remove('menu-open');
+  });
+}
+
+for (const navLink of navLinks) {
+  navLink.addEventListener('click', () => {
+    menu.style.display = 'none';
+    document.body.classList.remove('menu-open');
+  });
+}
+
+//#endregion
+
+// #region content in header nav
+
+function updateContent() {
+  const navHeader = document.querySelector('.nav--header');
+  const navLinksHeader = document.querySelectorAll('.nav__link--header');
+  for (let i = 0; i < navLinksHeader.length; i++) {
+    switch (i) {
+      case 0:
+        break;
+      case 1:
+        navLinksHeader[i].textContent = 'Services';
+        navLinksHeader[i].setAttribute('href', '#services');
+        break;
+      case 2:
+        navLinksHeader[i].textContent = 'Testimonials';
+        navLinksHeader[i].setAttribute('href', '#testimonials');
+        break;
+      case 3:
+        navLinksHeader[i].textContent = 'Contact Us';
+        navLinksHeader[i].setAttribute('href', '#feedback');
+        break;
+      default:
+        navLinksHeader[i].remove();
+    }
+  }
+}
+
+updateContent();
+
+window.addEventListener('resize', updateContent);
+
+//#endregion
+
+// #region sections appearance effect
+
+const sections = document.querySelectorAll('.hidden');
+let initialScroll = true;
+
+const toggleSectionVisibility = () => {
+  sections.forEach((section) => {
+    const sectionTop = section.getBoundingClientRect().top;
+    const sectionBottom = section.getBoundingClientRect().bottom;
+    const windowHeight = window.innerHeight;
+
+    if (sectionTop < windowHeight - 20 && sectionBottom > 20) {
+      section.classList.add('visible');
+    } else if (!section.classList.contains('nav--header')) {
+      section.classList.remove('visible');
+    }
+  });
+};
+
+const handleScroll = () => {
+  if (initialScroll) {
+    window.scrollTo(0, 0);
+    initialScroll = false;
+  }
+  if (menu.style.display !== 'block') {
+    toggleSectionVisibility();
+  }
+};
+
+window.addEventListener('scroll', handleScroll);
+document.addEventListener('DOMContentLoaded', handleScroll);
+
+// #endregion
