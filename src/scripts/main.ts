ef81174909198bb5b47060824cd07be07a2479f2
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2024-09-04 18:51:20 +0300

    slider was fully implemented

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,15 +1,11 @@
 'use strict';
 
-let SlideDirection;
+enum SlideDirection {
+  left = 'left',
+  right = 'right',
+};
 
-// eslint-disable-next-line no-shadow
-(function(SlideDirection) {
-  SlideDirection['left'] = 'left';
-  SlideDirection['right'] = 'right';
-})(SlideDirection || (SlideDirection = {}));
-;
-
-window.addEventListener('hashchange', function() {
+window.addEventListener('hashchange', () => {
   if (window.location.hash === '#menu') {
     document.body.classList.add('body-menu');
   } else {
@@ -18,14 +14,13 @@ window.addEventListener('hashchange', function() {
 });
 
 const buttonLeft = document
-  .querySelector('.header__slider-button--left');
+  .querySelector('.header__slider-button--left') as HTMLButtonElement;
 const buttonRight = document
-  .querySelector('.header__slider-button--right');
+  .querySelector('.header__slider-button--right') as HTMLButtonElement;
 let sliderImageIndexNumber = 1;
 
-function slideImageLeftAndRight(direction, button) {
+function slideImageLeftAndRight(direction: SlideDirection, button: HTMLButtonElement) {
   button.disabled = true;
-
   const leftIndex = sliderImageIndexNumber === 1
     ? 3
     : sliderImageIndexNumber - 1;
@@ -33,46 +28,40 @@ function slideImageLeftAndRight(direction, button) {
     ? 1
     : sliderImageIndexNumber + 1;
   const sliderImage = document
-    .querySelector('.header__slider-main-image');
-  const newSliderImage = document.createElement('img');
-
+    .querySelector('.header__slider-main-image') as HTMLElement;
+  const newSliderImage = document.createElement('img') as HTMLElement;
   sliderImageIndexNumber = direction === SlideDirection.left
     ? leftIndex
     : rightIndex;
 
   newSliderImage.setAttribute(
     'src',
-    '/slider-'.concat(sliderImageIndexNumber, '.png'),
+    `/slider-${sliderImageIndexNumber}.png`
   );
   newSliderImage.setAttribute('alt', 'Slider Image');
   newSliderImage.classList.add('header__slider-main-image');
   newSliderImage.style.opacity = '0';
-
   newSliderImage.style.transform = direction === SlideDirection.left
     ? 'translateX(100%)'
     : 'translateX(-50%)';
   sliderImage.after(newSliderImage);
-
-  setTimeout(function() {
+  setTimeout(() => {
     sliderImage.style.opacity = '0';
-
     sliderImage.style.transform = direction === SlideDirection.left
       ? 'translateX(-50%)'
       : 'translateX(100%)';
     newSliderImage.style.opacity = '100%';
     newSliderImage.style.transform = 'translateX(0)';
   }, 10);
-
-  setTimeout(function() {
-    sliderImage.remove();
+  setTimeout(() => {
+    sliderImage.remove()
     button.disabled = false;
   }, 300);
 }
 
-buttonLeft.addEventListener('click', function() {
-  return slideImageLeftAndRight(SlideDirection.left, buttonLeft);
-});
-
-buttonRight.addEventListener('click', function() {
-  return slideImageLeftAndRight(SlideDirection.right, buttonRight);
-});
+buttonLeft.addEventListener('click',
+  () => slideImageLeftAndRight(SlideDirection.left, buttonLeft)
+);
+buttonRight.addEventListener('click',
+  () => slideImageLeftAndRight(SlideDirection.right, buttonRight)
+);
