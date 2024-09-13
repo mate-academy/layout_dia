003ac3822c2217ecbb6ea2e56e8185e4d76ef0d2
Iryna Mariiko <IrynaMM497@gmail.com>
2024-09-13 19:12:54 +0300

    air done

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,37 @@
-'use strict';
+document.addEventListener('DOMContentLoaded', function () {
+  const prevButton = document.querySelector('.slider__switch--prev');
+  const nextButton = document.querySelector('.slider__switch--next');
+  const photos = document.querySelectorAll('.slider__photo');
+
+  let currentIndex = 0;
+
+  function updateSlider() {
+    photos.forEach(function(photo, index) {
+      if(index===currentIndex) {
+        photo.style.display = 'block';
+      } else {
+        photo.style.display = 'none';
+      }
+    });
+  }
+
+  prevButton.addEventListener('click', function() {
+    if (currentIndex > 0) {
+      currentIndex = currentIndex - 1;
+    } else {
+      currentIndex = photos.length - 1;
+    }
+    updateSlider();
+  });
+
+  nextButton.addEventListener('click', function() {
+    if (currentIndex < photos.length - 1) {
+      currentIndex = currentIndex + 1;
+      } else {
+        currentIndex = 0;
+      }
+      updateSlider();
+  });
+
+  updateSlider();
+});
