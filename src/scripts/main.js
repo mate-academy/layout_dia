041488860bc8adb8d2c7c4421c77e676da2a1015
hardaira <hardaira@yahoo.com>
2024-09-17 11:20:27 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,12 +14,18 @@ function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides');
 
+  if (slides.length === 0) {
+    console.error('No slides found.');
+    return;
+  }
+
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none';
   }
