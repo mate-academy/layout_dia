5ce1c65c4aa2b82714b64f0ddef2e7ffbbf82f9f
hardaira <hardaira@yahoo.com>
2024-09-17 08:56:22 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,46 +1,41 @@
 'use strict';
-      let slideIndex = 1;
+let slideIndex = 1;
 
-      // Next/previous controls
-      function plusSlides(n) {
-        showSlides((slideIndex += n));
-      }
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
 
-      // Thumbnail image controls
-      function currentSlide(n) {
-        showSlides((slideIndex = n));
-      }
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
 
-      function showSlides(n) {
-        let i;
-        let slides = document.getElementsByClassName('mySlides');
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName('mySlides');
 
-        if (n > slides.length) {
-          slideIndex = 1;
-        }
-        if (n < 1) {
-          slideIndex = slides.length;
-        }
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = 'none';
-        }
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
 
-        slides[slideIndex - 1].style.display = 'block';
+  slides[slideIndex - 1].style.display = 'block';
+}
 
-      }
-
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   showSlides(slideIndex);
-  /* setInterval(function () {
+
+  document.getElementById('prev-img').addEventListener('click', function () {
+    plusSlides(-1);
+  });
+
+  document.getElementById('next-img').addEventListener('click', function () {
     plusSlides(1);
-  }, 2000); */
-
-  document.getElementById("prev-img").addEventListener("click", function () {
-    showSlides(-1);
   });
-
-  document.getElementById("next-img").addEventListener("click", function () {
-    showSlides(1);
-  });
-
 });
