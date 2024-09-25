c186a67f0b431a6140a289f65605315a599b3d7e
hardaira <hardaira@yahoo.com>
2024-09-25 17:22:16 +0200

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,15 +1,17 @@
 'use strict';
+
 var slideIndex = 1;
 showSlides(slideIndex);
-
+console.log('hello');
 function plusSlides(n) {
+
   showSlides((slideIndex += n));
 }
 
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName('mySlides');
-
+console.log('hello dear');
   if (slides.length === 0) {
     console.error('No slides found.');
     return;
@@ -27,3 +29,10 @@ function showSlides(n) {
   }
   slides[slideIndex - 1].style.display = 'block';
 }
+
+const element1 = document.getElementById("prev-img");
+element1.addEventListener("click", plusSlides(-1));
+console.log(element1);
+const element2 = document.getElementById("next-img");
+element2.addEventListener("click", plusSlides(1));
+console.log(element2);
