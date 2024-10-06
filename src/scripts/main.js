ccbf579a1ff1eedef606d77511a0a6455c68d127
Albina <yakunina.albina@gmail.com>
2024-10-06 12:15:34 +0200

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,2 +1,13 @@
 'use strict';
 
+const dropdown = document.querySelector('.dropdown');
+const button = dropdown.querySelector('.dropdown__button');
+const content = dropdown.querySelector('.dropdown__content');
+
+triger.addEventListener('contextmenu', (event) => {
+  event.preventDefault();
+
+  dropdown.classList.toggle('.dropdown--active');
+
+});
+
