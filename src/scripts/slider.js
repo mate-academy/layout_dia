70cb79b73073d2ba4733dbe34e80af7bb8750df0
Marian <getmoodyup@gmail.com>
2024-10-08 17:11:08 +0300

    another variable for slides

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const theSlides = document.querySelectorAll('.slider_slide');
+const aSlides = document.querySelectorAll('.slider_slide');
 const nextSlideBtns = document.querySelectorAll('.slider_slide_extra_control--right');
 const prevSlideBtns = document.querySelectorAll('.slider_slide_extra_control--left');
 
@@ -16,24 +16,24 @@ prevSlideBtns.forEach(button => {
 });
 
 document.addEventListener('DOMContentLoaded', function () {
-  theSlides[slideIndex].classList.add('slider_slide--visible');
+  aSlides[slideIndex].classList.add('slider_slide--visible');
   intervalId = setInterval(nextSlide, 5000);
 });
 
 function showSlide(index) {
-  if (index >= theSlides.length) {
+  if (index >= aSlides.length) {
     slideIndex = 0;
   } else if (index < 0) {
-    slideIndex = theSlides.length - 1;
+    slideIndex = aSlides.length - 1;
   } else {
     slideIndex = index;
   }
 
-  theSlides.forEach(slide => {
+  aSlides.forEach(slide => {
     slide.classList.remove('slider_slide--visible');
   });
 
-  theSlides[slideIndex].classList.add('slider_slide--visible');
+  aSlides[slideIndex].classList.add('slider_slide--visible');
 }
 
 function prevSlide() {
