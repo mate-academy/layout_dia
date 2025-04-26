1587daad43cf524494f1d274e24223377ee9a041
Ralfendeck25 <poseidoncenghizerzhemetmhossad@outlook.com>
2025-04-26 00:10:56 -0300

    some chnages

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const btnLeft = document.getElementById('btn-left');
+
+const btnRight = document.getElementById('btn-right');
+
+const images = [
+  './images/slider/slide-1.png',
+  './images/slider/slide-2.png',
+  './images/slider/slide-3.png',
+];
+
+let num = 0;
+
+btnLeft.onclick = function() {
+  const slider = document.getElementById('slider');
+
+  num++;
+
+  if (num >= images.length) {
+    num = 0;
+  }
+  slider.src = images[num];
+};
+
+btnRight.onclick = function() {
+  const slider = document.getElementById('slider');
+
+  num--;
+
+  if (num < 0) {
+    num = images.length - 1;
+  }
+
+  slider.src = images[num];
+};
