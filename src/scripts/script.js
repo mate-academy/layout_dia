795909ee1ce16778ec165e452bd9cf5efce0bc06
Mykola Melnichuk <nikolfutdeveloper@gmail.com>
2025-05-08 15:55:02 +0200

    solution

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.mySwiper', {
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
