9da1ee61a710384a034313fc60ee1bea17697f2b
Yuriy Chornyi <chornyi@MacBook-Air-13.local>
2025-10-22 19:30:29 +0200

    air

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const sliderCollection = Array.from(document.querySelectorAll('.slide'));
+const moveBack = document.querySelector('.arrow__right');
+const moveForward = document.querySelector('.arrow__left');
+const sliderWrapper = document.querySelector('.slider__wrapper');
+
+let index = 0;
+
+moveForward.addEventListener('click', () => {
+  sliderCollection[index].classList.remove('active');
+  if (index >= sliderCollection.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  sliderCollection[index].classList.add('active');
+});
+
+moveBack.addEventListener('click', () => {
+  sliderCollection[index].classList.remove('active');
+  if (index < 1) {
+    index = sliderCollection.length - 1;
+  } else {
+    index -= 1;
+  }
+  sliderCollection[index].classList.add('active');
+});
