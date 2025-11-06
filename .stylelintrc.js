b8822da1c131366b41dbd69b447495b11515a252
Daniel <danielf.4691@gmail.com>
2025-11-05 22:11:58 -0300

    kebab-case fix

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,9 +3,5 @@ module.exports = {
   plugins: [
     "stylelint-scss"
   ],
-  rules: {
-    "custom-property-pattern": null,
-    "scss/at-mixin-pattern": null,
-    "scss/dollar-variable-pattern": null
-  }
+  rules: {}
 };
