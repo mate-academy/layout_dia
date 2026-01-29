8b3c452fd67b65824a9f56cb383aef1463843061
Yurii Kumpanenko <yuriikumpanenko@ukr.net>
2026-01-29 13:02:20 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const leftButton = document.querySelector('.arrow--left');
+const rightButton = document.querySelector('.arrow--right');
+const list = document.querySelectorAll('.slider__img');
+const listLength = list.length;
+let current = 0;
+
+const changeSlide = (step) => {
+  list[current].classList.remove('slider__img--active');
+
+  current = (current + step + listLength) % listLength;
+
+  list[current].classList.add('slider__img--active');
+};
+
+rightButton.addEventListener('click', () => {
+  changeSlide(1);
+});
+
+leftButton.addEventListener('click', () => {
+  changeSlide(-1);
+});
