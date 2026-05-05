e350b4c9be98a5f6116933ca8c58beaf0c5b510f
Victoria Neustroeva <neustrvk@gmail.com>
2026-05-05 23:29:10 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  //loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.icon--sliderArrow--next',
+    prevEl: '.icon--sliderArrow--prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
