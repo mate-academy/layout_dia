7b8c494d44e65774ba36cd03efa168fbb92e4d79
chumachenkoUA <kirito22122005@gmail.com>
2026-05-18 00:53:45 +0300

    complete task

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const slides = document.querySelectorAll('.slider__image');
+const prevButton = document.querySelector('.slider__control--prev');
+const nextButton = document.querySelector('.slider__control--next');
+
+let currentSlideIndex = 0;
+
+function showSlide(index) {
+  slides[currentSlideIndex].classList.remove('slider__image--active');
+  slides[index].classList.add('slider__image--active');
+
+  currentSlideIndex = index;
+}
+
+
+nextButton.addEventListener('click', () => {
+  const nextIndex = (currentSlideIndex + 1) % slides.length;
+
+  showSlide(nextIndex);
+});
+
+prevButton.addEventListener('click', () => {
+  const prevIndex = (currentSlideIndex - 1 + slides.length) % slides.length;
+
+  showSlide(prevIndex);
+})
+
+
+const form = document.querySelector('.footer__form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  form.reset();
+
+  window.scrollTo(0, 0);
+});
