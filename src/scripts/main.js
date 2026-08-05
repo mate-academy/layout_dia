e9bce311e5ef08532ff6743bd2ab9dca9bb548e9
Oleh Puzin <olegpuzin77@icloud.com>
2026-08-05 19:34:41 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  form.reset();
+  window.scrollTo({top:0}) || location.reload();
+
+});
+
+
+
